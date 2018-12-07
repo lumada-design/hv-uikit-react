c0c7f7e915b029c124266a696c8a0b1c2926816c
Francisco Guilherme <francisco.guilherme@hitachivantara.com>
2018-12-07 12:06:02 -0800

    Cleanup

--- examples/hv-ui-react/stories/button.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import HvButton from "../src/Button";
-
-storiesOf("Button", module).add("Button", () => <HvButton>Click!</HvButton>);
