927cdfc1316689554ba71cee716f67739fbde930
Francisco Guilherme <francisco.guilherme@hitachivantara.com>
2018-12-10 16:53:38 +0000

    Added info support for storybook

--- packages/ui-react/.storybook/config.js
+++ packages/ui-react/.storybook/config.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { configure, addDecorator } from "@storybook/react";
+import { withInfo } from '@storybook/addon-info';
 import HvProvider from "../src/Provider";
 import theme from "@hv-ui/themes/dist/theme";
 
@@ -8,6 +9,8 @@ const req = require.context("../stories", true, /\.js$/);
 const loadStories = () => {
   req.keys().forEach(filename => req(filename));
 };
+
+addDecorator(withInfo);
 
 addDecorator(story => (
   <HvProvider theme={theme} r>
