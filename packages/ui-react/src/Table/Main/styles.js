de7ff3bd0d88f7ecd8b1416f16235182a401393b
Diogo Mariano <diogo.mariano@hitachivantara.com>
2018-12-13 17:56:29 +0000

    [IA-451] - Review table component interface
    - Allow control of server side pagination, show pagination controls and sort as props
    - Reviewed styles to not lock the table styles to component props
    - Documentation to each input prop added
    - Unit tests still to be added

--- packages/ui-react/src/Table/Main/styles.js
+++ packages/ui-react/src/Table/Main/styles.js
@@ -8,54 +8,95 @@
  *  under which the software has been supplied.
  */
 
-const styles = theme => ({
-  getProps: () => ({
-    style: {
-      ...theme.hv.typography.normalText,
-      fontFamily: theme.hv.typography.fontFamily,
-      textAlign: "right",
-      border: "none"
-    }
-  }),
-  getTableProps: () => ({
-    style: {
-      border: `solid 1px ${theme.palette.grey.plain}`,
-      borderBottom: "none"
-    }
-  }),
-  getTheadProps: () => ({
-    style: {
-      background: theme.palette.grey.rainy,
-      textAlign: `right`,
-      borderBottom: `solid 1px ${theme.palette.grey.plain}`,
-      boxShadow: `none`
-    }
-  }),
-  getTrGroupProps: () => ({
-    style: {
-      borderBottom: `solid 1px ${theme.palette.grey.plain}`
-    }
-  }),
-  getTdProps: () => ({
-    style: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "flex-end",
-      border: "none"
-    }
-  }),
-  getTheadThProps: () => ({
-    style: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "flex-end",
-      border: "none",
-      boxShadow: "none"
-    }
-  }),
-  rtSortIcon: {
-    marginRight: `${theme.spacing.xs}px`
-  }
+const tableStyleOverrides = classes => ({
+  getProps: () => ({ className: classes.root }),
+  getTableProps: () => ({ className: classes.table }),
+  getTheadGroupProps: () => ({ className: classes.theadGroup }),
+  getTheadGroupTrProps: () => ({ className: classes.theadGroupTr }),
+  getTheadGroupThProps: () => ({ className: classes.theadGroupTh }),
+  getTheadProps: () => ({ className: classes.thead }),
+  getTheadThProps: () => ({ className: classes.theadTh }),
+  getTheadFilterProps: () => ({ className: classes.theadFilter }),
+  getTheadFilterTrProps: () => ({ className: classes.theadFilterTr }),
+  getTheadFilterThProps: () => ({className: classes.theadFilterTh  }),
+  getTbodyProps: () => ({ className: classes.tbody }),
+  getTrGroupProps: () => ({ className: classes.trGroups }),
+  getTrProps: () => ({ className: classes.tr }),
+  // getThProps: () => ({ className: classes.th }), TODO: There is a bug in the component, where this prop is not used
+  getTdProps: () => ({ className: classes.td }),
+  getTfootProps: () => ({ className: classes.tfoot }),
+  getTfootTrProps: () => ({ className: classes.tfootTr }),
+  getTfootThProps: () => ({ className: classes.tfootTh }),
+  getPaginationProps: () => ({ className: classes.pagination }),
+  getLoadingProps: () => ({ className: classes.loading }),
+  getNoDataProps: () => ({ className: classes.noDate }),
+  getResizerProps: () => ({ className: classes.resizer })
 });
 
-export default styles;
+const styles = theme => ({
+  root: {
+    ...theme.hv.typography.normalText,
+    fontFamily: theme.hv.typography.fontFamily,
+    textAlign: "right",
+    border: "none",
+    "& $table": {
+      border: `solid 1px ${theme.palette.grey.plain}`,
+      borderBottom: "none",
+      "& $thead": {
+        background: theme.palette.grey.rainy,
+        textAlign: `right`,
+        borderBottom: `solid 1px ${theme.palette.grey.plain}`,
+        boxShadow: `none`,
+        "& $theadTh": {
+          height: 32,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          border: "none",
+          boxShadow: "none"
+        }
+      },
+      "& $tbody": {
+        "& $trGroups": {
+          borderBottom: `solid 1px ${theme.palette.grey.plain}`
+        },
+        "& $td": {
+          border: "none"
+        }
+      }
+    },
+    "& $td": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+    }
+  },
+  table: {},
+  theadGroup: {},
+  theadGroupTr: {},
+  theadGroupTh: {},
+  thead: {},
+  theadTh: {},
+  theadFilter: {},
+  theadFilterTr: {},
+  theadFilterTh : {},
+  tbody: {},
+  trGroups: {},
+  tr: {},
+  // th: {}, TODO: There is a bug in the component, where this prop is not used
+  td: {},
+  tfoot: {},
+  tfootTr: {},
+  tfootTh: {},
+  pagination: {},
+  loading: {},
+  noDate: {},
+  resizer: {},
+
+
+  rtSortIcon: {
+    marginRight: `${theme.spacing.xs}px`
+  },
+});
+
+export { styles, tableStyleOverrides };
