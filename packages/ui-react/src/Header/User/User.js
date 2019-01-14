1d3e9b6751014a244396516d032eaeaa8b717c11
Dakai Du <dakai.du@hitachivantara.com>
2019-01-14 12:02:51 -0800

    Navbar Settings Menu

--- packages/ui-react/src/Header/User/User.js
+++ packages/ui-react/src/Header/User/User.js
@@ -10,27 +10,111 @@
 
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import Button from "@material-ui/core/Button";
 import styles from "./styles";
 
-const User = ({ classes, userData, logout }) => {
-  if (!userData) return "";
+import SettingsButton from "@material-ui/icons/SettingsOutlined";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
 
-  return (
-    <div className={classes.user}>
-      <div className={classes.userInfo}>
-        <Typography className={classes.userName}>{userData.name}</Typography>
-        <Typography className={classes.userRole}>{userData.role}</Typography>
+class User extends React.Component {
+  state = {
+    userMenuOpen: false,
+    settingsMenuOpen: false
+  };
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount = () => {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  };
+
+  setWrapperRef = node => {
+    this.wrapperRef = node;
+  };
+
+  handleClickOutside = event => {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      this.setState({
+        settingsMenuOpen: false,
+        userMenuOpen: false
+      });
+    }
+  };
+
+  toggleUserMenu = () => {
+    this.setState({
+      userMenuOpen: !this.state.userMenuOpen,
+      settingsMenuOpen: false
+    });
+  };
+
+  toggleSettingsMenu = () => {
+    this.setState({
+      settingsMenuOpen: !this.state.settingsMenuOpen,
+      userMenuOpen: false
+    });
+  };
+
+  render() {
+    const { classes, userData, logout } = this.props;
+    if (!userData) return "";
+
+    return (
+      <div className={classes.user} ref={this.setWrapperRef}>
+        <div className={classes.userInfo}>
+          <Typography className={classes.userName}>{userData.name}</Typography>
+          <Typography className={classes.userRole}>{userData.role}</Typography>
+        </div>
+        <IconButton
+          className={classes.userButton}
+          onClick={this.toggleUserMenu}
+        >
+          <AccountCircle
+            className={classNames(
+              this.state.userMenuOpen ? classes.dropdown : "",
+              classes.userIcon
+            )}
+          />
+          {this.state.userMenuOpen && (
+            <div className={classes.menuList}>
+              <div className={classes.menuItem}>Profile</div>
+              <div className={classes.menuItem} onClick={() => logout()}>
+                Logout
+              </div>
+            </div>
+          )}
+        </IconButton>
+        <IconButton
+          className={classes.userButton}
+          onClick={this.toggleSettingsMenu}
+        >
+          <SettingsButton
+            className={classNames(
+              this.state.settingsMenuOpen ? classes.dropdown : "",
+              classes.userIcon
+            )}
+          />
+          {this.state.settingsMenuOpen && (
+            <div className={classes.menuList}>
+              <div className={classes.menuItem}>Event Settings</div>
+              <div className={classes.menuItem}>Work Request Settings</div>
+              <div className={classes.menuItem}>Anaytics Settings</div>
+              <div className={classes.menuItem}>User Management</div>
+            </div>
+          )}
+        </IconButton>
       </div>
-      <IconButton className={classes.userButton} onClick={() => logout()}>
-        <AccountCircle className={classes.userIcon} />
-      </IconButton>
-    </div>
-  );
-};
+    );
+  }
+}
 
 User.propTypes = {
   classes: PropTypes.instanceOf(Object).isRequired,
--- packages/ui-react/src/Header/User/styles.js
+++ packages/ui-react/src/Header/User/styles.js
@@ -38,10 +38,41 @@ const styles = theme => ({
   },
   userButton: {
     color: theme.palette.text.main,
-    width: 40
+    width: 50,
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
   },
   userIcon: {
-    fontSize: 35
+    fontSize: 35,
+    position: "relative",
+    background: "white"
+  },
+  dropdown: {
+    border: `solid 2px ${theme.palette.grey.passive}`,
+    borderBottom: "none",
+    bottom: "-1px"
+  },
+  menuList: {
+    border: `solid 2px ${theme.palette.grey.passive}`,
+    padding: `${theme.spacing.xs}px`,
+    position: "absolute",
+    whiteSpace: "nowrap",
+    top: 48,
+    right: 5,
+    zIndex: "-1"
+  },
+  menuItem: {
+    display: "block",
+    padding: `${theme.spacing.xs}px`,
+    fontFamily: theme.typography.fontFamily,
+    color: `${theme.palette.secondary.main}`,
+    textAlign: "left",
+    fontSize: "15px",
+    textTransform: "capitalize",
+    "&:hover": {
+      background: `${theme.palette.grey.clear}`
+    }
   }
 });
 
