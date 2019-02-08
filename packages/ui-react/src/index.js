25ebc75aed5f56945df97ef784015045f2b0962f
Francisco Guilherme <francisco.guilherme@hitachivantara.com>
2019-02-08 16:59:56 +0000

    [CLEANUP] - Export showcase component on index

--- packages/ui-react/src/index.js
+++ packages/ui-react/src/index.js
@@ -21,6 +21,7 @@ export { default as HvTable } from "./Table";
 export { default as HvSlider } from "./Slider";
 export { default as HvNavigationAnchors } from "./NavigationAnchors";
 export { HvCheckBox, HvRadio, labelPositions } from "./Selectors";
+export { default as HvShowCase, HvShowCaseHeader } from "./Showcase";
 export { default as HvSnackbar } from "./Snackbar";
 export {
   default as HvInput,
