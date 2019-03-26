cfc061af1523fb8bd446ff8e4dd476b6835423c1
Diogo Mariano <diogo.mariano@hitachivantara.com>
2019-03-26 14:27:42 +0000

    Add License Headers

--- packages/ui-react/doc/samples/lab/searchBox/searchBoxSimple.js
+++ packages/ui-react/doc/samples/lab/searchBox/searchBoxSimple.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Hitachi Vantara Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from "react";
 import { forceReRender } from "@storybook/react";
 import HvSearchBox from "@hv-ui/react/core/SearchBox";
@@ -14,11 +30,9 @@ class WrapperWithList extends React.Component {
     forceReRender();
   };
 
-  filter = () => {
-    return this.state.value !== ""
+  filter = () => this.state.value !== ""
       ? this.list.filter(word => word.includes(this.state.value))
       : this.list;
-  };
 
   render() {
     return (
--- packages/ui-react/src/theme/overrides/muiButton.js
+++ packages/ui-react/src/theme/overrides/muiButton.js
@@ -1,11 +1,17 @@
-/**
- * Copyright (c) 2018 Hitachi Vantara Corporation.
+/*
+ * Copyright 2019 Hitachi Vantara Corporation
  *
- *  The copyright to the computer software herein is the property of
- *  Hitachi Vantara Corporation. The software may be used and/or copied only
- *  with the written permission of Hitachi Vantara Corporation or in accordance
- *  with the terms and conditions stipulated in the agreement/contract
- *  under which the software has been supplied.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import palette from "../palette";
