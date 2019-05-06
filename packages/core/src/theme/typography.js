37701eb1171098727efaa05ba2515694a9d2b390
Tiago Simões Santos <tiago.santos@hitachivantara.com>
2019-05-06 11:49:55 +0100

    refactor(typography): reintroduce typography variant overrides. #132

--- packages/core/src/theme/typography.js
+++ packages/core/src/theme/typography.js
@@ -20,7 +20,34 @@ const typography = (palette, theme) =>
   createTypography(palette, {
     useNextVariants: true,
     suppressDeprecationWarnings: true,
-    fontFamily: theme.typography.fontFamily
+    fontFamily: theme.typography.fontFamily,
+    h1: {
+      ...theme.typography.xlTitle
+    },
+    h2: {
+      ...theme.typography.lTitle
+    },
+    h3: {
+      ...theme.typography.mTitle
+    },
+    h4: {
+      ...theme.typography.sTitle
+    },
+    body1: {
+      ...theme.typography.normalText
+    },
+    body2: {
+      ...theme.typography.infoText
+    },
+    subtitle1: {
+      ...theme.typography.highlightText
+    },
+    subtitle2: {
+      ...theme.typography.labelText
+    },
+    button: {
+      ...theme.typography.highlightText
+    }
   });
 
 export default typography;
