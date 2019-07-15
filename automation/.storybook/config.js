0dbfd5c6d2e1994c180425c8beacf464a384f9a0
Diogo Mariano <diogo.mariano@hitachivantara.com>
2019-07-15 14:13:26 +0100

    feat(qa): created QA Automation Storybook. #228

---
+++ automation/.storybook/config.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { addDecorator, configure, addParameters } from "@storybook/react";
+import HvProvider from "@hv/uikit-react-core/dist/Provider";
+import { create } from "@storybook/theming";
+import withLayout from "./layout-addon";
+
+const req = require.context("../stories", true, /\.js$/);
+
+const routerWrapper = {
+  push: (route, params, options) => {},
+  prefetch: (route, params) => {}
+};
+
+const loadStories = () => {
+  req.keys().forEach(filename => req(filename));
+};
+
+let appTheme = "dawn";
+
+addParameters({
+  options: {
+    theme: create({
+      brandTitle: "React UI-Kit",
+      brandUrl: "https://github.com/pentaho/hv-uikit-react"
+    }),
+    showPanel: false
+  }
+});
+
+addDecorator(withLayout());
+
+const App = ({ story, initialTheme }) => {
+  const [theme, setTheme] = useState(initialTheme);
+
+  const switchTheme = () => {
+    let newTheme = theme === "dawn" ? "wicked" : "dawn";
+    setTheme(newTheme);
+    appTheme = newTheme;
+  };
+
+  return (
+    <HvProvider
+      router={routerWrapper}
+      uiKitTheme={theme}
+      changeTheme={() => switchTheme()}
+    >
+      {story()}
+    </HvProvider>
+  );
+};
+
+addDecorator(story => <App story={story} initialTheme={appTheme} />);
+
+configure(loadStories, module);
---
+++ automation/.storybook/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports =  ({ config, mode }) => {
+  // Extend defaultConfig as you need.
+
+  config.module.rules.push({
+    test: /\.txt$/i,
+    use: "raw-loader"
+  });
+
+  config.module.rules.push({
+    test: /\.js?$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-docgen",
+          ]
+        }
+      }
+    ]
+  });
+
+  return config;
+};
