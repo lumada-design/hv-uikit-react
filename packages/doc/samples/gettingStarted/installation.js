be61bcf74c3083ff7250bf3dcabbaad7e92c1998
Tiago Simões Santos <tiago.santos@hitachivantara.com>
2019-08-08 17:40:44 +0100

    refactor(storybook): change location of md. #280

--- packages/doc/samples/gettingStarted/installation.js
+++ packages/doc/samples/gettingStarted/installation.js
@@ -17,7 +17,7 @@
 import React from "react";
 import CodeBlock from "./CodeBlock";
 import ReactMarkdown from "react-markdown";
-import input from "../../../../INSTALLATION.md";
+import input from "../../pages/gettingStarted/INSTALLATION.md";
 
 const Code = ({ value }) => <CodeBlock value={value} language="bash" />;
 
--- packages/doc/samples/gettingStarted/supportedComponents.js
+++ packages/doc/samples/gettingStarted/supportedComponents.js
@@ -17,7 +17,7 @@
 import React from "react";
 import "./supportedComponents.css";
 import ReactMarkdown from "react-markdown";
-import input from "../../../../SUPPORTED_COMPONENTS.md";
+import input from "../../pages/gettingStarted/SUPPORTED_COMPONENTS.md";
 
 const SupportedComponents = classes => <ReactMarkdown source={input} />;
 
