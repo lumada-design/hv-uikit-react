6411b9dc0a1d0babfac79b7950c4068b4740ac9a
Tiago Simões Santos <tiago.santos@hitachivantara.com>
2019-08-08 18:59:13 +0100

    feat(examples): add example to storybook. #280

--- packages/doc/samples/gettingStarted/examples.js
+++ packages/doc/samples/gettingStarted/examples.js
@@ -16,18 +16,8 @@
 
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import input from "../../pages/gettingStarted/INTRODUCTION.md";
+import input from "../../pages/gettingStarted/EXAMPLES.md";
 
-import corePackage from "../../../core/package";
-import labPackage from "../../../lab/package";
-import iconsPackage from "../../../icons/package";
-
-const Introduction = () => {
-  const inputParsed = input
-    .replace("%CORE_VERSION%", corePackage.version)
-    .replace("%LAB_VERSION%", labPackage.version)
-    .replace("%ICONS_VERSION%", iconsPackage.version);
-  return <ReactMarkdown source={inputParsed} />;
-};
+const Introduction = () => <ReactMarkdown source={input} />;
 
 export default Introduction;
--- packages/doc/stories/0-gettingStarted/0-4-examples.js
+++ packages/doc/stories/0-gettingStarted/0-4-examples.js
@@ -15,19 +15,7 @@
  */
 
 import React from "react";
-import ReactMarkdown from "react-markdown";
-import input from "../../pages/gettingStarted/INTRODUCTION.md";
+import { storiesOf } from "@storybook/react";
+import Examples from "../../samples/gettingStarted/examples";
 
-import corePackage from "../../../core/package";
-import labPackage from "../../../lab/package";
-import iconsPackage from "../../../icons/package";
-
-const Introduction = () => {
-  const inputParsed = input
-    .replace("%CORE_VERSION%", corePackage.version)
-    .replace("%LAB_VERSION%", labPackage.version)
-    .replace("%ICONS_VERSION%", iconsPackage.version);
-  return <ReactMarkdown source={inputParsed} />;
-};
-
-export default Introduction;
+storiesOf("Getting Started", module).add("Examples", () => <Examples />);
