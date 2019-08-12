3ca4c549e91621c116b5c0d8f745e7bf883633a9
Elio Freitas <elio.freitas@hitachivantara.com>
2019-08-12 10:53:23 +0100

    refactor(list): exposing id for items in list. #62

--- packages/doc/samples/components/list/multiSelectionWithSelectors.js
+++ packages/doc/samples/components/list/multiSelectionWithSelectors.js
@@ -3,23 +3,28 @@ import List from "@hv/uikit-react-core/dist/List";
 
 const data = [
   {
+    id: "1",
     label: "Arhauss is somewhere",
     selected: false
   },
   {
+    id: "2",
     label: "Allentown is not are 51",
     selected: false
   },
   {
+    id: "3",
     label: "Bergamo where you can eat",
     selected: true
   },
   {
+    id: "4",
     label: "Bergen city",
     selected: false
   },
   {
-    label: "Boston of the Seven Seas",
+    id: "5",
+    label: "Bergen city",
     selected: false
   }
 ];
