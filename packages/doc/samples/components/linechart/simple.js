0741ba46fb879175ee2742b53b1d571afa8e03a6
Tiago Simões Santos <tiago.santos@hitachivantara.com>
2019-09-19 16:55:55 +0100

    fix(linechart): corrections on samples. #550

--- packages/doc/samples/components/linechart/simple.js
+++ packages/doc/samples/components/linechart/simple.js
@@ -2,20 +2,31 @@ import React from "react";
 import Linechart from "@hv/uikit-react-core/dist/Linechart";
 
 const trace1 = {
-  x: ["January", "February", "March"],
-  y: [2300, 1000, 8500],
+  x: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ],
+  y: [5929, 2393, 1590, 7817, 4749, 1702, 2381, 2909, 6732, 3098, 2119, 2146],
   name: "Sales Target"
 };
 const data = [trace1];
 
 export default (
-  <div>
-    <Linechart
-      title="Simple Vertical Bar Chart"
-      subtitle="Sales performance (YTD)"
-      data={data}
-      xAxisTitle="2018"
-      yAxisTitle="Thousands of Dollars ($)"
-    />
-  </div>
+  <Linechart
+    title="Simple line"
+    subtitle="Sales performance (YTD)"
+    data={data}
+    xAxisTitle="2018"
+    yAxisTitle="Thousands of Dollars ($)"
+  />
 );
