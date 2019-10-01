508d4e9be6a2e89664b4099b1d2f2b1f8a09eed4
Elio Freitas <elio.freitas@hitachivantara.com>
2019-10-01 16:50:55 +0100

    feat(listview): implement List view.#557

---
+++ packages/core/src/AssetInventory/ListView/ListViewRow/ListViewRow.js
@@ -0,0 +1,316 @@
+/*
+ * Copyright 2019 Hitachi Vantara Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import map from "lodash/map";
+import isNil from "lodash/isNil";
+import Grid from "@material-ui/core/Grid";
+import MoreVert from "@hv/uikit-react-icons/dist/MoreOptionsVertical.S";
+import HvCheckbox from "../../../Selectors/CheckBox";
+import HvButton from "../../../Button";
+import DropDownMenu from "../../../DropDownMenu";
+import Cell from "../ListViewCell";
+import { ListViewContextConsumer } from "../ListViewContext/ListViewContext";
+
+const renderActions = (
+  id,
+  actions,
+  actionsCallback,
+  classes,
+  maxVisibleActions
+) => {
+  if (!Array.isArray(actions)) {
+    return React.isValidElement(actions) ? actions : null;
+  }
+
+  const renderButton = action => (
+    <HvButton
+      className={classes.button}
+      disabled={action.disabled}
+      onClick={() => actionsCallback(id, action)}
+      category="ghostSecondary"
+    >
+      {action.icon && action.icon({ classes })}
+      {action.label}
+    </HvButton>
+  );
+
+  const renderActionsGrid = acts => (
+    <Grid container>
+      <Grid item xs={8} className={classes.item}>
+        {renderButton(acts[0])}
+      </Grid>
+      <Grid item xs={4} className={classes.item}>
+        <DropDownMenu
+          className={classes.button}
+          icon={<MoreVert />}
+          placement="left"
+          disablePortal={false}
+          onClick={action => actionsCallback(id, action)}
+          dataList={acts.slice(1).map(a => ({ ...a, leftIcon: a.icon }))}
+        />
+      </Grid>
+    </Grid>
+  );
+
+  return actions.length > maxVisibleActions
+    ? renderActionsGrid(actions)
+    : actions.map(a => renderButton(a));
+};
+
+const selectCell = (
+  classes,
+  onCheckboxSelected,
+  checkboxValue,
+  checkboxSelected,
+  checkboxIndeterminate,
+  checkboxSemantic
+) => (
+  <Cell className={classes.selectCell} semantic={checkboxSemantic}>
+    <HvCheckbox
+      value={checkboxValue}
+      onChange={onCheckboxSelected}
+      checked={checkboxSelected}
+      indeterminate={checkboxIndeterminate}
+    />
+  </Cell>
+);
+
+const actionsCell = (classes, id, viewConfiguration) => (
+  <Cell className={classes.actionSeparator}>
+    {renderActions(
+      id,
+      viewConfiguration.actions,
+      viewConfiguration.actionsCallback,
+      classes,
+      viewConfiguration.maxVisibleActions
+    )}
+  </Cell>
+);
+
+const row = (
+  viewConfiguration,
+  classes,
+  className,
+  children,
+  id,
+  isSelectable,
+  onSelection,
+  checkboxValue,
+  checkboxSelected,
+  checkboxIndeterminate,
+  checkboxSemantic,
+  other
+) => {
+  const columnConfiguration =
+    isNil(viewConfiguration) || isNil(viewConfiguration.columnConfiguration)
+      ? null
+      : viewConfiguration.columnConfiguration;
+  const onCheckboxSelected =
+    isNil(viewConfiguration) || isNil(viewConfiguration.onSelection)
+      ? onSelection
+      : viewConfiguration.onSelection;
+  let renderSelectCell = !isNil(isSelectable);
+  renderSelectCell =
+    isNil(viewConfiguration) || isNil(viewConfiguration.isSelectable)
+      ? renderSelectCell
+      : viewConfiguration.isSelectable;
+  const renderActionsCell = !(
+    isNil(viewConfiguration) || isNil(viewConfiguration.actions)
+  );
+  const clonedChildren = map(children, (child, index) => {
+    if (!isNil(columnConfiguration)) {
+      return React.cloneElement(child, {
+        style: {
+          ...columnConfiguration[index].style
+        },
+        cellSpacing: columnConfiguration[index].spacing,
+        columnIndex: index
+      });
+    }
+    return child;
+  });
+
+  return (
+    <tr
+      id={id}
+      className={classNames(className, classes.root, {
+        [classes.selectable]: renderSelectCell,
+        [classes.notSelectable]: !renderSelectCell
+      })}
+      {...other}
+    >
+      {renderSelectCell &&
+        selectCell(
+          classes,
+          onCheckboxSelected,
+          checkboxValue,
+          checkboxSelected,
+          checkboxIndeterminate,
+          checkboxSemantic
+        )}
+      {clonedChildren}
+      {renderActionsCell &&
+        actionsCell(classes, checkboxValue || id, viewConfiguration)}
+    </tr>
+  );
+};
+
+const ListViewRow = ({
+  viewConfiguration,
+  classes,
+  className,
+  id,
+  children,
+  isSelectable,
+  onSelection,
+  checkboxValue,
+  checkboxSelected,
+  checkboxIndeterminate,
+  checkboxSemantic,
+  ...other
+}) => (
+  <ListViewContextConsumer>
+    {contextConfiguration => {
+      if (contextConfiguration && isNil(viewConfiguration)) {
+        return row(
+          contextConfiguration,
+          classes,
+          className,
+          children,
+          id,
+          isSelectable,
+          onSelection,
+          checkboxValue,
+          checkboxSelected,
+          checkboxIndeterminate,
+          checkboxSemantic,
+          other
+        );
+      }
+      return row(
+        viewConfiguration,
+        classes,
+        className,
+        children,
+        id,
+        isSelectable,
+        onSelection,
+        checkboxValue,
+        checkboxSelected,
+        checkboxIndeterminate,
+        checkboxSemantic,
+        other
+      );
+    }}
+  </ListViewContextConsumer>
+);
+
+ListViewRow.propTypes = {
+  /**
+   * Id to be applied to the root node this value is used to identify the checkbox clicks or actions.
+   */
+  id: PropTypes.string,
+  /**
+   * Class names to be applied.
+   */
+  className: PropTypes.string,
+  /**
+   * This value is provided by the asset inventory list view and contains necessary configurations for the columns.
+   */
+  viewConfiguration: PropTypes.instanceOf(Object),
+  /**
+   * A Jss Object used to override or extend the styles applied.
+   */
+  classes: PropTypes.shape({
+    /**
+     * Styles applied to the root class.
+     */
+    root: PropTypes.string
+  }).isRequired,
+  /**
+   * The content inside the row.
+   */
+  children: PropTypes.node.isRequired,
+  /**
+   *  The function that will be executed when the row is selected.
+   */
+  onSelection: PropTypes.func,
+  /**
+   * ´true´ if the row should have a checkbox in the the left part to be selectable ´false´ if it is not required.
+   */
+  isSelectable: PropTypes.bool,
+  /**
+   *  The value the checkbox in the in the left part of the row will return when selected.
+   */
+  checkboxValue: PropTypes.string,
+  /**
+   *  ´true´ if the checkbox is selected or ´false´ if not selected.
+   *
+   *  Note: if this value is specified the checkbox becomes a controlled component and it's state should be set from outside.
+   */
+  checkboxSelected: PropTypes.bool,
+  /**
+   *  ´true´ if the checkbox should use the intermediate state when selected ´false´ if not.
+   */
+  checkboxIndeterminate: PropTypes.bool,
+  /**
+   *  The border to the right of the checkbox
+   */
+  checkboxSemantic: PropTypes.oneOf([
+    "sema1",
+    "sema2",
+    "sema3",
+    "sema4",
+    "sema5",
+    "sema6",
+    "sema7",
+    "sema8",
+    "sema9",
+    "sema10",
+    "sema11",
+    "sema12",
+    "sema13",
+    "sema14",
+    "sema15",
+    "sema16",
+    "sema17",
+    "sema18",
+    "sema19",
+    "atmo1",
+    "atmo2",
+    "atmo3",
+    "atmo4",
+    "atmo5",
+    "atmo6"
+  ])
+};
+
+ListViewRow.defaultProps = {
+  className: "",
+  isSelectable: undefined,
+  id: undefined,
+  viewConfiguration: null,
+  checkboxValue: "",
+  checkboxSelected: undefined,
+  checkboxIndeterminate: undefined,
+  checkboxSemantic: undefined,
+  onSelection: () => {}
+};
+
+export default ListViewRow;
---
+++ packages/doc/stories/3-components/assetinventorylistview.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Hitachi Vantara Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import AssetInventoryListView from "@hv/uikit-react-core/dist/AssetInventory/ListView";
+
+storiesOf("Components", module).add("AssetInventoryListView", () => <AssetInventoryListView />, {
+  title: "AssetInventoryListView",
+  description: "",
+  usage: "import AssetInventoryListView from '@hv/uikit-react-core/dist/AssetInventoryListView'",
+  examples: [
+    {
+      title: "Complete usage",
+      description: "Sample usage of AssetInventoryListView",
+      src: "components/assetinventorylistview/sample.js"
+    },
+    {
+      title: "Not selectable",
+      description: "Sample usage of AssetInventoryListView but is not selectable",
+      src: "components/assetinventorylistview/sampleNotSelectable.js"
+    },
+    {
+      title: "Not selectable with menu",
+      description: "Sample usage of AssetInventoryListView but is not configured",
+      src: "components/assetinventorylistview/sampleNotSelectableWithMenu.js"
+    },
+    {
+      title: "No configuration",
+      description: "Sample usage of AssetInventoryListView with no configuration",
+      src: "components/assetinventorylistview/sampleNotStyled.js"
+    }
+  ]
+});*/
