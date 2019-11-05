1d01ca047d5263f7c92db00af017cf96bc10632c
Tiago Simões Santos <tiago.santos@hitachivantara.com>
2019-11-05 14:42:09 +0000

    refactor(templates): fix templates. #759

---
+++ packages/doc/samples/templates/home/views/detail/table/Table.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import HvTable from "@hv/uikit-react-core/dist/Table";
+
+const Table = ({data, columns, title}) => {
+  const [pageSize, setPageSize] = useState(10);
+
+  const onPageSizeChange = newPageSize => setPageSize(newPageSize);
+
+  return (
+    <HvTable
+      data={data}
+      columns={columns}
+      defaultPageSize={10}
+      pageSize={pageSize}
+      resizable={false}
+      labels={{
+        titleText: title
+      }}
+      idForCheckbox="id"
+      onPageSizeChange={onPageSizeChange}
+    />
+  );
+};
+
+export default Table;
