48c69ed924e6bb290492f6b68d3c8df8d5cd3bc3
João Gonçalves <joao.goncalves@hitachivantara.com>
2019-11-12 21:51:32 +0000

    automation(selenium): smoke tests for some complex components #729

--- automation/storybook/stories/core/dropdownMenu.js
+++ automation/storybook/stories/core/dropdownMenu.js
@@ -19,13 +19,13 @@ import { storiesOf } from "@storybook/react";
 import Grid from "@hv/uikit-react-core/dist/Grid";
 import DropdownMenu1 from "../../../../packages/doc/samples/components/dropdownMenu/dropdownMenu1";
 import DropdownMenu2 from "../../../../packages/doc/samples/components/dropdownMenu/dropdownMenu2";
-
+import DropdownMenu3 from "../../../../packages/doc/samples/components/dropdownMenu/dropdownMenu3";
 // sample scenarios
 const samples = {};
 
 samples.DropdownMenu1 = DropdownMenu1;
 samples.DropdownMenu2 = DropdownMenu2;
-
+samples.DropdownMenu3 = DropdownMenu3;
 
 // create CoreTextArea for each sample
 Object.keys(samples).forEach(key =>
