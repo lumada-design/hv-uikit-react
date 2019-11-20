286debdd4936353c804f9b1f23bdb12660032a0e
Joao Goncalves <9842927+JoaoEnesGoncalves@users.noreply.github.com>
2019-11-20 14:00:58 +0000

    fix(automation): adapt automation scripts #836

--- automation/storybook/stories/core/snackbar.js
+++ automation/storybook/stories/core/snackbar.js
@@ -21,7 +21,8 @@ import Snackbar1 from "../../../../packages/doc/samples/components/snackbar/snac
 import Snackbar2 from "../../../../packages/doc/samples/components/snackbar/snackbar2";
 import Snackbar3 from "../../../../packages/doc/samples/components/snackbar/snackbar3";
 import Snackbar4 from "../../../../packages/doc/samples/components/snackbar/snackbar4";
-import Snackbar5 from "../../../../packages/doc/samples/components/snackbar/snackbar6";
+import Snackbar5 from "../../../../packages/doc/samples/components/snackbar/snackbar5";
+import Snackbar6 from "../../../../packages/doc/samples/components/snackbar/snackbar6";
 
 // sample scenarios
 const samples = {};
@@ -30,6 +31,7 @@ samples.Snackbar2 = Snackbar2;
 samples.Snackbar3 = Snackbar3;
 samples.Snackbar4 = Snackbar4;
 samples.Snackbar5 = Snackbar5;
+samples.Snackbar6 = Snackbar6;
 
 // create CoreTextArea for each sample
 Object.keys(samples).forEach(key =>
