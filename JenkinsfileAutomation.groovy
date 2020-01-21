773dacb1370cc980952022477ce5c15be1434ee1
João Gonçalves <joao.goncalves@hitachivantara.com>
2020-01-21 15:22:40 +0000

    build(automation): increase jenkins pipeline timeout

--- JenkinsfileAutomation.groovy
+++ JenkinsfileAutomation.groovy
@@ -6,7 +6,7 @@ pipeline {
     }
     options {
         timestamps ()
-        timeout(time: 145, unit: 'MINUTES')
+        timeout(time: 180, unit: 'MINUTES')
         disableConcurrentBuilds()
     }
 
