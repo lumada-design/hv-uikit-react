83c00db2f55d223bb70815e45b6748a28d40aee7
Bruno Henriques <bruno.henriques@hitachivantara.com>
2020-02-28 16:04:51 +0000

    chore: uniformize new files
    Fix accessibility tests
    Remove licence header from new files

--- packages/core/src/Tabs/Tabs.js
+++ packages/core/src/Tabs/Tabs.js
@@ -1,23 +1,6 @@
-/*
- * Copyright 2020 Hitachi Vantara Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React from "react";
 import PropTypes from "prop-types";
-import Tabs from "@material-ui/core/Tabs";
-import withStyles from "@material-ui/core/styles/withStyles";
+import { Tabs, withStyles } from "@material-ui/core";
 import styles from "./styles";
 
 const HvTabs = ({ classes, ...others }) => (
@@ -57,7 +40,7 @@ HvTabs.propTypes = {
     /**
      * Styles applied to the `TabIndicator` component.
      */
-    scroller: PropTypes.string,
+    scroller: PropTypes.string
   }).isRequired,
   /**
    * Callback fired when the value changes.
