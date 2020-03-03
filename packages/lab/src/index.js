875fb0f3767a38e57dc1af5b182ae5598a4beb1f
Bruno Henriques <bruno.henriques@hitachivantara.com>
2020-03-03 19:07:02 +0000

    feat(deprecations): remove searchbox from lab. #820

--- packages/lab/src/index.js
+++ packages/lab/src/index.js
@@ -3,5 +3,4 @@ export { default as HvFooter } from "./Footer";
 export { default as HvFormComposer } from "./FormComposer";
 export { default as HvNavigationAnchors } from "./NavigationAnchors";
 export { default as HvNotificationPanel } from "./NotificationPanel";
-export { default as HvSearchBox } from "./SearchBox";
 export { default as HvSlider } from "./Slider";
