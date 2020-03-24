68950b73ce869eb0b94dad643c7ab00012e7f204
Hugo Figueira <hugo.figueira@hitachivantara.com>
2020-03-24 18:39:14 +0000

    fix(badge): alignments and styling. #1273

--- packages/core/src/Badge/styles.js
+++ packages/core/src/Badge/styles.js
@@ -32,13 +32,14 @@ const styles = theme => ({
   },
   badgeIcon: {
     position: "relative",
-    top: "2px",
+    top: "1px",
     left: "-7px"
   },
   showCount: {
     ...theme.hv.typography.labelText,
     fontFamily: theme.hv.typography.fontFamily,
-    padding: "0 0.5em",
+    padding: "0 5px",
+    maxWidth: 30,
     color: theme.hv.palette.atmosphere.atmo1
   },
   badgeOneDigit: {
--- packages/doc/samples/components/badge/badgeSimple.js
+++ packages/doc/samples/components/badge/badgeSimple.js
@@ -3,15 +3,17 @@ import HvBadge from "@hv/uikit-react-core/dist/Badge";
 
 const exampleStyles = {
   display: "flex",
-  width: "400px",
+  flexWrap: "wrap",
+  maxWidth: "800px",
+  padding: "0",
   justifyContent: "space-between"
 };
 
 export default (
   <div style={exampleStyles}>
     <HvBadge count={1} />
-    <HvBadge showCount count={8} />
-    <HvBadge showCount count={22} />
+    <HvBadge showCount count={2} />
+    <HvBadge showCount count={22} maxCount={9} />
     <HvBadge showCount count={100} />
   </div>
 );
