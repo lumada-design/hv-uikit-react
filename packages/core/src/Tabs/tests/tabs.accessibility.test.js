7a6784717dbbf2d206c7fab5a23af33fbe1576c3
Julio Costa <julio.costa@hitachivantara.com>
2020-04-01 10:40:49 +0100

    update tests

--- packages/core/src/Tabs/tests/tabs.accessibility.test.js
+++ packages/core/src/Tabs/tests/tabs.accessibility.test.js
@@ -1,29 +1,14 @@
-/*
- * Copyright 2020 Hitachi Vantara Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /* eslint-env jest */
 
 import React from "react";
 import { mount } from "enzyme";
 
-import { axe, toHaveNoViolations } from "jest-axe";
+import { toHaveNoViolations } from "jest-axe";
+import axe from "../../../config/axe-config";
 
 import HvProvider from "../../Provider";
-import TabWidthStyles from "../../Tab";
-import TabsWidthStyles from "../index";
+import Tab from "../../Tab";
+import Tabs from "..";
 
 expect.extend(toHaveNoViolations);
 
@@ -32,19 +17,19 @@ describe("Tabs A11Y", () => {
 
   beforeEach(async () => {
     wrapper = mount(
-      <HvProvider>
-        <TabsWidthStyles value={1}>
-          <TabWidthStyles label="Clickable Tab" />
-          <TabWidthStyles label="Clickable Tab" />
-          <TabWidthStyles label="Clickable Tab" />
-          <TabWidthStyles label="Clickable Tab" />
-        </TabsWidthStyles>
-      </HvProvider>
+        <HvProvider>
+          <Tabs value={1}>
+            <Tab label="Clickable Tab" />
+            <Tab label="Clickable Tab" />
+            <Tab label="Clickable Tab" />
+            <Tab label="Clickable Tab" />
+          </Tabs>
+        </HvProvider>
     );
   });
 
   it("should render correctly", async () => {
-    const results = await axe(wrapper.getDOMNode()[1]);
+    const results = await axe(wrapper.html());
     expect(results).toHaveNoViolations();
   });
 });
