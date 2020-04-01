c5e5ae4b405f05ad56cf3848ec0ee631bbe7a64a
João Gonçalves <joao.goncalves@hitachivantara.com>
2020-04-01 09:34:24 +0100

    test(automation): incresead pipeline timeout

--- JenkinsfileAutomation.groovy
+++ JenkinsfileAutomation.groovy
@@ -6,7 +6,7 @@ pipeline {
     }
     options {
         timestamps ()
-        timeout(time: 180, unit: 'MINUTES')
+        timeout(time: 360, unit: 'MINUTES')
         disableConcurrentBuilds()
     }
 
