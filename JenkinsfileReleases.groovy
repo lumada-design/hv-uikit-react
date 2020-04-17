0b25fa16509642fdb44f5f46be0a8842867a4f70
Diogo Mariano <diogo.mariano@hitachivantara.com>
2020-04-17 17:02:39 +0100

    chore: remove params with correct default values

--- JenkinsfileReleases.groovy
+++ JenkinsfileReleases.groovy
@@ -32,11 +32,8 @@ pipeline {
 
             steps {
                 build job: "ui-kit/react/master", parameters: [
-                    booleanParam(name: 'skipLint', value: false),
-                    booleanParam(name: 'skipJavascriptTest', value: false),
                     booleanParam(name: 'skipAutomationTest', value: false),
-                    booleanParam(name: 'skipPublish', value: false),
-                    booleanParam(name: 'skipPublishDoc', value: 'false')
+                    booleanParam(name: 'skipPublish', value: false)
                 ]
             }
         }
