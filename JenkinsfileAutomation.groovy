82bdf215d51b641b1f12f4103b094986b1da99d4
Diogo Mariano <diogo.mariano@hitachivantara.com>
2020-04-23 11:14:52 +0100

    chore: fix ciren alpha nightly checks

--- JenkinsfileAutomation.groovy
+++ JenkinsfileAutomation.groovy
@@ -14,11 +14,7 @@ pipeline {
         stage('Automation Regression Tests') {
             steps {
                 build job: "ui-kit/react/alpha", parameters: [
-                    booleanParam(name: 'skipLint', value: false),
-                    booleanParam(name: 'skipJavascriptTest', value: true),
-                    booleanParam(name: 'skipAutomationTest', value: false),
-                    booleanParam(name: 'skipPublish', value: true),
-                    booleanParam(name: 'skipPublishDoc', value: true)
+                    booleanParam(name: 'skipAutomationTest', value: false)
                 ]
             }
         }
