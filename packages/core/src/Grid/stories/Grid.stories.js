20d6cd6f3ec3eb59db31e8c7278f159677649192
Nelson Antunes <nelson.antunes@hitachivantara.com>
2020-05-04 15:33:07 +0100

    docs: fixes story instantiation to allow use of hv theme #1431

--- packages/core/src/Grid/stories/Grid.stories.js
+++ packages/core/src/Grid/stories/Grid.stories.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import { withStyles, withTheme } from "@material-ui/core";
+import { withStyles, useTheme } from "@material-ui/core";
 import { HvGrid, HvTypography, useWidth } from "../..";
 
 export default {
@@ -36,79 +36,77 @@ const styles = theme => ({
 const StyledBox = withStyles(styles)(Box);
 
 export const Main = () => {
-  const Sample = withTheme(({ theme }) => {
-    const breakpoint = useWidth();
-    const title = `Breakpoint: ${breakpoint}`;
+  const theme = useTheme();
 
-    const styledContainer = {
-      border: "1px solid",
-      borderColor: theme.hv.palette.atmosphere.atmo5,
-      backgroundColor: theme.hv.palette.atmosphere.atmo2,
-      marginTop: theme.hv.spacing.xs
-    };
+  const breakpoint = useWidth();
+  const title = `Breakpoint: ${breakpoint}`;
 
-    return (
-      <div>
-        <HvTypography variant="highlightText">{title}</HvTypography>
-        <div style={styledContainer}>
-          <HvGrid container>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(value => (
-              <HvGrid key={value} item xs={1} sm={1} md={1} lg={1} xl={1}>
-                <StyledBox text={value.toString()} />
-              </HvGrid>
-            ))}
-          </HvGrid>
-        </div>
+  const styledContainer = {
+    border: "1px solid",
+    borderColor: theme.hv.palette.atmosphere.atmo5,
+    backgroundColor: theme.hv.palette.atmosphere.atmo2,
+    marginTop: theme.hv.spacing.xs
+  };
+
+  return (
+    <div>
+      <HvTypography variant="highlightText">{title}</HvTypography>
+      <div style={styledContainer}>
+        <HvGrid container>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(value => (
+            <HvGrid key={value} item xs={1} sm={1} md={1} lg={1} xl={1}>
+              <StyledBox text={value.toString()} />
+            </HvGrid>
+          ))}
+        </HvGrid>
       </div>
-    );
-  });
-  return <Sample />;
+    </div>
+  );
 };
 
 export const Behaviour = () => {
-  const Sample = withTheme(({ theme }) => {
-    const breakpoint = useWidth();
-    const title = `Breakpoint: ${breakpoint}`;
+  const theme = useTheme();
 
-    const styledContainer = {
-      border: "1px solid",
-      borderColor: theme.hv.palette.atmosphere.atmo5,
-      backgroundColor: theme.hv.palette.atmosphere.atmo2,
-      marginTop: theme.hv.spacing.xs
-    };
+  const breakpoint = useWidth();
+  const title = `Breakpoint: ${breakpoint}`;
 
-    return (
-      <div>
-        <HvTypography variant="highlightText">{title}</HvTypography>
-        <div style={styledContainer}>
-          <HvGrid container>
-            <HvGrid item xs={4} sm={8} md={8} lg={12} xl={12}>
-              <StyledBox text={"xs={4} sm={8} md={8} lg={12} xl={12}"} />
-            </HvGrid>
-            <HvGrid item xs={4} sm={4} md={4} lg={6} xl={6}>
-              <StyledBox text={"xs={4} sm={4} md={4} lg={6} xl={6}"} />
-            </HvGrid>
-            <HvGrid item xs={4} sm={4} md={4} lg={6} xl={6}>
-              <StyledBox text={"xs={4} sm={4} md={4} lg={6} xl={6}"} />
-            </HvGrid>
-            <HvGrid item xs={1} sm={2} md={2} lg={3} xl={3}>
-              <StyledBox text={"xs={1} sm={2} md={2} lg={3} xl={3}"} />
-            </HvGrid>
-            <HvGrid item xs={1} sm={2} md={2} lg={3} xl={3}>
-              <StyledBox text={"xs={1} sm={2} md={2} lg={3} xl={3}"} />
-            </HvGrid>
-            <HvGrid item xs={1} sm={2} md={2} lg={3} xl={3}>
-              <StyledBox text={"xs={1} sm={2} md={2} lg={3} xl={3}"} />
-            </HvGrid>
-            <HvGrid item xs={1} sm={2} md={2} lg={3} xl={3}>
-              <StyledBox text={"xs={1} sm={2} md={2} lg={3} xl={3}"} />
-            </HvGrid>
+  const styledContainer = {
+    border: "1px solid",
+    borderColor: theme.hv.palette.atmosphere.atmo5,
+    backgroundColor: theme.hv.palette.atmosphere.atmo2,
+    marginTop: theme.hv.spacing.xs
+  };
+
+  return (
+    <div>
+      <HvTypography variant="highlightText">{title}</HvTypography>
+      <div style={styledContainer}>
+        <HvGrid container>
+          <HvGrid item xs={4} sm={8} md={8} lg={12} xl={12}>
+            <StyledBox text={"xs={4} sm={8} md={8} lg={12} xl={12}"} />
           </HvGrid>
-        </div>
+          <HvGrid item xs={4} sm={4} md={4} lg={6} xl={6}>
+            <StyledBox text={"xs={4} sm={4} md={4} lg={6} xl={6}"} />
+          </HvGrid>
+          <HvGrid item xs={4} sm={4} md={4} lg={6} xl={6}>
+            <StyledBox text={"xs={4} sm={4} md={4} lg={6} xl={6}"} />
+          </HvGrid>
+          <HvGrid item xs={1} sm={2} md={2} lg={3} xl={3}>
+            <StyledBox text={"xs={1} sm={2} md={2} lg={3} xl={3}"} />
+          </HvGrid>
+          <HvGrid item xs={1} sm={2} md={2} lg={3} xl={3}>
+            <StyledBox text={"xs={1} sm={2} md={2} lg={3} xl={3}"} />
+          </HvGrid>
+          <HvGrid item xs={1} sm={2} md={2} lg={3} xl={3}>
+            <StyledBox text={"xs={1} sm={2} md={2} lg={3} xl={3}"} />
+          </HvGrid>
+          <HvGrid item xs={1} sm={2} md={2} lg={3} xl={3}>
+            <StyledBox text={"xs={1} sm={2} md={2} lg={3} xl={3}"} />
+          </HvGrid>
+        </HvGrid>
       </div>
-    );
-  });
-  return <Sample />;
+    </div>
+  );
 };
 
 Behaviour.story = {
