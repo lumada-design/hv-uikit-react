dd15c7837f550d6b2ec530e70a820e56c1cd9464
Bruno Henriques <bruno.henriques@hitachivantara.com>
2020-05-08 15:51:21 +0100

    chore: add generator script

---
+++ scripts/generator/templates/component/ComponentName.js
@@ -0,0 +1,36 @@
+import React from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core";
+import styles from "./styles";
+
+/**
+ * ComponentName description/documentation paragraph
+ */
+const HvComponentName = props => {
+  const { className, classes, ...others } = props;
+
+  return (
+    <div className={clsx(className, classes.root)} {...others}>
+      <div>ComponentName</div>
+    </div>
+  );
+};
+
+HvComponentName.propTypes = {
+  /**
+   * Class names to be applied.
+   */
+  className: PropTypes.string,
+  /**
+   * A Jss Object used to override or extend the styles applied.
+   */
+  classes: PropTypes.shape({
+    /**
+     * Styles applied to the component root class.
+     */
+    root: PropTypes.string
+  }).isRequired
+};
+
+export default withStyles(styles, { name: "HvComponentName" })(HvComponentName);
