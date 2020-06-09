deae9b64a655f7c6bba4aeee6fe49ea9efc963c9
Diogo Mariano <diogo.mariano@hitachivantara.com>
2020-06-09 10:56:29 +0100

    docs(code-editor): add configuration instructions. #1629

--- packages/lab/src/CodeEditor/CodeEditor.js
+++ packages/lab/src/CodeEditor/CodeEditor.js
@@ -24,6 +24,10 @@ export const DefaultCodeEditorOptions = {
   }
 };
 
+/**
+ * A wrapper to the React Monaco editor (https://github.com/react-monaco-editor/react-monaco-editor) with our styles.
+ * Please make sure you follow the instructions (found in the repository) to include the component. Webpack configurations, and MonacoWebpackPlugin, are required to see the editor syntax highlight.
+ */
 const CodeEditor = ({ classes, theme, defaultValue, options, editorProps, ...others }) => {
   // merges the 2 objects together, overriding defaults with passed in options
   const mergedOptions = {
