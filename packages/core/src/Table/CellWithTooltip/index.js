abd78851701c43fcdea70a93146eafa483c8c991
Tiago Santos <tiago.santos@hitachivantara.com>
2020-06-15 16:39:59 +0100

    feat(table): add tooltip capability. #1609

---
+++ packages/core/src/Table/CellWithTooltip/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import withStyles from "@material-ui/core/styles/withStyles";
+import React, { useEffect, useRef, useState } from "react";
+import clsx from "clsx";
+import { HvTooltip, HvTypography } from "../..";
+import { styles } from "../styles";
+
+const Index = ({ classes, data }) => {
+  const [isOverflowed, setIsOverflowed] = useState(false);
+  const textRef = useRef();
+
+  useEffect(() => {
+    if (textRef?.current) {
+      setIsOverflowed(
+        textRef.current.scrollWidth > textRef.current.clientWidth ||
+          textRef.current.scrollHeight > textRef.current.clientHeight
+      );
+    }
+  }, []);
+
+  return (
+    <HvTooltip
+      disableHoverListener={!isOverflowed}
+      placement="top-start"
+      title={<HvTypography variant="infoText">{data}</HvTypography>}
+    >
+      <div ref={textRef} className={clsx(classes.tooltipAnchor, classes.textContainer)}>
+        {data}
+      </div>
+    </HvTooltip>
+  );
+};
+export default withStyles(styles, {
+  name: "OverflowTooltip"
+})(React.memo(Index));
