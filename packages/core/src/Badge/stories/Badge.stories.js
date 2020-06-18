f5fa745e750a0661db6ea7ec48604a57a8ad7e3c
Tomoto Washio <tomoto.washio@hitachivantara.com>
2020-06-18 11:26:35 -0700

    docs: add more DS-compliant example

--- packages/core/src/Badge/stories/Badge.stories.js
+++ packages/core/src/Badge/stories/Badge.stories.js
@@ -24,7 +24,8 @@ export const Main = () => (
     <HvBadge id="badge2" showCount count={8} />
     <HvBadge id="badge3" showCount count={22} />
     <HvBadge id="badge4" showCount count={100} />
-    <HvBadge id="labelBadge1" label="New" />
+    <HvBadge id="labelBadge1" label="100%" />
+    <HvBadge id="labelBadge2" label="New" />
   </>
 );
 
@@ -39,7 +40,8 @@ export const WithIcon = () => (
     <HvBadge id="badge7" showCount count={8} icon={<Alert />} />
     <HvBadge id="badge8" showCount count={88} icon={<Alert />} />
     <HvBadge id="badge9" showCount count={888} icon={<Alert />} />
-    <HvBadge id="labelBadge2" label="New" icon={<Alert />} />
+    <HvBadge id="labelBadge3" label="100%" icon={<Alert />} />
+    <HvBadge id="labelBadge4" label="New" icon={<Alert />} />
   </>
 );
 
@@ -59,7 +61,8 @@ export const WithText = () => (
     <HvBadge id="badge12" showCount count={8} text="Events" textVariant="sTitle" />
     <HvBadge id="badge13" showCount count={88} text="Events" textVariant="sTitle" />
     <HvBadge id="badge14" showCount count={888} text="Events" textVariant="sTitle" />
-    <HvBadge id="labelBadge3" label="New" text="Events" textVariant="sTitle" />
+    <HvBadge id="labelBadge5" label="100%" text="Events" textVariant="sTitle" />
+    <HvBadge id="labelBadge6" label="New" text="Events" textVariant="sTitle" />
   </>
 );
 
