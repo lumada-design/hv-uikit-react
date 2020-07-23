da9e9505ee78f1dc1110a898e94feda5b494fa48
Elio Freitas <elio.freitas@hitachivantara.com>
2020-07-23 16:24:15 +0100

    feat(input): infomessage.#1762

---
+++ packages/core/src/Forms/InfoMessage/stories/InfoMessage.stories.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { HvInfoMessage } from "../../..";
+
+export default {
+  title: "Patterns/Forms/Info Message",
+  parameters: {
+    componentSubtitle: null,
+    usage: "import { HvInfoMessage } from '@hv/uikit-react-core/dist'"
+  },
+  component: HvInfoMessage
+};
+
+export const Main = () => <HvInfoMessage id="infoMessage">Info message</HvInfoMessage>;
+
+export const DisabledInfoMessage = () => {
+  return (
+    <HvInfoMessage id="infoMessage-disabled" disabled>
+      Info message
+    </HvInfoMessage>
+  );
+};
+
+DisabledInfoMessage.story = {
+  parameters: {
+    docs: {
+      storyDescription: "Info message showcasing the disabled state."
+    },
+    pa11y: {
+      ignore: [
+        "region",
+        // Text or images of text that are part of an inactive user interface component have no contrast requirement.
+        // https://github.com/lumada-design/hv-uikit-react/issues/775#issuecomment-557167364
+        "WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Fail",
+        "color-contrast"
+      ]
+    }
+  }
+};
---
+++ packages/core/src/Forms/InfoMessage/styles.js
@@ -0,0 +1,13 @@
+const styles = theme => ({
+  root: {
+    display: "inline-block"
+  },
+  infoDisabled: {
+    color: theme.hv.palette.atmosphere.atmo5
+  },
+  gutter: {
+    marginBottom: 8
+  }
+});
+
+export default styles;
