a2eab927e87790bcfa8da8d98b78873b84061246
Tiago Santos <tiago.santos@hitachivantara.com>
2020-08-19 15:52:58 +0100

    feat(templates): remove section. HVUIKIT-5480

--- doc/Templates/home/views/detail/index.js
+++
@@ -1,5 +0,0 @@
-import withStyles from "@material-ui/core/styles/withStyles";
-import styles from "./styles";
-import Detail from "./Detail";
-
-export default withStyles(styles)(Detail);
--- doc/Templates/login/RecoverForm/index.js
+++
@@ -1,5 +0,0 @@
-import { withStyles } from "@material-ui/core";
-import styles from "./styles";
-import RecoverForm from "./RecoverForm";
-
-export default withStyles(styles, { name: "RecoverForm" })(RecoverForm);
