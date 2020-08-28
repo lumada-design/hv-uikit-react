f4b77a8a279b31fbff5e47f79825107e501138d3
Diogo Mariano <diogo.mariano@hitachivantara.com>
2020-08-28 14:34:01 +0100

    fix lint

--- packages/core/src/BulkActions/stories/BulkActions.stories.test.js
+++ packages/core/src/BulkActions/stories/BulkActions.stories.test.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { wait, screen, fireEvent } from "@testing-library/dom/dist/@testing-library/dom.umd";
 import React, { useState } from "react";
 
