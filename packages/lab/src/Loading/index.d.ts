d74bd092c74b5ed476e5ad495ed2bf826d2ade41
Diogo Mariano <diogo.mariano@hitachivantara.com>
2020-09-27 21:57:15 +0100

    feat: remove deprecated components from lab

--- packages/lab/src/Loading/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "./LoadingWithDelay";
-export * from "./LoadingWithDelay";
--- packages/lab/src/index.js
+++ packages/lab/src/index.js
@@ -1,8 +1,6 @@
 // components
 export { default as HvCodeEditor } from "./CodeEditor";
-export { default as HvFooter } from "./Footer";
 export { default as HvFormComposer } from "./FormComposer";
-export { default as HvLoading } from "./Loading";
 export { default as HvNavigationAnchors } from "./NavigationAnchors";
 export { default as HvNotificationPanel } from "./NotificationPanel";
 export { default as HvSlider } from "./Slider";
