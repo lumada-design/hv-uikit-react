9e038e64fa641e4f5b7df62416a04783e4ef712a
Bruno Henriques <bruno.henriques@hitachivantara.com>
2020-10-07 18:28:38 +0100

    fix(lab): add ts indexes. HVUIKIT-5471

--- packages/lab/src/index.js
+++ packages/lab/src/index.js
@@ -1,6 +1,8 @@
 // components
+export { default as HvAppSwitcherPanel } from "./AppSwitcherPanel";
 export { default as HvCodeEditor } from "./CodeEditor";
 export { default as HvFormComposer } from "./FormComposer";
 export { default as HvNavigationAnchors } from "./NavigationAnchors";
 export { default as HvNotificationPanel } from "./NotificationPanel";
 export { default as HvSlider } from "./Slider";
+export { default as HvTimePicker } from "./TimePicker";
