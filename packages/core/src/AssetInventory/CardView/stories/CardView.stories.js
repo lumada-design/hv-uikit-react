e10e5d162b65351c483f590630918db40d9aa607
Bruno Henriques <bruno.henriques@hitachivantara.com>
2020-10-27 12:49:01 +0000

    docs: move asset to components. HVUIKIT-5591

--- packages/core/src/AssetInventory/CardView/stories/CardView.stories.js
+++ packages/core/src/AssetInventory/CardView/stories/CardView.stories.js
@@ -17,7 +17,7 @@ import leaf from "../../../Card/stories/resources/leaf.png";
 /* eslint-disable react/prop-types */
 
 export default {
-  title: "Layout/Asset Inventory/Card View",
+  title: "Components/Asset Inventory/Card View",
   parameters: {
     componentSubtitle: null,
     usage: "import { HvCardView } from '@hv/uikit-react-core/dist'",
