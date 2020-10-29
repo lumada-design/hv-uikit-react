af7af97a10729b167ff5195fb271322620ac9069
Nelson Antunes <nelson.antunes@hitachivantara.com>
2020-10-29 18:54:54 +0000

    docs: fix typography and colors (#2100)

--- .storybook/blocks/DocsContainer.js
+++ .storybook/blocks/DocsContainer.js
@@ -6,10 +6,13 @@ import * as React from "react";
 import { DocsContainer } from "@storybook/addon-docs/blocks";
 import { Global } from "@storybook/theming";
 
+import HvProvider from "@hv/uikit-react-core/dist/Provider";
+
 import { getStoredTheme, getStorybookTheme, getDocsStylesOverrides } from "../themes";
 
 export default ({ context, children }) => {
-  const theme = getStorybookTheme(getStoredTheme());
+  const themeName = getStoredTheme();
+  const theme = getStorybookTheme(themeName);
 
   const docsContext = { ...context };
   docsContext.parameters = { ...context.parameters };
@@ -21,7 +24,10 @@ export default ({ context, children }) => {
   return (
     <>
       <Global styles={getDocsStylesOverrides(theme)} />
-      <DocsContainer context={docsContext}>{children}</DocsContainer>
+
+      <DocsContainer context={docsContext}>
+        <HvProvider uiKitTheme={themeName}>{children}</HvProvider>
+      </DocsContainer>
     </>
   );
 };
