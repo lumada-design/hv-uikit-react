a34fc9380acd2c247c38d2970ea02270476ce845
Nelson Antunes <nelson.antunes@hitachivantara.com>
2020-11-20 12:15:50 +0000

    test(applitools): change viewport and add edge browser

--- automation/applitools/applitools.config.js
+++ automation/applitools/applitools.config.js
@@ -1,16 +1,25 @@
 const isIncludedPath = (kind) => {
-  const includedPaths = ["Components/", "Forms/", "Tests/", "Visualizations/"];
+  const includedPaths = ["Components/", "Forms/", "Visualizations/", "Tests/"];
   return includedPaths.some((p) => kind.startsWith(p));
 };
 
 const isExcludedSample = (kind) => {
   const excludedSamples = [
+    // asset inventory stories excluded due inconsistent view port (applitools ticket 34169)
     "Components/Asset Inventory",
+
+    // dialog is opened and tested via Tests/Dialog
     "Components/Dialog",
+
+    // no way to take a stable screenshot of a loading animation
     "Components/Loading",
+
+    // plottly visualizations with axis rendering appear to produce small variations
+    // https://insightgroup.atlassian.net/browse/HVUIKIT-5448
     "Visualizations/Bar Chart",
     "Visualizations/Line Chart",
   ];
+
   return excludedSamples.some((p) => kind.includes(p));
 };
 
@@ -20,26 +29,17 @@ module.exports = {
   runInDocker: true,
   variations: () => ["theme:wicked"],
 
-  appName: "v3 pre-release",
+  appName: "UI Kit v3.x",
 
   browser: [
-    { width: 1920, height: 1080, name: "ie11" },
-    { width: 1920, height: 1080, name: "chrome" },
-    { width: 1920, height: 1080, name: "firefox" },
-    { width: 1920, height: 1080, name: "safari" },
-    // { width: 1920, height: 1080, name: "edgechromium" },
-
-    // { width: 1920, height: 1080, name: "chrome-one-versionsback" },
-    // { width: 1920, height: 1080, name: "firefox-one-version-back" },
-    // { width: 1920, height: 1080, name: "safari-one-version-back" },
-    // { width: 1920, height: 1080, name: "edgechromium-one-version-back" },
-
-    // { width: 1920, height: 1080, name: "chrome-two-versions-back" },
-    // { width: 1920, height: 1080, name: "firefox-two-versions-back" },
-    // { width: 1920, height: 1080, name: "safari-two-versions-back" }
+    { width: 1024, height: 768, name: "ie11" },
+    { width: 1024, height: 768, name: "chrome" },
+    { width: 1024, height: 768, name: "firefox" },
+    { width: 1024, height: 768, name: "safari" },
+    { width: 1024, height: 768, name: "edgechromium" },
   ],
-  // asset inventory stories excluded due inconsistent view port (applitools ticket 34169)
 
-  include: ({ name, kind, parameters }) => isIncludedPath(kind) && !isExcludedSample(kind),
+  include: ({ kind }) => isIncludedPath(kind) && !isExcludedSample(kind),
+
   concurrency: 10,
 };
