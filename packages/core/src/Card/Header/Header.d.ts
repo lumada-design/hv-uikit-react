06da25fa0d90799c5c4e7e631ca48c1e0c24f7bc
Tomoto Washio <57977241+tomoto-vantara@users.noreply.github.com>
2020-11-25 02:57:56 -0800

    fix(card): workaround type issue of HvCardHeader.title #2163

--- packages/core/src/Card/Header/Header.d.ts
+++ packages/core/src/Card/Header/Header.d.ts
@@ -9,11 +9,17 @@ export type HvCardHeaderClassKey =
   | "action"
   | "content";
 
-export interface HvCardHeaderProps extends StandardProps<CardHeaderProps, HvCardHeaderClassKey> {
+export interface HvCardHeaderProps extends StandardProps<CardHeaderProps, HvCardHeaderClassKey, 'title'> {
   /**
    *  The renderable content inside the icon slot of the header.
    */
   icon?: React.ReactNode;
+
+  /**
+   * The renderable content inside the title slot of the header.
+   * (explicitly redeclared here to workaround the type inference issue)
+   */
+  title?: React.ReactNode;
 }
 
 export default function HvCardHeader(props: HvCardHeaderProps): JSX.Element | null;
