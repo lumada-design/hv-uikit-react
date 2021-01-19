e240965d2ebab91f0fa7641c363c252b196a7a7e
Julio Costa <julio.costa@hitachivantara.com>
2021-01-19 16:15:16 +0000

    feat(list): have selection with multi selection using shift. HVUIKIT-5677

--- packages/core/src/ListContainer/stories/ListContainer.stories.js
+++ packages/core/src/ListContainer/stories/ListContainer.stories.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { withStyles } from "@material-ui/core";
 import { DropRightXS, Calendar, LineChart, Machine, Plane, User } from "@hv/uikit-react-icons";
@@ -204,6 +204,116 @@ export const WithTitle = () => {
         <HvListItem>Delete</HvListItem>
         <HvListItem>
           <StyledHvLink route="https://www.hitachivantara.com">Update</StyledHvLink>
+        </HvListItem>
+      </HvListContainer>
+    </HvPanel>
+  );
+};
+
+export const MultiSelectionWithShift = () => {
+  const initialSelection = {
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+  };
+
+  const [selectionAnchor, setSelectionAnchor] = useState();
+  const [selectedItems, setSelectedItems] = useState({ ...initialSelection });
+
+  useEffect(() => {
+    const existingSelections = Object.entries(selectedItems).filter((item) => {
+      return item[1] === true;
+    })[0];
+
+    if (existingSelections === undefined) {
+      setSelectionAnchor(null);
+    }
+  }, [selectedItems, selectionAnchor]);
+
+  const handleListItemClick = (_evt, index) => {
+    if (_evt.shiftKey || _evt.ctrlKey) {
+      let selectionStart;
+      let selectionEnd;
+
+      if (selectionAnchor === null) {
+        setSelectionAnchor(index);
+        selectionStart = index;
+      } else {
+        selectionStart = selectionAnchor;
+      }
+
+      selectionEnd = selectionStart === index ? index + 1 : index + 1;
+
+      if (selectionStart >= selectionEnd) {
+        const originalSelectionStart = selectionStart;
+        selectionStart = index;
+        selectionEnd = originalSelectionStart + 1;
+      }
+
+      const itemsToFlip = Object.entries(initialSelection).slice(selectionStart, selectionEnd);
+      const updatedArray = itemsToFlip.map((item) => {
+        const updatedEntries = [item[0], !item[1]];
+        return updatedEntries;
+      });
+
+      setSelectedItems({
+        ...initialSelection,
+        ...Object.fromEntries(updatedArray),
+      });
+    } else {
+      setSelectionAnchor(index);
+      setSelectedItems(() => {
+        return {
+          ...initialSelection,
+          [index]: !selectedItems[index],
+        };
+      });
+    }
+  };
+  return (
+    <HvPanel m="10px" style={{ float: "left" }}>
+      <HvListContainer interactive multiSelect condensed aria-label="Stores" role="listbox">
+        <HvListItem
+          onClick={(event) => handleListItemClick(event, 0)}
+          selected={selectedItems[0]}
+          role="option"
+          style={{ "user-select": "none" }}
+        >
+          98001, Store Manager
+        </HvListItem>
+        <HvListItem
+          onClick={(event) => handleListItemClick(event, 1)}
+          selected={selectedItems[1]}
+          role="option"
+          style={{ "user-select": "none" }}
+        >
+          98002, Store Manager
+        </HvListItem>
+        <HvListItem
+          onClick={(event) => handleListItemClick(event, 2)}
+          selected={selectedItems[2]}
+          role="option"
+          style={{ "user-select": "none" }}
+        >
+          98003, Store Manager
+        </HvListItem>
+        <HvListItem
+          onClick={(event) => handleListItemClick(event, 3)}
+          selected={selectedItems[3]}
+          role="option"
+          style={{ "user-select": "none" }}
+        >
+          98004, Store Manager
+        </HvListItem>
+        <HvListItem
+          onClick={(event) => handleListItemClick(event, 4)}
+          selected={selectedItems[4]}
+          role="option"
+          style={{ "user-select": "none" }}
+        >
+          98005, Store Manager
         </HvListItem>
       </HvListContainer>
     </HvPanel>
