e1469b8a03d2793b540be433ef7ec2c4a49d5669
Bruno Henriques <bruno.henriques@hitachivantara.com>
2021-02-04 11:25:47 +0000

    fix(dialog): add indentcontent declaration. HVUIKIT-5688

--- packages/core/src/Dialog/DialogContent/DialogContent.d.ts
+++ packages/core/src/Dialog/DialogContent/DialogContent.d.ts
@@ -2,6 +2,12 @@ import { DialogContentProps, StandardProps } from "@material-ui/core";
 
 export type HvDialogContentClassKey = "root" | "textContent";
 
-export type HvDialogContentProps = StandardProps<DialogContentProps, HvDialogContentClassKey>;
+export interface HvDialogContentProps
+  extends StandardProps<DialogContentProps, HvDialogContentClassKey> {
+  /**
+   * Content should be indented in relationship to the Dialog title.
+   */
+  indentContent: boolean;
+}
 
 export default function HvDialogContent(props: HvDialogContentProps): JSX.Element | null;
