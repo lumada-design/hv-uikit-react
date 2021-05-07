3e45b93600c30bf5bb16cb0b7b4cd6c884e4c588
Nelson Antunes <nelson.antunes@hitachivantara.com>
2021-05-07 17:18:53 +0100

    feat(time-picker): support external error message HVUIKIT-5772

--- packages/lab/src/TimePicker/TimePicker.d.ts
+++ packages/lab/src/TimePicker/TimePicker.d.ts
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { StandardProps } from "@material-ui/core";
 import { HvFormElementProps, HvBaseDropdownProps } from "@hv/uikit-react-core";
 
@@ -32,53 +31,9 @@ export interface HvTimePickerProps
   id?: string;
 
   /**
-   * The form element name.
-   */
-  name?: string;
-
-  /**
-   * The value of the form element.
-   *
-   * When defined the value state becomes controlled.
-   */
-  value?: any;
-  /**
    * When uncontrolled, defines the initial value.
    */
   defaultValue?: any;
-
-  /**
-   * Indicates that user input is required on the form element.
-   */
-
-  required?: boolean;
-
-  /**
-   * Indicates that user input is disabled on the form element.
-   */
-
-  disabled?: boolean;
-
-  /**
-   * The label of the form element.
-   *
-   * The form element must be labeled for accessibility reasons.
-   * If not provided, an aria-label or aria-labelledby must be provided instead.
-   */
-  label?: string;
-  /**
-   * The status of the form element.
-   *
-   * Valid is correct, invalid is incorrect and standBy means no validations have run.
-   *
-   * When uncontrolled and unspecified it will default to "standBy" and change to either "valid"
-   * or "invalid" after any change to the state.
-   */
-  status?: "standBy" | "valid" | "invalid";
-  /**
-   * The error message to show when `status` is "invalid". Defaults to "Required".
-   */
-  statusMessage?: React.ReactNode;
 
   /**
    * If the time should be presented in 12 or 24 hour format.
@@ -124,11 +79,6 @@ export interface HvTimePickerProps
    * It is always invoked with the hours in a 24h format
    */
   onChange?: (timeIn24Format: object) => void;
-
-  /**
-   * Provide additional descriptive text for the form element.
-   */
-  description?: React.ReactNode;
 
   /**
    * Disable the portal behavior. The children stay within it's parent DOM hierarchy.
