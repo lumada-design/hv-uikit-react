f90e4bd338cbfd9bc4d4d4549e0d5de201b727a9
Francisco Guilherme <francisco.guilherme@hitachivantara.com>
2021-05-07 12:31:28 +0100

    fix(list): fix showing unneeded tooltips. HVUIKIT-5767

--- packages/core/src/withTooltip.js
+++ packages/core/src/withTooltip.js
@@ -1,30 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "./Typography";
 import Tooltip from "./Tooltip";
 
-const withTooltip = (Component, label = "", placement, tooltipProps, tooltipContainerProps) => (
-  props
-) => {
-  const styles = {
-    truncate: {
-      overflow: "hidden",
-      textOverflow: "ellipsis",
-      whiteSpace: "nowrap",
-    },
-  };
+const styles = {
+  truncate: {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+  },
+};
+
+const withTooltip = (
+  Component,
+  label = "",
+  placement,
+  hideTooltip,
+  tooltipProps,
+  tooltipContainerProps
+) => (props) => {
+  const [isHoverDisabled, setIsHoverDisabled] = useState(false);
 
   const title = <Typography>{label}</Typography>;
+
+  const handleMouseEnter = (evt) => {
+    const isHidden = hideTooltip?.(evt);
+    setIsHoverDisabled(isHidden);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHoverDisabled(false);
+  };
 
   return (
     <Tooltip
       style={{ ...styles.truncate }}
+      disableHoverListener={isHoverDisabled}
       disableFocusListener
       disableTouchListener
       title={title}
       placement={placement}
       {...tooltipProps}
     >
-      <div {...tooltipContainerProps}>
+      <div
+        {...tooltipContainerProps}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <Component {...props} />
       </div>
     </Tooltip>
