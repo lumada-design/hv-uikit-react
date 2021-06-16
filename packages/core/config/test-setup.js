65eaecfd221de75e037e0898e38d857d8a49adc3
Julio Costa <julio.costa@webdetails.pt>
2021-06-16 10:01:53 +0100

    feat: added support for react 17 HVUIKIT-5618 (#2449)
    Fixes HVUIKIT-5796
    
    Co-authored-by: Julio Costa <julio.costa@hitachivantara.com>

--- packages/core/config/test-setup.js
+++ packages/core/config/test-setup.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 import "@testing-library/jest-dom";
 
