94fb3613f183ab3b29816083b15869f3bca9eda6
Diogo Mariano <diogo.mariano@hitachivantara.com>
2022-03-31 10:35:41 +0100

    test: update jest test configs

--- packages/core/config/jest-config.js
+++ packages/core/config/jest-config.js
@@ -21,6 +21,8 @@ module.exports = {
     ".*\\.(css|less|styl|scss|sass)$": "<rootDir>/config/jest-mocks/cssModule.js",
     ".*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/config/jest-mocks/image.js",
+    "^react($|/.+)": "<rootDir>/node_modules/react$1",
+    "^@material-ui($|/.+)": "<rootDir>/node_modules/@material-ui$1",
     "^@hitachivantara/uikit-react-icons$": "<rootDir>/../icons/bin",
     "^@hitachivantara/uikit-react-icons/dist/(.*)$": "<rootDir>/../icons/bin/$1",
     "^@hitachivantara/uikit-common-themes/dist/(.*)$": "<rootDir>/../themes/src/$1",
