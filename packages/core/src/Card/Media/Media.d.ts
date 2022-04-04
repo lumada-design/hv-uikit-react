aa0c58519546313111817323573642fced22ecb4
Hyorran Preigschadt <phyorran@gmail.com>
2022-04-04 15:30:53 +0100

    fix(HvCardMedia): fixed props composition within the base component props

--- packages/core/src/Card/Media/Media.d.ts
+++ packages/core/src/Card/Media/Media.d.ts
@@ -2,6 +2,6 @@ import { CardMediaProps, StandardProps } from "@material-ui/core";
 
 export type HvCardMediaClassKey = "root" | "media";
 
-export type HvCardMediaProps = StandardProps<CardMediaProps, HvCardMediaClassKey>;
+export type HvCardMediaProps = StandardProps<CardMediaProps<"div", { component?: "div" }>, HvCardMediaClassKey>;
 
 export default function HvCardMedia(props: HvCardMediaProps): JSX.Element | null;
