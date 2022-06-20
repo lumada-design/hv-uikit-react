138b4709b0981746254051f493b8d2570e18bedb
Marcos Tavares da Silva Larsen <mlarsen@pentaho.com>
2022-06-20 11:20:25 +0100

    Update index.js

--- .github/codedx-analysis/index.js
+++ .github/codedx-analysis/index.js
@@ -69,6 +69,8 @@ async function main() {
 
   const form = new FormData();
   form.append("file", fs.createReadStream(filePath));
+  form.append("includeGitSource", true);
+  form.append("gitBranchName", branchName);
   form.append("branchName", branchName);
 
   axios
