8d7eaa44a4e93f53730e1cf8c7433d5dc902165a
Paulo Lagoá <paulo.lagoa@@hitachivantara.com>
2022-11-28 16:42:29 +0000

    feat(Loading): add Loading component

---
+++ packages/core/src/components/Loading/Loading.tsx
@@ -0,0 +1,66 @@
+import { theme } from "@hitachivantara/uikit-styles";
+import clsx from "clsx";
+import range from "lodash/range";
+import {
+  StyledBar,
+  StyledBarContainer,
+  StyledLabel,
+  StyledRoot,
+} from "./Loading.Styles";
+
+export interface LoadingProps extends BaseProps<HTMLDivElement> {
+  /** Indicates if the component should be render in a small size. */
+  small?: boolean;
+  /** The label to be displayed.  */
+  label?: string | React.ReactNode;
+  /** Whether the loading animation is hidden. */
+  hidden?: boolean;
+  /** Color applied to the bars. */
+  color?: string;
+  classes?: {
+    root?: string;
+    barContainer?: string;
+    loadingBar?: string;
+    label?: string;
+    overlay?: string;
+    blur?: string;
+    hidden?: string;
+  };
+}
+
+/**
+ * Loading provides feedback about a process that is taking place in the application.
+ */
+export const Loading = (props: LoadingProps) => {
+  const { color, hidden, small, label, classes, className, ...others } = props;
+
+  const getColor = (colorName) => {
+    if (color) console.log(color, colorName, theme.colors[color]);
+    return color ? theme.colors[color] || color : theme.colors[colorName];
+  };
+
+  const size = small ? "small" : "regular";
+  const colorVariant = color ? "Color" : "";
+  const variant = `${size}${colorVariant}`;
+
+  const inline = { backgroundColor: getColor(small ? "acce1" : "acce3") };
+  return (
+    <StyledRoot
+      hidden={!!hidden}
+      className={clsx(className, classes?.root, "hidden")}
+      {...others}
+    >
+      <StyledBarContainer>
+        {range(0, 3).map((e) => (
+          <StyledBar
+            key={e}
+            style={inline}
+            className={clsx(classes?.loadingBar, variant)}
+            variant={variant}
+          ></StyledBar>
+        ))}
+      </StyledBarContainer>
+      {label && <StyledLabel variant="caption1">{label}</StyledLabel>}
+    </StyledRoot>
+  );
+};
