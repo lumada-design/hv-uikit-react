c866d8181dba2ba4993e0e243e96942bee65dba2
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2022-12-05 18:07:52 +0000

    feat(Footer): add footer component (#2997)
    Co-authored-by: Paulo Lagoá <paulo.lagoa@@hitachivantara.com>

---
+++ packages/core/src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Typography } from "../";
+import { Footer, FooterProps } from "./Footer";
+
+const meta: Meta<typeof Footer> = {
+  title: "Structure/Footer",
+  component: Footer,
+};
+export default meta;
+
+export const Main: StoryObj<FooterProps> = {
+  args: {},
+  argTypes: {
+    classes: { control: { disable: true } },
+  },
+  render: (args) => {
+    return <Footer {...args} />;
+  },
+};
+
+export const CustomLabels: StoryObj<FooterProps> = {
+  args: {
+    name: "Hitachi Vantara",
+    copyright: "© Hitachi Vantara Corporation 2022",
+    links: (
+      <Typography variant="label" style={{ color: "#2064B4" }}>
+        <a href="https://www.hitachivantara.com" target="_blank">
+          License information
+        </a>
+      </Typography>
+    ),
+  },
+  argTypes: {
+    classes: { control: { disable: true } },
+  },
+  render: (args) => {
+    return <Footer {...args} />;
+  },
+};
