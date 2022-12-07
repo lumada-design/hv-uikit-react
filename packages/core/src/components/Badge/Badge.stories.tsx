ee3a9100c0f69ffa198877a159987e843ec37122
Paulo Lagoá <paulo.lagoa@@hitachivantara.com>
2022-12-07 14:16:27 +0000

    feat(Badge): add Badge component

---
+++ packages/core/src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,180 @@
+import { useState } from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { Alert } from "@hitachivantara/uikit-icons";
+import { Badge, BadgeProps } from "./Badge";
+import { Button } from "..";
+
+const container = {
+  width: 400,
+  display: "flex",
+  justifyContent: "space-between",
+};
+
+const meta: Meta<typeof Badge> = {
+  title: "Display/Badge",
+  component: Badge,
+};
+export default meta;
+
+export const Main: StoryObj<BadgeProps> = {
+  args: {
+    count: 1,
+    showCount: false,
+    label: "",
+    maxCount: 99,
+    text: "",
+    textVariant: "",
+  },
+  argTypes: {
+    classes: { control: { disable: true } },
+    icon: { control: { disable: true } },
+  },
+  render: (args) => {
+    return <Badge {...args} />;
+  },
+};
+
+export const Multiple: StoryObj<BadgeProps> = {
+  argTypes: {
+    classes: { control: { disable: true } },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ ...container, width: 500 }}>
+        <Story />
+      </div>
+    ),
+  ],
+  render: () => {
+    return (
+      <>
+        <Badge id="badge1" count={1} />
+        <Badge id="badge2" showCount count={8} />
+        <Badge id="badge3" showCount count={22} />
+        <Badge id="badge4" showCount count={100} />
+        <Badge id="labelBadge1" label="100%" />
+      </>
+    );
+  },
+};
+
+export const WithIcon: StoryObj<BadgeProps> = {
+  argTypes: {
+    classes: { control: { disable: true } },
+  },
+  parameters: {
+    docs: {
+      description: { story: "Badge sample that uses a custom icon." },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ ...container, width: 500 }}>
+        <Story />
+      </div>
+    ),
+  ],
+  render: () => {
+    return (
+      <>
+        <Badge id="badge5" count={0} icon={<Alert />} />
+        <Badge id="badge6" count={1} icon={<Alert />} />
+        <Badge id="badge7" showCount count={8} icon={<Alert />} />
+        <Badge id="badge8" showCount count={88} icon={<Alert />} />
+        <Badge id="badge9" showCount count={888} icon={<Alert />} />
+        <Badge id="labelBadge3" label="100%" icon={<Alert />} />
+      </>
+    );
+  },
+};
+
+export const WithText: StoryObj<BadgeProps> = {
+  argTypes: {
+    classes: { control: { disable: true } },
+  },
+  parameters: {
+    docs: {
+      description: { story: "Badge sample using only text." },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ ...container, width: 650 }}>
+        <Story />
+      </div>
+    ),
+  ],
+  render: () => {
+    return (
+      <>
+        <Badge id="badge10" count={0} text="Events" textVariant="title3" />
+        <Badge id="badge11" count={1} text="Events" textVariant="title3" />
+        <Badge
+          id="badge12"
+          showCount
+          count={8}
+          text="Events"
+          textVariant="title2"
+        />
+        <Badge
+          id="badge13"
+          showCount
+          count={88}
+          text="Events"
+          textVariant="title3"
+        />
+        <Badge
+          id="badge14"
+          showCount
+          count={888}
+          text="Events"
+          textVariant="title3"
+        />
+        <Badge
+          id="labelBadge5"
+          label="100%"
+          text="Events"
+          textVariant="title3"
+        />
+      </>
+    );
+  },
+};
+
+export const WithState: StoryObj<BadgeProps> = {
+  argTypes: {
+    classes: { control: { disable: true } },
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "Badge sample using react hooks to set the number of events.",
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ ...container, width: 650 }}>
+        <Story />
+      </div>
+    ),
+  ],
+  render: () => {
+    const [count, setCount] = useState(1);
+    const addCount = () => setCount(count * 2);
+
+    return (
+      <>
+        <Button onClick={addCount}>Double Value</Button>
+        <p />
+        <Badge
+          id="badge15"
+          showCount
+          count={count}
+          text="Events"
+          textVariant="sTitle"
+        />
+      </>
+    );
+  },
+};
---
+++ packages/core/src/components/Badge/Badge.tsx
@@ -0,0 +1,90 @@
+import { StyledRoot, StyledContainer, StyledBadge } from "./Badge.styles";
+import { Typography } from "..";
+
+export interface BadgeProps extends BaseProps {
+  /**
+   * Count is the number of unread notifications.
+   * Note count and label are mutually exclusive.
+   * count is ignored when label is specified at the same time.
+   */
+  count?: number;
+  /**
+   * True if count should be displayed.
+   * Note showCount and label are mutually exclusive.
+   * showCount is ignored when label is specified at the same time.
+   */
+  showCount?: boolean;
+  /** The maximum number of unread notifications to be displayed */
+  maxCount?: number;
+  /**
+   * Custom text to show in place of count.
+   * Note showCount and label are mutually exclusive.
+   * showCount is ignored when label is specified at the same time.
+   */
+  label?: string;
+  /** Icon which the notification will be attached. */
+  icon?: React.ReactNode;
+  /** Text which the notification will be attached. */
+  text?: string;
+  /** Text variant. */
+  textVariant?: string;
+  /** A Jss Object used to override or extend the styles applied to the empty state component. */
+  classes?: {
+    root?: string;
+    badgeContainer?: string;
+    badgePosition?: string;
+  };
+}
+
+/**
+ * The badge is a component used to notify the user that something has occurred, in the app context.
+ */
+export const Badge = (props: BadgeProps) => {
+  const {
+    classes,
+    className,
+    showCount = false,
+    count = 0,
+    maxCount = 99,
+    label = null,
+    icon = null,
+    text = null,
+    textVariant = null,
+    ...others
+  } = props;
+
+  const renderedCount = count > maxCount ? `${maxCount}+` : count;
+  // If label is specified and non-empty, render it.
+  // If showCount is specified and count > 0, render the count.
+  // Otherwise, render nothing on the badge.
+  // (Note count=0 should not be rendered to avoid ghosty 0.)
+  const renderedCountOrLabel =
+    label || (showCount && count > 0 && renderedCount) || null;
+  const Component =
+    icon || (text && <Typography variant={textVariant}>{text}</Typography>);
+
+  return (
+    <StyledRoot
+      aria-label={renderedCountOrLabel}
+      className={classes?.root}
+      {...others}
+    >
+      {Component}
+      <StyledContainer
+        component={Component ? true : false}
+        className={Component ? classes?.badgeContainer : null}
+      >
+        <StyledBadge
+          className={classes?.badgePosition}
+          badge={count > 0 || renderedCountOrLabel}
+          showCount={!label && renderedCountOrLabel}
+          showLabel={label}
+          icon={icon}
+          badgeOneDigit={String(renderedCountOrLabel).length === 1}
+        >
+          {renderedCountOrLabel}
+        </StyledBadge>
+      </StyledContainer>
+    </StyledRoot>
+  );
+};
