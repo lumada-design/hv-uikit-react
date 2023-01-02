fbf5c266f79ba4f1a700f5449d5579f435d3e697
Henrique Raposo <henrique.raposo@hitachivantara.com>
2023-01-02 10:47:27 +0000

    feat(simple-grid): add simple grid component #HVUIKIT-6528 (#3031)

---
+++ packages/core/src/components/SimpleGrid/SimpleGrid.styles.tsx
@@ -0,0 +1,65 @@
+import styled from "@emotion/styled";
+import { theme } from "@hitachivantara/uikit-styles";
+import { Spacing } from "./SimpleGrid";
+
+function size(props) {
+  if (typeof props.size === "number") {
+    return props.size;
+  }
+
+  return props.sizes[props.size] || props.size || props.sizes.md;
+}
+
+function getSortedBreakpoints(breakpoints) {
+  if (breakpoints.length === 0) {
+    return breakpoints;
+  }
+
+  const property = "maxWidth" in breakpoints[0] ? "maxWidth" : "minWidth";
+  const sorted = [...breakpoints].sort(
+    (a, b) =>
+      size({ size: b[property], sizes: theme.breakpoints }) -
+      size({ size: a[property], sizes: theme.breakpoints })
+  );
+
+  return property === "minWidth" ? sorted.reverse() : sorted;
+}
+
+export const StyledContainer = styled("div")(
+  ({
+    breakpoints,
+    spacing,
+    cols,
+  }: {
+    breakpoints?: any;
+    spacing: Spacing;
+    cols?: number;
+  }) => ({
+    boxSizing: "border-box",
+    display: "grid",
+    gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))`,
+    gap: theme.space[spacing],
+    ...(breakpoints &&
+      getSortedBreakpoints(breakpoints).reduce((acc, breakpoint) => {
+        const property = "maxWidth" in breakpoint ? "max-width" : "min-width";
+        const breakpointSize = size({
+          size:
+            property === "max-width"
+              ? breakpoint.maxWidth
+              : breakpoint.minWidth,
+          sizes: theme.breakpoints,
+        });
+
+        acc[
+          `@media (${property}: ${
+            breakpointSize + (property === "max-width" ? 0 : 1)
+          }px)`
+        ] = {
+          gridTemplateColumns: `repeat(${breakpoint.cols}, minmax(0, 1fr))`,
+          gap: theme.space[spacing],
+        };
+
+        return acc;
+      }, {})),
+  })
+);
---
+++ packages/core/src/components/SimpleGrid/SimpleGrid.tsx
@@ -0,0 +1,54 @@
+import { StyledContainer } from "./SimpleGrid.styles";
+
+export const SimpleGrid = ({
+  children,
+  breakpoints,
+  spacing = "sm",
+  cols,
+  ...others
+}: SimpleGridProps) => {
+  return (
+    <StyledContainer
+      spacing={spacing}
+      cols={cols}
+      breakpoints={breakpoints}
+      {...others}
+    >
+      {children}
+    </StyledContainer>
+  );
+};
+
+export type Spacing = "sm" | "md" | "lg" | "xl";
+
+export type Breakpoint = {
+  cols?: number;
+  maxWidth?: number;
+  minWidth?: number;
+  spacing?: Spacing;
+};
+
+export type SimpleGridProps = {
+  /**
+   * -
+   */
+  children: JSX.Element | JSX.Element[];
+  /**
+   * Spacing with pre-defined values according the values defined in the theme
+   */
+  spacing?: Spacing;
+  /**
+   * Provide an array to define responsive behavior:
+   *
+   *    maxWidth or minWidth: max-width or min-width at which media query will work
+   *
+   *    cols: number of columns per row at given max-width
+   *
+   *    spacing: optional spacing at given max-width, if not provided spacing from component prop will be used instead
+   */
+  breakpoints?: Breakpoint[];
+  /**
+   * Number of how many columns the content will be displayed
+   */
+  cols?: number;
+};
