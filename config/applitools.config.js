163acbd06e87c5ab1d58f637b73f24d1e04dfa29
francisco-guilherme <francisco.guilherme@hitachivantara.com>
2023-02-06 17:30:31 +0000

    chore: workflows setup

---
+++ config/applitools.config.js
@@ -0,0 +1,28 @@
+const isExcludedPath = (kind) => {
+  const excludePaths = ["Templates/"];
+
+  return excludePaths.some((p) => kind.includes(p));
+};
+
+module.exports = {
+  matchLevel: "Strict",
+
+  puppeteerOptions: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    ignoreHTTPSErrors: true,
+  },
+
+  appName: "Next UI Kit",
+  batchId: process.env.APPLITOOLS_BATCH_ID,
+
+  browser: [
+    { width: 1024, height: 768, name: "chrome" },
+    { width: 1024, height: 768, name: "firefox" },
+    { width: 1024, height: 768, name: "safari" },
+    { width: 1024, height: 768, name: "edgechromium" },
+  ],
+
+  include: ({ kind }) => !isExcludedPath(kind),
+
+  concurrency: 10,
+};
