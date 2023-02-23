c5bd1b5b1996526660745d97247b6242330f597e
André Rua <andre.rua@hitachivantara.com>
2023-02-23 19:11:52 +0000

    fix: corrected ts typings for VerticalNavigation

--- packages/lab/src/VerticalNavigation/VerticalNavigation.d.ts
+++ packages/lab/src/VerticalNavigation/VerticalNavigation.d.ts
@@ -2,25 +2,25 @@ import { NavigationData } from "@hitachivantara/uikit-react-core";
 
 export interface HvVerticalNavigationProps {
   /**
-   * Id to be applied to the root node of the panel.
-   */
-  id?: string;
-  /**
    * Called when a menu item is clicked.
    */
   onNavigationChange?: Function;
+  /**
+   * Called the collpase / expand button is clicked.
+   */
+  onToggleExpanded?: Function;
   /**
    * An array containing the data for each menu item.
    */
   data: NavigationData[];
   /**
-   * Text to be displayed in the collpase area when the panel is expanded.
-   */
-  collapseLabel?: string;
-  /**
    * The ID of the selected page.
    */
   selected?: string;
+  /**
+   * Flag that sets the panel to expanded / collapsed.
+   */
+  expanded: boolean;
   /**
    * The top value where the panel will be rendered. Default is 44 as it is the height of the Header component.
    */
@@ -33,6 +33,10 @@ export interface HvVerticalNavigationProps {
    * The width of the panel when collapsed. Default vlaue is 52.
    */
   collapsedPanelWidth?: number;
+  /**
+   * Sets the Css position of the panel. Default value is `sticked`
+   */
+  position?: "static" | "relative" | "fixed" | "absolute" | "sticky";
 }
 
 export default function HvVerticalNavigation(props: HvVerticalNavigationProps): JSX.Element | null;
