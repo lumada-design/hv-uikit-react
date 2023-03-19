06da8c808bbac6559f5eaeb1ed3d06ace7dae603
francisco-guilherme <francisco.guilherme@hitachivantara.com>
2023-03-19 22:09:01 +0000

    chore: tmp fix imports to unblock tests

--- packages/core/src/hocs/withTooltip.tsx
+++ packages/core/src/hocs/withTooltip.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { HvTypography, HvTooltip } from "components";
+import { HvTypography } from "components/Typography";
+import { HvTooltip } from "components/Tooltip";
 
 const styles = {
   truncate: {
