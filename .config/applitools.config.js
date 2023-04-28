63ff8903ae2b0838f6fb15f3e3e81556fb60ad90
francisco-guilherme <francisco.guilherme@hitachivantara.com>
2023-04-28 12:32:28 +0100

    chore: set applitools app name and baseline branch

--- .config/applitools.config.js
+++ .config/applitools.config.js
@@ -14,7 +14,7 @@ module.exports = {
 
   appName: process.env.APPLITOOLS_APP_NAME,
   batchName: process.env.APPLITOOLS_BATCH_ID,
-  branchName: process.env.APPLITOOLS_BRANCH_NAME,
+  baselineBranchName: process.env.APPLITOOLS_BASELINE_BRANCH_NAME,
 
   browser: [
     { width: 1024, height: 768, name: "chrome" },
