ace8f2936ee6e82243e9908de09b218d82e83458
Andre Fernando Rua <andre.rua@hitachivantara.com>
2023-05-18 16:05:21 +0100

    fix(VerticalNavigation): added appropriate z-index value to the popup (#3347)

--- packages/core/src/components/VerticalNavigation/NavigationPopup/NavigationPopup.styles.tsx
+++ packages/core/src/components/VerticalNavigation/NavigationPopup/NavigationPopup.styles.tsx
@@ -1,7 +1,14 @@
 import styled from "@emotion/styled";
 
 import { theme } from "@hitachivantara/uikit-styles";
+import { Popper } from "@mui/base";
 
-export const StyledPopupContainer = styled("div")({
+const StyledPopupContainer = styled("div")({
   marginLeft: theme.spacing("xs"),
 });
+
+const StyledPopper = styled(Popper)({
+  zIndex: theme.zIndices.popover,
+});
+
+export { StyledPopupContainer, StyledPopper };
