429a7cf2160c659bf92ac314412a07efddd29ceb
Bruno <zettca@users.noreply.github.com>
2023-05-19 17:51:20 +0100

    test: improve test setup
    add some missing jsdom mocks
    remove unnecessary cleanup

--- .config/test.setup.ts
+++ .config/test.setup.ts
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
-import { cleanup, render } from "@testing-library/react";
-import { beforeEach, afterEach } from "vitest";
+import { render } from "@testing-library/react";
+import { beforeAll, beforeEach, vi } from "vitest";
 
 const customRender = (ui: React.ReactElement, options = {}) =>
   render(ui, {
@@ -9,11 +9,32 @@ const customRender = (ui: React.ReactElement, options = {}) =>
     ...options,
   });
 
-beforeEach(async (context) => {
+beforeEach(async (context: any) => {
   // extend context
   context.render = customRender;
 });
 
-afterEach(() => {
-  cleanup();
+// Adds missing JSDOM implementations
+beforeAll(() => {
+  class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+
+  window.ResizeObserver = ResizeObserver;
+
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // Deprecated
+      removeListener: vi.fn(), // Deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
 });
--- packages/core/src/components/OverflowTooltip/OverflowTooltip.test.tsx
+++ packages/core/src/components/OverflowTooltip/OverflowTooltip.test.tsx
@@ -1,25 +1,8 @@
 import { render } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { HvOverflowTooltip } from "./OverflowTooltip";
 
 describe("OverflowTooltip", () => {
-  const { ResizeObserver } = window;
-
-  beforeEach(() => {
-    // @ts-ignore
-    delete window.ResizeObserver;
-    window.ResizeObserver = vi.fn().mockImplementation(() => ({
-      observe: vi.fn(),
-      unobserve: vi.fn(),
-      disconnect: vi.fn(),
-    }));
-  });
-
-  afterEach(() => {
-    window.ResizeObserver = ResizeObserver;
-    vi.restoreAllMocks();
-  });
-
   it("should be defined", () => {
     const { container } = render(<HvOverflowTooltip data="" />);
     expect(container).toBeDefined();
