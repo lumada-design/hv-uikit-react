62bdff3f56eac9789712b6bae0f6587c83b4766d
Paulo Lagoá <paulo.lagoa@@hitachivantara.com>
2023-06-05 16:41:37 +0100

    chore: fix font sizes section and small refactor

--- app/src/generator/FontSizes/FontSizes.tsx
+++ app/src/generator/FontSizes/FontSizes.tsx
@@ -1,15 +1,16 @@
 import { useContext, useEffect, useState } from "react";
 import {
+  theme,
   useTheme,
-  HvInput,
   createTheme,
-  HvTypography,
-  HvDropdown,
   HvBox,
+  HvDropdown,
   HvListValue,
 } from "@hitachivantara/uikit-react-core";
 import { GeneratorContext } from "generator/GeneratorContext";
 import { css } from "@emotion/css";
+import { FontSize } from "components/common";
+import { extractFontSizeUnit } from "generator/utils";
 import { styles } from "./FontSizes.styles";
 
 const FontSizes = () => {
@@ -17,7 +18,7 @@ const FontSizes = () => {
   const { customTheme, updateCustomTheme } = useContext(GeneratorContext);
   const [fontSizes, setFontSizes] = useState<HvListValue[]>([]);
   const [fontSize, setFontSize] = useState(""); // base, sm, ...
-  const [fontValue, setFontValue] = useState<number>(0); // 14, 16, ...
+  const [fontValue, setFontValue] = useState<number>(); // 14, 16, ...
   const [unit, setUnit] = useState("px"); // px, rem, ...
   const [currSizes, setCurrSizes] = useState<Map<string, string>>(
     new Map<string, string>()
@@ -37,89 +38,90 @@ const FontSizes = () => {
     const currSize = currSizes.get(size);
     setFontSize(size);
     if (currSize) {
-      const value = Number(currSize);
+      const value = parseFloat(currSize);
       setUnit(currSize.replace(value.toString(), ""));
       setFontValue(value);
     } else {
-      setUnit("px");
-      setFontValue(Number(activeTheme?.fontSizes[size]));
+      const f = activeTheme?.fontSizes[size];
+      const u = extractFontSizeUnit(f);
+      setFontValue(parseFloat(f));
+      setUnit(u);
     }
   };
-
-  const onValueChangedHandler = (event, value) => {
+  const onValueChangeHandler = (value) => {
     setFontValue(value);
+  };
 
+  const onValueSetHandler = (value) => {
     // check updated font sizes
     const currFontSizes = {};
     for (const [key, val] of currSizes.entries()) {
       currFontSizes[key] = val;
     }
 
+    const fixedValue = value.toFixed(unit === "em" || unit === "rem" ? 1 : 0);
+
     const newTheme = createTheme({
       ...customTheme,
       fontSizes: {
         ...currFontSizes,
-        [fontSize]: `${value}${unit}`,
+        [fontSize]: `${fixedValue}${unit}`,
       },
     });
     updateCustomTheme(newTheme);
 
     // update curr sizes
     const map = new Map<string, string>(currSizes);
-    map.set(fontSize, `${value}${unit}`);
+    map.set(fontSize, `${fixedValue}${unit}`);
     setCurrSizes(map);
   };
 
-  const onUnitChangedHandler = (selectedUnit) => {
-    setUnit(selectedUnit);
+  const onUnitChangedHandler = (selectedUnit: HvListValue) => {
+    const parsedUnit = selectedUnit.label as string;
+
+    setUnit(parsedUnit);
+
+    if (
+      (parsedUnit === "em" || parsedUnit === "rem") &&
+      fontValue &&
+      fontValue > 10
+    ) {
+      setFontValue(10);
+    }
+
     const newTheme = createTheme({
       ...customTheme,
       fontSizes: {
-        [fontSize]: `${fontValue}${selectedUnit}`,
+        ...customTheme.fontSizes,
+        [fontSize]: `${fontValue}${parsedUnit}`,
       },
     });
     updateCustomTheme(newTheme);
     // update curr sizes
     const map = new Map<string, string>(currSizes);
-    map.set(fontSize, `${fontValue}${selectedUnit}`);
+    map.set(fontSize, `${fontValue}${parsedUnit}`);
     setCurrSizes(map);
   };
 
   return (
-    <div className={styles.root}>
-      <HvTypography variant="label">Font Sizes</HvTypography>
-      <HvBox
-        css={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
+    <HvBox className={styles.root}>
+      <HvBox css={{ marginBottom: theme.space.sm }}>
         <HvDropdown
+          label="Font Sizes"
           classes={{ root: css({ width: 120 }) }}
           values={fontSizes}
-          onChange={(item) => onSizeChangedHandler(item.label)}
-        />
-        <HvDropdown
-          classes={{ root: css({ width: 120 }) }}
-          values={[
-            { label: "px", selected: unit === "px" },
-            {
-              label: "rem",
-              selected: unit === "rem",
-            },
-            { label: "em", selected: unit === "em" },
-            { label: "pt", selected: unit === "pt" },
-          ]}
-          onChange={(item) => onUnitChangedHandler(item.label)}
-        />
-        <HvInput
-          value={fontValue?.toString()}
-          classes={{ root: css({ width: 80 }) }}
-          onChange={onValueChangedHandler}
+          onChange={(item) => onSizeChangedHandler(item?.label)}
         />
       </HvBox>
-    </div>
+      <FontSize
+        disabled={fontSize === ""}
+        fontSize={fontValue}
+        fontUnit={unit}
+        onAfterChange={(val) => onValueSetHandler(val)}
+        onChange={(val) => onValueChangeHandler(val)}
+        onUnitChange={(val) => onUnitChangedHandler(val)}
+      />
+    </HvBox>
   );
 };
 
