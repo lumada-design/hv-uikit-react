dc6038dbfa6bacd4c10090b91d7d92f40e1bf0eb
Márcia Esteves <marciaannie.araujoesteves@hitachivantara.com>
2023-06-20 14:46:42 +0100

    docs: refactor bar chart story custom tooltip

--- packages/viz/src/components/BarChart/stories/customTooltip.ts
+++ packages/viz/src/components/BarChart/stories/customTooltip.ts
@@ -84,46 +84,29 @@ const thresholdSwitcher = (
   return `${roundedValue}% - ${previousValue}%`;
 };
 
+const renderSvgContent = (content: string) => `
+  <div class="${css(styles.icon)}">
+    <svg viewBox="0 0 16 16" height="16" width="16">${content}</svg>
+  </div>
+`;
+
 const iconSwitcher = (value: string) =>
   ({
-    good: `<div class="${css(styles.icon)}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="16" width="16">
-                  <path fill="${
-                    theme.colors.positive
-                  }" d="M6.387 12 4 9.613l.707-.707 1.613 1.613 4.65-5.58.768.641zM8 15a6.953 6.953 0 0 1-4.943-2.057A7.023 7.023 0 1 1 8 15m8-7a8 8 0 1 0-8 8 8 8 0 0 0 8-8z"></path>
-                </svg>
-              </div>`,
-    average: `<div class="${css(styles.icon)}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="16" width="16">
-                  <g fill="${theme.colors.warning}">
-                    <path d="M8 0C3.58179 0 0 3.58173 0 8s3.58179 8 8 8 8-3.58173 8-8c-.00488-4.41626-3.58374-7.99506-8-8zm0 15c-3.86597 0-7-3.13403-7-7 0-3.86603 3.13403-7 7-7s7 3.13397 7 7c-.00439 3.86414-3.13574 6.99561-7 7z"></path>
-                    <path d="M5 7.5h6v1H5z"></path>
-                  </g>
-                </svg>
-              </div>`,
-    poor: `<div class="${css(styles.icon)}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="16" width="16">
-                  <path fill="${
-                    theme.colors.sema12
-                  }" d="M7.5 4.001h1v6h-1zm0 8h1v-1h-1zM16 8a8 8 0 1 0-8 8 8 8 0 0 0 8-8zm-1 0a7 7 0 1 1-7-7 7 7 0 0 1 7 7z"></path>
-                </svg>
-              </div>`,
-    bad: `<div class="${css(styles.icon)}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="16" width="16">
-                  <g fill="${theme.colors.sema14}">
-                    <path d="M8 0C3.58179 0 0 3.58173 0 8s3.58179 8 8 8 8-3.58173 8-8c-.00488-4.41626-3.58374-7.99506-8-8zm0 15c-3.86597 0-7-3.13403-7-7 0-3.86603 3.13403-7 7-7s7 3.13397 7 7c-.00439 3.86414-3.13574 6.99561-7 7z"></path>
-                    <path d="M9 11h1v1H9zm-3 0h1v1H6zm3-7h1v6H9zM6 4h1v6H6z"></path>
-                 </g>
-                </svg>
-              </div>`,
-    critical: `<div class="${css(styles.icon)}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="16" width="16">
-                  <g fill="${theme.colors.sema6}">
-                    <path d="M8 0C3.58179 0 0 3.58173 0 8s3.58179 8 8 8 8-3.58173 8-8c-.00488-4.41626-3.58374-7.99506-8-8zm0 15c-3.86597 0-7-3.13403-7-7 0-3.86603 3.13403-7 7-7s7 3.13397 7 7c-.00439 3.86414-3.13574 6.99561-7 7z"></path>
-                    <path d="M7.5 11h1v1h-1zm3 0h1v1h-1zm-6 0h1v1h-1zm3-7h1v6h-1zm3 0h1v6h-1zm-6 0h1v6h-1z"></path>
-                  </g>
-                </svg>
-              </div>`,
+    good: renderSvgContent(
+      `<path fill="${theme.colors.positive}" d="M6.387 12 4 9.613l.707-.707 1.613 1.613 4.65-5.58.768.641zM8 15a6.953 6.953 0 0 1-4.943-2.057A7.023 7.023 0 1 1 8 15m8-7a8 8 0 1 0-8 8 8 8 0 0 0 8-8z"></path>`
+    ),
+    average: renderSvgContent(
+      `<g fill="${theme.colors.warning}"><path d="M8 0C3.58179 0 0 3.58173 0 8s3.58179 8 8 8 8-3.58173 8-8c-.00488-4.41626-3.58374-7.99506-8-8zm0 15c-3.86597 0-7-3.13403-7-7 0-3.86603 3.13403-7 7-7s7 3.13397 7 7c-.00439 3.86414-3.13574 6.99561-7 7z"></path><path d="M5 7.5h6v1H5z"></path></g>`
+    ),
+    poor: renderSvgContent(
+      `<path fill="${theme.colors.sema12}" d="M7.5 4.001h1v6h-1zm0 8h1v-1h-1zM16 8a8 8 0 1 0-8 8 8 8 0 0 0 8-8zm-1 0a7 7 0 1 1-7-7 7 7 0 0 1 7 7z"></path>`
+    ),
+    bad: renderSvgContent(
+      `<g fill="${theme.colors.sema14}"><path d="M8 0C3.58179 0 0 3.58173 0 8s3.58179 8 8 8 8-3.58173 8-8c-.00488-4.41626-3.58374-7.99506-8-8zm0 15c-3.86597 0-7-3.13403-7-7 0-3.86603 3.13403-7 7-7s7 3.13397 7 7c-.00439 3.86414-3.13574 6.99561-7 7z"></path><path d="M9 11h1v1H9zm-3 0h1v1H6zm3-7h1v6H9zM6 4h1v6H6z"></path></g>`
+    ),
+    critical: renderSvgContent(
+      `<g fill="${theme.colors.sema6}"><path d="M8 0C3.58179 0 0 3.58173 0 8s3.58179 8 8 8 8-3.58173 8-8c-.00488-4.41626-3.58374-7.99506-8-8zm0 15c-3.86597 0-7-3.13403-7-7 0-3.86603 3.13403-7 7-7s7 3.13397 7 7c-.00439 3.86414-3.13574 6.99561-7 7z"></path><path d="M7.5 11h1v1h-1zm3 0h1v1h-1zm-6 0h1v1h-1zm3-7h1v6h-1zm3 0h1v6h-1zm-6 0h1v6h-1z"></path></g>`
+    ),
   }[value.toLowerCase()]);
 
 export const renderTooltip = (params?: HvChartTooltipParams) => {
