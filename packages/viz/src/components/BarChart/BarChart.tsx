e39dfbff1b617a466b82fab4e5129edeb61ca1f6
Márcia Esteves <marciaannie.araujoesteves@hitachivantara.com>
2023-06-20 14:47:08 +0100

    refactor: simplify charts api

--- packages/viz/src/components/BarChart/BarChart.tsx
+++ packages/viz/src/components/BarChart/BarChart.tsx
@@ -18,40 +18,8 @@ export interface HvBarChartProps
  * A bar chart is a chart or graph that presents categorical data with rectangular bars.
  */
 export const HvBarChart = ({
-  data,
-  groupBy,
-  splitBy,
-  measures,
-  sortBy,
-  xAxis,
-  yAxis,
-  legend,
-  tooltip,
-  seriesNameFormatter,
-  stack,
-  horizontalRangeSlider,
-  classes,
   horizontal = false,
-  grid,
+  ...others
 }: HvBarChartProps) => {
-  return (
-    <HvBaseChart
-      type="bar"
-      data={data}
-      groupBy={groupBy}
-      splitBy={splitBy}
-      measures={measures}
-      sortBy={sortBy}
-      yAxis={yAxis}
-      xAxis={xAxis}
-      tooltip={tooltip}
-      legend={legend}
-      seriesNameFormatter={seriesNameFormatter}
-      stack={stack}
-      horizontalRangeSlider={horizontalRangeSlider}
-      horizontal={horizontal}
-      classes={classes}
-      grid={grid}
-    />
-  );
+  return <HvBaseChart type="bar" horizontal={horizontal} {...others} />;
 };
--- packages/viz/src/components/LineChart/LineChart.tsx
+++ packages/viz/src/components/LineChart/LineChart.tsx
@@ -19,44 +19,18 @@ export interface HvLineChartProps
  * connected by straight line segments. It is a basic type of chart common in many fields.
  */
 export const HvLineChart = ({
-  data,
-  groupBy,
-  splitBy,
-  measures,
-  sortBy,
-  xAxis,
-  yAxis,
-  legend,
-  tooltip,
-  seriesNameFormatter,
   area = false,
-  stack,
   emptyCellMode = "void",
-  horizontalRangeSlider,
   areaOpacity = 0.5,
-  classes,
-  grid,
+  ...others
 }: HvLineChartProps) => {
   return (
     <HvBaseChart
       type="line"
-      data={data}
-      groupBy={groupBy}
-      splitBy={splitBy}
-      measures={measures}
-      sortBy={sortBy}
-      xAxis={xAxis}
-      yAxis={yAxis}
-      legend={legend}
-      tooltip={tooltip}
-      seriesNameFormatter={seriesNameFormatter}
       area={area}
-      stack={stack}
       emptyCellMode={emptyCellMode}
-      horizontalRangeSlider={horizontalRangeSlider}
       areaOpacity={areaOpacity}
-      classes={classes}
-      grid={grid}
+      {...others}
     />
   );
 };
