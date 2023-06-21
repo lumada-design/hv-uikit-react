cd66ddf0f2e1de815968d4bae9847c354e455c19
Bruno Henriques <zettca@users.noreply.github.com>
2023-06-21 14:42:56 +0100

    test(Snackbar): improve tests

--- packages/core/src/components/Snackbar/Snackbar.test.tsx
+++ packages/core/src/components/Snackbar/Snackbar.test.tsx
@@ -1,22 +1,41 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { Alert } from "@hitachivantara/uikit-react-icons";
 import { describe, expect, it } from "vitest";
-import { HvSnackbar } from "./Snackbar";
+import { HvSnackbar, HvSnackbarProps } from "./Snackbar";
+
+const setup = (props?: Partial<HvSnackbarProps>) =>
+  render(
+    <HvSnackbar
+      open
+      showIcon
+      variant="success"
+      label="My snackbar"
+      {...props}
+    />
+  );
 
 describe("Snackbar", () => {
-  it("should be defined", () => {
-    const { container } = render(<HvSnackbar />);
-    expect(container).toBeDefined();
+  it("renders the label text", () => {
+    setup();
+    expect(screen.getByRole("alert")).toBeInTheDocument();
+    expect(screen.getByText("My snackbar")).toBeInTheDocument();
   });
 
-  it("should render correctly", () => {
-    const { container } = render(<HvSnackbar />);
-    expect(container).toMatchSnapshot();
+  it("doesn't render when closed", () => {
+    setup({ open: false });
+    expect(screen.queryByRole("alert")).toBeNull();
+    expect(screen.queryByText("My snackbar")).toBeNull();
   });
 
-  it("should render the snackbar text correctly", () => {
-    const { getByText } = render(
-      <HvSnackbar open showIcon variant="success" label="My snackbar" />
-    );
-    expect(getByText("My snackbar")).toBeInTheDocument();
+  it("renders the label custom content", () => {
+    setup({ label: <div>Custom content</div> });
+    expect(screen.getByText("Custom content")).toBeInTheDocument();
+  });
+
+  it("renders the custom icon icon", () => {
+    setup({ customIcon: <Alert data-testid="iconId" /> });
+
+    expect(screen.getByText("My snackbar")).toBeInTheDocument();
+    expect(screen.getByTestId("iconId")).toBeInTheDocument();
   });
 });
