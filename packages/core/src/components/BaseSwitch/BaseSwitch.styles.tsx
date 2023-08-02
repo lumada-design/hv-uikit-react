38310d8ad3ece564ec1bc6efa7d3e5937490c3fc
Márcia Esteves <marciaannie.araujoesteves@hitachivantara.com>
2023-08-02 17:03:38 +0100

    refactor(BaseSwitch): port to create classes

--- packages/core/src/components/BaseSwitch/BaseSwitch.styles.tsx
+++ packages/core/src/components/BaseSwitch/BaseSwitch.styles.tsx
@@ -1,55 +1,31 @@
-import styled from "@emotion/styled";
-
 import { theme } from "@hitachivantara/uikit-styles";
 
-import {
-  Switch as MuiSwitch,
-  switchClasses as MuiSwitchClasses,
-} from "@mui/material";
-
 import { outlineStyles } from "@core/utils/focusUtils";
-import { transientOptions } from "@core/utils/transientOptions";
+import { createClasses } from "@core/utils/classes";
 
-export const StyledSwitch = styled(
-  MuiSwitch,
-  transientOptions
-)(
-  ({
-    $focusVisible,
-    $readOnly,
-    $disabled,
-  }: {
-    $focusVisible: boolean;
-    $readOnly: boolean;
-    $disabled: boolean;
-  }) => ({
+export const { staticClasses, useClasses } = createClasses("HvBaseSwitch", {
+  root: {
     padding: theme.baseSwitch.padding,
     cursor: "pointer",
     width: theme.baseSwitch.width,
     height: theme.baseSwitch.height,
     alignItems: "center",
     justifyContent: "center",
+    borderRadius: theme.baseSwitch.borderRadius,
+
     "&:hover": {
       backgroundColor: theme.baseSwitch.hoverBackgroundColor,
     },
-    borderRadius: theme.baseSwitch.borderRadius,
 
-    [`&.${MuiSwitchClasses.root}`]: {
-      width: theme.baseSwitch.width,
-      height: theme.baseSwitch.height,
-      padding: theme.baseSwitch.padding,
-    },
-
-    [`& .${MuiSwitchClasses.switchBase}`]: {
-      width: theme.baseSwitch.width,
-      height: theme.baseSwitch.height,
-      padding: theme.baseSwitch.padding,
+    // Higher CSS specificity here
+    "& $switchBase": {
       "&:hover": {
         backgroundColor: theme.baseSwitch.hoverBaseBackgroundColor,
       },
-      [`&.${MuiSwitchClasses.checked}`]: {
+
+      "&$checked": {
         transform: "translateX(16px)",
-        [`& + .${MuiSwitchClasses.track}`]: {
+        "& + $track": {
           opacity: theme.baseSwitch.checkedOpacity,
           backgroundColor: theme.baseSwitch.checkedTrackBackgroundColor,
         },
@@ -59,54 +35,66 @@ export const StyledSwitch = styled(
       },
     },
 
-    [`.${MuiSwitchClasses.track}`]: {
-      opacity: theme.baseSwitch.track.opacity,
-      borderRadius: theme.baseSwitch.track.borderRadius,
-      height: theme.baseSwitch.track.height,
-      width: theme.baseSwitch.track.width,
-      border: theme.baseSwitch.track.border,
-      backgroundColor: theme.baseSwitch.track.backgroundColor,
-    },
-
-    [`.${MuiSwitchClasses.thumb}`]: {
-      position: "relative",
-      left: theme.baseSwitch.thumb.left,
-      width: theme.baseSwitch.thumb.width,
-      height: theme.baseSwitch.thumb.height,
-      border: theme.baseSwitch.thumb.border,
-      backgroundColor: theme.baseSwitch.thumb.backgroundColor,
-      marginLeft: theme.baseSwitch.thumb.marginLeft,
-      marginTop: theme.baseSwitch.thumb.marginTop,
-      boxShadow: theme.baseSwitch.thumb.boxShadow,
-    },
-
-    [`.${MuiSwitchClasses.disabled}`]: {
-      [`& .${MuiSwitchClasses.thumb}`]: {
-        backgroundColor: theme.baseSwitch.disabled.thumbBackgroundColor,
-        border: theme.baseSwitch.disabled.thumbBorder,
-      },
-      [`&.${MuiSwitchClasses.switchBase} + .${MuiSwitchClasses.track}`]: {
-        opacity: theme.baseSwitch.disabled.trackOpacity,
-        backgroundColor: theme.baseSwitch.disabled.trackBackgroundColor,
-        border: theme.baseSwitch.disabled.trackBorder,
+    // Higher CSS specificity here
+    "&$disabled": {
+      "& $switchBase": {
+        "& + $track": {
+          opacity: theme.baseSwitch.disabled.trackOpacity,
+          backgroundColor: theme.baseSwitch.disabled.trackBackgroundColor,
+          border: theme.baseSwitch.disabled.trackBorder,
+        },
       },
     },
+  },
+  switch: {
+    width: theme.baseSwitch.width,
+    height: theme.baseSwitch.height,
+    padding: theme.baseSwitch.padding,
 
-    ...($readOnly && {
-      [`.${MuiSwitchClasses.switchBase}`]: {
-        cursor: "default",
-      },
-    }),
+    "&$readOnly $switchBase": {
+      cursor: "default",
+    },
+  },
+  switchBase: {
+    width: theme.baseSwitch.width,
+    height: theme.baseSwitch.height,
+    padding: theme.baseSwitch.padding,
+  },
+  track: {
+    opacity: theme.baseSwitch.track.opacity,
+    borderRadius: theme.baseSwitch.track.borderRadius,
+    height: theme.baseSwitch.track.height,
+    width: theme.baseSwitch.track.width,
+    border: theme.baseSwitch.track.border,
+    backgroundColor: theme.baseSwitch.track.backgroundColor,
+  },
+  thumb: {
+    position: "relative",
+    left: theme.baseSwitch.thumb.left,
+    width: theme.baseSwitch.thumb.width,
+    height: theme.baseSwitch.thumb.height,
+    border: theme.baseSwitch.thumb.border,
+    backgroundColor: theme.baseSwitch.thumb.backgroundColor,
+    marginLeft: theme.baseSwitch.thumb.marginLeft,
+    marginTop: theme.baseSwitch.thumb.marginTop,
+    boxShadow: theme.baseSwitch.thumb.boxShadow,
+  },
+  checked: {},
+  disabled: {
+    cursor: "not-allowed",
 
-    ...($focusVisible && {
-      borderRadius: theme.baseSwitch.focusBorderRadius,
-      ...outlineStyles,
-    }),
+    "& $switch": {
+      cursor: "not-allowed",
+    },
 
-    ...($disabled && {
-      [`&.${MuiSwitchClasses.root}`]: {
-        cursor: "not-allowed",
-      },
-    }),
-  })
-);
+    "& $thumb": {
+      backgroundColor: theme.baseSwitch.disabled.thumbBackgroundColor,
+      border: theme.baseSwitch.disabled.thumbBorder,
+    },
+  },
+  readOnly: {},
+  focusVisible: {
+    borderRadius: theme.baseSwitch.focusBorderRadius,
+    ...outlineStyles,
+  },
+});
--- packages/core/src/components/BaseSwitch/index.ts
+++ packages/core/src/components/BaseSwitch/index.ts
@@ -1,3 +1,1 @@
-export { default as baseSwitchClasses } from "./baseSwitchClasses";
-export * from "./baseSwitchClasses";
 export * from "./BaseSwitch";
