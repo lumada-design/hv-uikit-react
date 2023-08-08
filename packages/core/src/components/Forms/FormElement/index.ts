975e8c285befd1262d963d0b63362fb9dbc82be2
Márcia Esteves <marciaannie.araujoesteves@hitachivantara.com>
2023-08-08 11:53:26 +0100

    refactor(FormElement): port to create classes

--- packages/core/src/components/Forms/FormElement/index.ts
+++ packages/core/src/components/Forms/FormElement/index.ts
@@ -1,5 +1,3 @@
-export { default as formElementClasses } from "./formElementClasses";
-export * from "./formElementClasses";
 export * from "./FormElement";
 export * from "./context/FormElementContext";
 export * from "./context/FormElementValueContext";
