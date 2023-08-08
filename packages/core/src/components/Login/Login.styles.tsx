0755388e8396ed7943bf64bd2a5d96950b73d154
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2023-08-08 14:41:35 +0100

    refactor(Login): port to create classes (#3581)

--- packages/core/src/components/Login/Login.styles.tsx
+++ packages/core/src/components/Login/Login.styles.tsx
@@ -1,20 +1,21 @@
-import styled from "@emotion/styled";
+import { createClasses } from "@core/utils/classes";
 
 import { theme } from "@hitachivantara/uikit-styles";
 
-export const StyledRoot = styled("div")({
-  backgroundRepeat: "no-repeat",
-  backgroundPosition: "bottom",
-  backgroundSize: "cover",
-  width: "100%",
-  height: "100%",
-  padding: 0,
-  margin: "-1px 0 0 0",
-});
-
-export const StyledFormContainer = styled("div")({
-  background: theme.colors.atmo2,
-  marginLeft: "auto",
-  maxWidth: 500,
-  height: "100%",
+export const { staticClasses, useClasses } = createClasses("HvLogin", {
+  root: {
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "bottom",
+    backgroundSize: "cover",
+    width: "100%",
+    height: "100%",
+    padding: 0,
+    margin: "-1px 0 0 0",
+  },
+  formContainer: {
+    background: theme.colors.atmo2,
+    marginLeft: "auto",
+    maxWidth: 500,
+    height: "100%",
+  },
 });
--- packages/core/src/components/Login/index.ts
+++ packages/core/src/components/Login/index.ts
@@ -1,3 +1,1 @@
-export { default as loginClasses } from "./loginClasses";
-export * from "./loginClasses";
 export * from "./Login";
