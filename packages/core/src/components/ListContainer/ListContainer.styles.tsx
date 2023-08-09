25209814ce293421cb1471622f9d808e9dad259a
Márcia Esteves <marciaannie.araujoesteves@hitachivantara.com>
2023-08-09 14:58:05 +0100

    refactor(ListContainer): port to create classes

---
+++ packages/core/src/components/ListContainer/ListContainer.styles.tsx
@@ -0,0 +1,5 @@
+import { createClasses } from "@core/utils/classes";
+
+export const { staticClasses, useClasses } = createClasses("HvListContainer", {
+  root: {},
+});
--- packages/core/src/components/ListContainer/ListContext/ListContext.ts
+++ packages/core/src/components/ListContainer/ListContext/ListContext.ts
@@ -1,6 +1,13 @@
 import React from "react";
 
-const ListContext = React.createContext({});
+const ListContext = React.createContext<{
+  interactive?: boolean;
+  nesting?: number;
+  condensed?: boolean;
+  disableGutters?: boolean;
+  topContainerRef?: React.MutableRefObject<HTMLUListElement | null>;
+}>({});
+
 ListContext.displayName = "ListContext";
 
 export default ListContext;
