1e3d0558063bf1070a159c344080fae30ce35ed4
Márcia Esteves <marciaannie.araujoesteves@hitachivantara.com>
2023-08-11 15:12:06 +0100

    refactor(TagsInput): port to create classes

--- packages/core/src/components/TagsInput/TagsInput.styles.tsx
+++ packages/core/src/components/TagsInput/TagsInput.styles.tsx
@@ -1,91 +1,66 @@
-import styled from "@emotion/styled";
+import { chipClasses } from "@mui/material";
 
 import { theme } from "@hitachivantara/uikit-styles";
 
-import {
-  HvCharCounter,
-  HvFormElement,
-  HvInfoMessage,
-  HvLabel,
-  HvSuggestions,
-  HvWarningText,
-  HvFormElementProps,
-  HvLabelProps,
-  HvInfoMessageProps,
-  HvCharCounterProps,
-  HvWarningTextProps,
-  HvSuggestionsProps,
-  suggestionsClasses,
-} from "@core/components/Forms";
-import { HvInput, HvInputProps } from "@core/components/Input";
-import { HvTag, HvTagProps } from "@core/components/Tag";
+import { suggestionsClasses } from "@core/components/Forms";
 import { baseInputClasses } from "@core/components/BaseInput";
-import {
-  HvListContainer,
-  HvListItemProps,
-  HvListItem,
-  listItemClasses,
-} from "@core/components/ListContainer";
-import { transientOptions } from "@core/utils/transientOptions";
+import { createClasses } from "@core/utils/classes";
 
-import tagsInputClasses from "./tagsInputClasses";
+export const { staticClasses, useClasses } = createClasses("HvTagsInput", {
+  input: {},
+  listItemGutters: { padding: "0 5px" },
+  listItemRoot: {
+    marginBottom: 2,
+    height: 24,
+    lineHeight: "24px",
 
-export const StyledFormElement = styled((props: HvFormElementProps) => (
-  <HvFormElement {...props} />
-))({
-  display: "inline-block",
-  width: "100%",
-});
+    "&:not(:last-child)": {
+      marginBottom: 2,
+    },
 
-export const StyledLabelContainer = styled("div")({
-  float: "left",
-  display: "flex",
-  alignItems: "flex-start",
-});
+    "&$singleLine": {
+      display: "table-cell",
+      paddingTop: "2px",
+    },
+  },
+  root: { display: "inline-block", width: "100%" },
+  chipRoot: {
+    [`&.${chipClasses.root}`]: {
+      maxWidth: "none",
+    },
+  },
+  disabled: {
+    "& $tagsList": {
+      backgroundColor: theme.tagsInput.disabledBackgroundColor,
+      border: `1px solid ${theme.colors.atmo4}`,
 
-export const StyledLabel = styled((props: HvLabelProps) => (
-  <HvLabel {...props} />
-))({
-  display: "block",
-  float: "left",
-  paddingBottom: "6px",
-});
+      "&:focus-within, &:hover": {
+        border: `1px solid ${theme.colors.atmo4}`,
+      },
+    },
+  },
+  readOnly: {
+    "& $tagsList": {
+      backgroundColor: theme.tagsInput.readOnlyBackgroundColor,
+      border: `1px solid ${theme.tagsInput.readOnlyBorderColor}`,
 
-export const StyledDescription = styled((props: HvInfoMessageProps) => (
-  <HvInfoMessage {...props} />
-))({
-  display: "block",
-  float: "left",
-});
-
-export const StyledCharCounter = styled((props: HvCharCounterProps) => (
-  <HvCharCounter {...props} />
-))(() => ({
-  display: "block",
-  float: "right",
-  textAlign: "right",
-  marginBottom: "6px",
-}));
-
-export const StyledTagsList = styled(
-  HvListContainer,
-  transientOptions
-)(
-  ({
-    $disabled,
-    $singleLine,
-    $error,
-    $resizable,
-    $invalid,
-    $readOnly,
-  }: {
-    $disabled: boolean;
-    $singleLine: boolean;
-    $error: boolean;
-    $resizable: boolean;
-    $invalid: boolean;
-    $readOnly: boolean;
-  }) => ({
+      "&:hover": {
+        border: `1px solid ${theme.tagsInput.readOnlyBorderColor}`,
+      },
+    },
+  },
+  resizable: { width: "auto", resize: "both", overflow: "auto" },
+  invalid: {},
+  labelContainer: { float: "left", display: "flex", alignItems: "flex-start" },
+  label: { display: "block", float: "left", paddingBottom: "6px" },
+  description: { display: "block", float: "left" },
+  characterCounter: {
+    display: "block",
+    float: "right",
+    textAlign: "right",
+    marginBottom: "6px",
+  },
+  tagsList: {
     display: "flex",
     alignContent: "flex-start",
     float: "left",
@@ -121,173 +96,93 @@ export const StyledTagsList = styled(
       boxShadow: "0 0 0 1px #52A8EC, 0 0 0 4px rgba(29,155,209,.3)",
     },
 
-    ...($disabled && {
-      backgroundColor: theme.tagsInput.disabledBackgroundColor,
-      border: `1px solid ${theme.colors.atmo4}`,
-
-      "&:focus-within, &:hover": {
-        border: `1px solid ${theme.colors.atmo4}`,
-      },
-    }),
-
-    ...($readOnly && {
-      backgroundColor: theme.tagsInput.readOnlyBackgroundColor,
-      border: `1px solid ${theme.tagsInput.readOnlyBorderColor}`,
-      "&:hover": {
-        border: `1px solid ${theme.tagsInput.readOnlyBorderColor}`,
-      },
-    }),
-
-    ...($singleLine && {
+    "&$singleLine": {
       overflowX: "hidden",
       overflowY: "hidden",
       height: 32,
       display: "table-row",
       paddingTop: 0,
-    }),
+    },
 
-    ...($error && {
+    "&$error": {
       border: `1px solid ${theme.colors.negative}`,
-    }),
+    },
 
-    ...($resizable && {
-      width: "auto",
-      resize: "both",
-      overflow: "auto",
-    }),
-
-    ...($invalid && {
-      border: `1px solid ${theme.colors.negative}!important`,
-    }),
-  })
-);
-
-export const StyledError = styled((props: HvWarningTextProps) => (
-  <HvWarningText {...props} />
-))({
-  float: "left",
-});
-
-export const StyledListItem = styled(
-  (props: HvListItemProps) => <HvListItem {...props} />,
-  transientOptions
-)(({ $singleLine }: { $singleLine: boolean }) => ({
-  marginBottom: 2,
-  height: 24,
-  lineHeight: "24px",
-  "&:not(:last-child)": {
-    marginBottom: 2,
+    "&$invalid": {
+      border: `1px solid ${theme.colors.negative}`,
+    },
   },
-  [`&.${listItemClasses.gutters}`]: {
-    padding: "0 5px",
-  },
-  ...($singleLine && {
-    display: "table-cell",
-    paddingTop: "2px!important",
-  }),
-}));
+  tagInputContainerRoot: {
+    display: "flex",
+    flexGrow: 1,
+    height: 24,
+    lineHeight: "24px",
 
-export const StyledTag = styled(
-  (props: HvTagProps) => <HvTag {...props} />,
-  transientOptions
-)(({ $selected }: { $selected: boolean }) => ({
-  [`& .${tagsInputClasses.chipRoot}`]: {
-    maxWidth: "none",
+    "&$singleLine": {
+      display: "table-cell",
+      minWidth: 100,
+      width: "100%",
+      paddingTop: "3px",
+      verticalAlign: "middle",
+    },
   },
-  ...($selected && {
+  tagInputRoot: {
+    [`& .${baseInputClasses.root}`]: {
+      width: "100%",
+      border: "none",
+      "&:hover $tagInputBorderContainer": {
+        background: "none",
+      },
+    },
+    [`&& .${baseInputClasses.inputRoot}`]: {
+      marginLeft: 0,
+      marginRight: 0,
+      flex: "1 1 auto",
+      minWidth: 48,
+      height: 24,
+      lineHeight: "24px",
+      padding: 0,
+      border: "none",
+    },
+    [`& .${baseInputClasses.inputBorderContainer}`]: {
+      border: "none",
+      background: "none",
+    },
+    [`& .${baseInputClasses.inputRootFocused}`]: {
+      outline: "none",
+      boxShadow: "none",
+    },
+    [`& .${baseInputClasses.root} .${baseInputClasses.inputRootReadOnly}`]: {
+      backgroundColor: "transparent",
+      border: "none",
+      "&:hover": {
+        border: "none",
+      },
+    },
+    [`&& .${baseInputClasses.input}`]: {
+      marginLeft: 0,
+    },
+  },
+  tagSelected: {
     outlineColor: "#52A8EC",
     outlineStyle: "solid",
     outlineWidth: "0px",
     outlineOffset: "-1px",
     boxShadow: "0 0 0 1px #52A8EC, 0 0 0 4px rgba(29,155,209,.3)",
-  }),
-}));
-
-export const StyledInputListItem = styled(
-  (props: HvListItemProps) => <HvListItem {...props} />,
-  transientOptions
-)(
-  ({
-    $singleLine,
-    $isTagSelected,
-  }: {
-    $singleLine: boolean;
-    $isTagSelected: boolean;
-  }) => ({
-    display: "flex",
-    flexGrow: 1,
-    height: 24,
-    lineHeight: "24px",
-    [`&.${listItemClasses.gutters}`]: {
-      padding: "0 5px",
-    },
-    ...($singleLine && {
-      display: "table-cell",
-      minWidth: 100,
+  },
+  tagInputBorderContainer: {},
+  tagInputRootFocused: {},
+  tagInputRootEmpty: {},
+  singleLine: {},
+  error: { float: "left" },
+  inputExtension: {},
+  suggestionsContainer: {
+    width: "100%",
+    position: "relative",
+    top: 59,
+    [`& .${suggestionsClasses.root} .${suggestionsClasses.list} &`]: {
       width: "100%",
-      paddingTop: "3px!important",
-      verticalAlign: "middle",
-    }),
-    ...($isTagSelected && {
-      [`& .${baseInputClasses.inputRoot}`]: {
-        backgroundColor: theme.colors.atmo1,
-      },
-    }),
-  })
-);
-
-export const StyledInput = styled(
-  (props: HvInputProps) => <HvInput {...props} />,
-  transientOptions
-)(({ $singleLine }: { $singleLine: boolean }) => ({
-  [`& .${baseInputClasses.root}`]: {
-    width: "100%",
-    border: "none",
-    [`&:hover .${tagsInputClasses.tagInputBorderContainer}`]: {
-      background: "none",
-    },
-    [`&:hover .${tagsInputClasses.tagInputBorderContainer}`]: {
-      background: "none",
     },
   },
-  [`&& .${baseInputClasses.inputRoot}`]: {
-    marginLeft: 0,
-    marginRight: 0,
-    flex: "1 1 auto",
-    minWidth: 48,
-    height: 24,
-    lineHeight: "24px",
-    padding: 0,
-    border: "none",
-  },
-  [`& .${baseInputClasses.inputBorderContainer}`]: {
-    border: "none",
-    background: "none",
-  },
-  [`& .${baseInputClasses.inputRootFocused}`]: {
-    outline: "none!important",
-    boxShadow: "none!important",
-  },
-  [`& .${baseInputClasses.root} .${baseInputClasses.inputRootReadOnly}`]: {
-    backgroundColor: "transparent ",
-    border: "none",
-    "&:hover": {
-      border: "none",
-    },
-  },
-  [`&& .${baseInputClasses.input}`]: {
-    marginLeft: 0,
-  },
-  ...($singleLine && {}),
-}));
-
-export const StyledSuggestions = styled((props: HvSuggestionsProps) => (
-  <HvSuggestions {...props} />
-))({
-  width: "100%",
-  position: "relative",
-  top: 59,
-  [`& .${suggestionsClasses.root} .${suggestionsClasses.list} &`]: {
-    width: "100%",
-  },
+  suggestionList: {},
 });
