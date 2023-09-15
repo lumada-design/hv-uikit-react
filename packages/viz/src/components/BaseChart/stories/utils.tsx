74d866520be10954fcb43f7016c7f9e20a591777
Bruno Henriques <zettca@users.noreply.github.com>
2023-09-15 16:51:05 +0100

    docs: allow all stories to overflow the preview block

--- packages/viz/src/components/BaseChart/stories/utils.tsx
+++ packages/viz/src/components/BaseChart/stories/utils.tsx
@@ -1,28 +1,14 @@
 import { DecoratorFn } from "@storybook/react";
-import { Global } from "@emotion/react";
 import { css } from "@emotion/css";
 import { theme } from "@hitachivantara/uikit-react-core";
 
 export const vizDecorator: DecoratorFn = (Story) => (
-  <>
-    <Global
-      styles={{
-        // This is necessary for the chart tooltips to not be hidden
-        ".sbdocs.sbdocs-preview": {
-          overflow: "visible",
-          "& > div, & > div > div": {
-            overflow: "visible",
-          },
-        },
-      }}
-    />
-    <div
-      className={css({
-        backgroundColor: theme.colors.atmo1,
-        padding: 20,
-      })}
-    >
-      {Story()}
-    </div>
-  </>
+  <div
+    className={css({
+      backgroundColor: theme.colors.atmo1,
+      padding: 20,
+    })}
+  >
+    {Story()}
+  </div>
 );
