61c555f2e87da8287926191c6128928adbf6e176
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2023-10-19 16:47:29 +0100

    feat(ActionBar): align with DS 5.7 (#3753)

--- packages/core/src/components/ActionBar/ActionBar.styles.tsx
+++ packages/core/src/components/ActionBar/ActionBar.styles.tsx
@@ -6,7 +6,7 @@ export const { staticClasses, useClasses } = createClasses("HvActionBar", {
   root: {
     width: "100%",
     padding: theme.space.sm,
-    borderTop: `1px solid ${theme.colors.atmo3}`,
+    borderTop: `1px solid ${theme.colors.atmo4}`,
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-end",
