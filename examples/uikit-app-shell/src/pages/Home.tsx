189767f3b686633f8ee556b79d0b5f2eb1a7cf7d
Bruno Henriques <zettca@users.noreply.github.com>
2023-11-20 19:49:05 +0000

    docs: simplify app-shell template

---
+++ examples/uikit-app-shell/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { HvTypography } from "@hitachivantara/uikit-react-core";
+
+export default function Home() {
+  return (
+    <>
+      <HvTypography variant="title1">
+        Welcome to UI Kit + App Shell 👋
+      </HvTypography>
+      <HvTypography variant="body">
+        Edit <code>src/pages/Home.tsx</code> and save to get started.
+      </HvTypography>
+    </>
+  );
+}
--- examples/uikit-app-shell/src/pages/Page2/Page2.tsx
+++
@@ -1,9 +0,0 @@
-import {
-  HvGrid,
-  HvTypography,
-  HvGlobalActions,
-} from "@hitachivantara/uikit-react-core";
-
-export default function Page2() {
-  return <HvTypography variant="title2">Page 2</HvTypography>;
-}
