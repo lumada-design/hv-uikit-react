f90f2f61d468126884ab3e410f1ff53698a68c8e
Bruno Henriques <zettca@users.noreply.github.com>
2023-11-23 10:00:29 +0000

    chore: add responsive & another sample

--- packages/lab/src/components/Dashboard/Dashboard.stories.tsx
+++ packages/lab/src/components/Dashboard/Dashboard.stories.tsx
@@ -2,6 +2,13 @@ import { Meta, StoryObj } from "@storybook/react";
 import { css } from "@emotion/css";
 import { theme } from "@hitachivantara/uikit-styles";
 import {
+  HvBarChart,
+  HvLineChart,
+  HvDonutChart,
+  HvChartData,
+  HvConfusionMatrix,
+} from "@hitachivantara/uikit-react-viz";
+import {
   HvDashboard,
   HvDashboardProps,
   HvDashboardItem,
@@ -19,7 +26,7 @@ interface Item extends HvDashboardItem {
   label: string;
 }
 
-const items = ["1", "2", "3"].map<Item>((id) => ({
+const items = ["1", "2", "3", "4"].map<Item>((id) => ({
   id,
   type: "txt",
   label: `Item ${id}`,
@@ -36,7 +43,8 @@ export const Main: StoryObj<HvDashboardProps> = {
         layout={[
           { i: "1", x: 0, y: 0, w: 6, h: 2, isDraggable: false },
           { i: "2", x: 6, y: 0, w: 4, h: 1, isResizable: false },
-          { i: "3", x: 6, y: 1, w: 2, h: 1 },
+          { i: "3", x: 6, y: 1, w: 2, h: 1, resizeHandles: ["e", "w"] },
+          { i: "4", x: 10, y: 0, w: 2, h: 1, static: true },
         ]}
         renderItem={(item) => (
           <div
@@ -52,6 +60,74 @@ export const Main: StoryObj<HvDashboardProps> = {
             {item.label}
           </div>
         )}
+      />
+    );
+  },
+};
+
+const itemIds = ["bar", "line", "conf", "donut", "bar", "txt", "null"] as const;
+
+interface ItemType extends HvDashboardItem {
+  type: (typeof itemIds)[number];
+  data: HvChartData;
+}
+
+const dataDrivenItems = itemIds.map<ItemType>((type, i) => ({
+  id: `${type}-${i}`,
+  type,
+  data: new Map<string, (string | number)[]>()
+    .set("Group", ["Group 1", "Group 2", "Group 3"])
+    .set("Sales Target", [2300, 1000, 7800])
+    .set("Sales Per Rep", [6000, 3900, 1000])
+    .set("Monthly Sales", [3700, 6700, 1100])
+    .set("Target", [2100, 7700, 3000])
+    .set("Cash", [500, 7600, 7800]),
+  groupBy: "Group",
+  measures: ["Sales Target", "Sales Per Rep", "Monthly Sales", "Target"],
+  measure: "Sales Target",
+}));
+
+const TxtRenderer = ({ data }) => (
+  <pre className={css({ width: "100%", overflow: "auto", textWrap: "wrap" })}>
+    <code>{JSON.stringify(Object.fromEntries(data))}</code>
+  </pre>
+);
+
+export const DataDriven: StoryObj<HvDashboardProps> = {
+  argTypes: {
+    classes: { control: { disable: true } },
+  },
+  render: () => {
+    return (
+      <HvDashboard
+        items={dataDrivenItems}
+        layout={dataDrivenItems.map(({ id }, i) => ({
+          i: id,
+          x: (i % 2) * (12 / 2),
+          y: i * 4,
+          w: 12 / 2,
+          h: 3,
+          isDraggable: false,
+        }))}
+        renderItem={(item) => {
+          const { id, type, ...props } = item;
+          const componentMap: Record<typeof type, any> = {
+            bar: HvBarChart,
+            line: HvLineChart,
+            donut: HvDonutChart,
+            conf: HvConfusionMatrix,
+            txt: TxtRenderer,
+            null: null,
+          };
+
+          const Component = componentMap[type];
+
+          if (!Component) {
+            return <div>❌ Cannot render {type} dashboard item</div>;
+          }
+
+          return <Component {...props} />;
+        }}
       />
     );
   },
--- packages/lab/src/components/Dashboard/Dashboard.tsx
+++ packages/lab/src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,10 @@
+import { useMemo } from "react";
 import { Global } from "@emotion/react";
-import ReactGridLayout, {
+import {
+  Responsive as ResponsiveGrid,
+  ResponsiveProps,
   ReactGridLayoutProps,
+  Layouts,
   WidthProvider,
 } from "react-grid-layout";
 import {
@@ -8,9 +12,20 @@ import {
   useDefaultProps,
 } from "@hitachivantara/uikit-react-core";
 
+import { useTheme } from "@core/hooks/useTheme";
+
 import { gridStyles, staticClasses, useClasses } from "./Dashboard.styles";
 
-const GridLayout = WidthProvider(ReactGridLayout);
+const GridLayout = WidthProvider(ResponsiveGrid);
+
+/** Default column breakpoints. For now, use always 12-column */
+const defaultCols: HvDashboardProps["cols"] = {
+  xs: 12,
+  sm: 12,
+  md: 12,
+  lg: 12,
+  xl: 12,
+};
 
 export { staticClasses as dashboardClasses };
 
@@ -23,9 +38,10 @@ export interface HvDashboardItem {
 
 export interface HvDashboardProps<
   Item extends HvDashboardItem = HvDashboardItem
-> extends ReactGridLayoutProps {
+> extends ResponsiveProps {
   classes?: HvDashboardClasses;
   items?: Item[];
+  layout?: ReactGridLayoutProps["layout"];
   renderItem: (item: Item) => React.ReactNode;
 }
 
@@ -37,17 +53,33 @@ export const HvDashboard = <Item extends HvDashboardItem = HvDashboardItem>(
 ) => {
   const {
     className,
+    layout,
+    layouts: layoutsProp,
     classes: classesProp,
     items,
     renderItem,
     ...others
   } = useDefaultProps("HvDashboard", props);
   const { classes, cx } = useClasses(classesProp);
+  const { activeTheme } = useTheme();
+
+  const layouts = useMemo<Layouts>(() => {
+    if (layoutsProp) return layoutsProp;
+    if (!layout) return {};
+
+    return { xs: layout, sm: layout, md: layout, lg: layout, xl: layout };
+  }, [layoutsProp, layout]);
 
   return (
     <>
       <Global styles={gridStyles} />
-      <GridLayout className={cx(classes.root, className)} {...others}>
+      <GridLayout
+        className={cx(classes.root, className)}
+        breakpoints={activeTheme?.breakpoints.values}
+        cols={defaultCols}
+        layouts={layouts}
+        {...others}
+      >
         {items?.map((item) => (
           <div key={item.id} className={classes.item}>
             {renderItem(item)}
