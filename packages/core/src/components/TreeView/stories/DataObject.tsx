716820e72ccfca2a99889bab6a35c3534de96eb7
Bruno Henriques <zettca@users.noreply.github.com>
2023-12-05 15:58:43 +0000

    docs(TreeView): use raw code samples

---
+++ packages/core/src/components/TreeView/stories/DataObject.tsx
@@ -0,0 +1,88 @@
+import { forwardRef } from "react";
+import { css } from "@emotion/css";
+import { Doc, Folders } from "@hitachivantara/uikit-react-icons";
+import {
+  HvPanel,
+  HvTreeView,
+  HvTreeItem,
+  HvTreeItemProps,
+} from "@hitachivantara/uikit-react-core";
+
+type TreeData = { id: string; label: string; children?: TreeData[] };
+
+const treeDataObject = {
+  id: "user",
+  label: "User",
+  children: [
+    {
+      id: "Applications",
+      label: "Applications",
+      children: [
+        { id: "Code", label: "Code.app" },
+        { id: "Chrome", label: "Chrome.app" },
+        { id: "Firefox", label: "Firefox.app" },
+      ],
+    },
+    {
+      id: "Documents",
+      label: "Documents",
+      children: [{ id: "secret", label: "secret.txt" }],
+    },
+    {
+      id: "git",
+      label: "git",
+      children: [
+        {
+          id: "uikit-react",
+          label: "uikit-react",
+          children: [{ id: "uikit-pkg", label: "package.json" }],
+        },
+        {
+          id: "app-shell",
+          label: "app-shell",
+          children: [{ id: "as-pkg", label: "package.json" }],
+        },
+      ],
+    },
+  ],
+} satisfies TreeData;
+
+const SimpleTreeItem = forwardRef<HTMLLIElement, HvTreeItemProps>(
+  (props, ref) => {
+    const { children, nodeId, label, ...others } = props;
+    const Icon = children ? Folders : Doc;
+
+    return (
+      <HvTreeItem
+        ref={ref}
+        nodeId={nodeId}
+        label={
+          <div className={css({ display: "flex", alignItems: "center" })}>
+            <Icon />
+            <span style={{ flex: 1 }}>{label}</span>
+          </div>
+        }
+        {...others}
+      >
+        {children}
+      </HvTreeItem>
+    );
+  }
+);
+
+/** Render tree view items */
+const renderItem = ({ id, label, children }: TreeData) => (
+  <SimpleTreeItem key={id} nodeId={id} label={label}>
+    {children?.map(renderItem)}
+  </SimpleTreeItem>
+);
+
+export const DataObject = () => {
+  return (
+    <HvPanel style={{ width: 400 }}>
+      <HvTreeView aria-label="file system navigator">
+        {renderItem(treeDataObject)}
+      </HvTreeView>
+    </HvPanel>
+  );
+};
---
+++ packages/core/src/components/TreeView/stories/VerticalNavigation.tsx
@@ -0,0 +1,119 @@
+import { forwardRef, useMemo, useState } from "react";
+import { css } from "@emotion/css";
+import { DropDownXS } from "@hitachivantara/uikit-react-icons";
+import {
+  HvPanel,
+  HvTreeView,
+  HvTreeItem,
+  HvTreeItemProps,
+  HvTypography,
+  NavigationData,
+  useHvTreeItem,
+  theme,
+} from "@hitachivantara/uikit-react-core";
+
+interface CustomTreeItemProps extends HvTreeItemProps {
+  /** Triggered when the tree item is expanded */
+  onOpen?: HvTreeItemProps["onClick"];
+}
+
+const NavigationItem = forwardRef<HTMLLIElement, CustomTreeItemProps>(
+  (props, ref) => {
+    const { children, nodeId, label, onOpen, onClick, ...others } = props;
+    const { disabled, expanded } = useHvTreeItem(nodeId);
+
+    const level = nodeId.split("-").length - 1;
+
+    return (
+      <HvTreeItem
+        ref={ref}
+        nodeId={nodeId}
+        style={{ pointerEvents: disabled ? "none" : undefined }}
+        classes={{
+          group: css({ marginLeft: 0 }),
+          content: css({ paddingLeft: 16 * level }),
+          selected: css({
+            borderLeft: `4px solid ${theme.colors.secondary}`,
+            backgroundColor: theme.colors.atmo3,
+          }),
+        }}
+        icon="" // remove left nav icon
+        label={
+          <div className={css({ display: "flex", alignItems: "center" })}>
+            <HvTypography
+              variant={children ? "label" : "body"}
+              style={{ flex: 1 }}
+            >
+              {label}
+            </HvTypography>
+            {children && (
+              <DropDownXS
+                style={{ transform: `rotate(${expanded ? 180 : 0}deg)` }}
+              />
+            )}
+          </div>
+        }
+        {...others}
+      >
+        {children}
+      </HvTreeItem>
+    );
+  }
+);
+
+/** Render tree view items */
+const renderItem = ({ id, label, data }: NavigationData) => (
+  <NavigationItem key={id} nodeId={id} label={label}>
+    {data?.map(renderItem)}
+  </NavigationItem>
+);
+
+export const VerticalNavigation = () => {
+  const [selected, setSelected] = useState("-1");
+
+  const navData = useMemo<NavigationData[]>(
+    () => [
+      { id: "00", label: "Overview" },
+      { id: "01", label: "Analytics" },
+      {
+        id: "02",
+        label: "Storage",
+        data: [
+          {
+            id: "02-01",
+            label: "Cloud",
+            data: [
+              { id: "02-01-01", label: "Servers" },
+              { id: "02-01-02", label: "HCP Anywhere" },
+              { id: "02-01-03", label: "This Computer", disabled: true },
+            ],
+          },
+        ],
+      },
+      {
+        id: "03",
+        label: "Administration",
+        data: [
+          {
+            id: "03-01",
+            label: "Rest API",
+            data: [{ id: "03-01-01", label: "Log Bundle" }],
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  return (
+    <HvPanel style={{ width: 300 }}>
+      <HvTreeView
+        aria-label="site navigation"
+        selected={selected}
+        onNodeSelect={(evt, nodeId) => setSelected(nodeId)}
+      >
+        {navData.map(renderItem)}
+      </HvTreeView>
+    </HvPanel>
+  );
+};
