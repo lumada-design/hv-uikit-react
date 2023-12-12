3bed589e8f7a7e6a8b5cb096c91439b88e76590f
francisco-guilherme <francisco.guilherme@hitachivantara.com>
2023-12-12 16:01:09 +0000

    chore: cleanup tsconfig and viteconfig build setup

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,13 +1,3 @@
-import path from "path";
-
-import { mergeConfig } from "vite";
-
 import viteConfig from "../../.config/vite.config";
 
-export default mergeConfig(viteConfig, {
-  resolve: {
-    alias: {
-      "@core": path.resolve(__dirname, "src"),
-    },
-  },
-});
+export default viteConfig;
