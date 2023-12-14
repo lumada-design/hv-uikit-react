2dee0db2bc4d3bdbda0e5c23ccc5f78b35474dac
Bruno Henriques <zettca@users.noreply.github.com>
2023-12-14 17:48:11 +0000

    refactor: move icon test samples to Tests/

---
+++ docs/tests/Icons.stories.tsx
@@ -0,0 +1,44 @@
+import { StoryObj } from "@storybook/react";
+import {
+  HvIconSprite,
+  HvIconSpriteProps,
+  IconBaseProps,
+  icons,
+} from "@hitachivantara/uikit-react-icons";
+
+export default {
+  title: "Tests/Icons",
+  parameters: {
+    eyes: { include: true },
+    docs: { disable: true },
+  },
+};
+
+export const IconLibrary: StoryObj<IconBaseProps> = {
+  args: {
+    iconSize: "M",
+    color: ["secondary", "negative", "positive"],
+  },
+  render: (args) => (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {Object.entries(icons).map(([name, Icon]) => (
+        <Icon key={name} {...args} />
+      ))}
+    </div>
+  ),
+};
+
+export const IconSpriteLibrary: StoryObj<HvIconSpriteProps> = {
+  args: {
+    iconSize: "M",
+    spriteUrl: "./assets/icons.svg",
+    color: ["secondary", "negative", "positive"],
+  },
+  render: (args) => (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {Object.entries(icons).map(([name]) => (
+        <HvIconSprite key={name} {...args} iconName={name} />
+      ))}
+    </div>
+  ),
+};
