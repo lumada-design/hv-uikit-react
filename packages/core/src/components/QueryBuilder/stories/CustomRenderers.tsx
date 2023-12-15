382dc3cbebffcdce78284af060267d084e2b24a4
Márcia Esteves <marciaannie.araujoesteves@hitachivantara.com>
2023-12-15 18:11:21 +0000

    docs(QueryBuilder): usage docs

--- packages/core/src/components/QueryBuilder/stories/CustomRenderers.tsx
+++ packages/core/src/components/QueryBuilder/stories/CustomRenderers.tsx
@@ -140,8 +140,6 @@ const SelectRenderer = ({
     }
   };
 
-  if (operator === "Empty") return null;
-
   if (operator === "range") {
     return (
       <div
@@ -213,11 +211,8 @@ const sliderRenderers = {
 const SliderRenderer = ({
   id,
   attribute,
-  operator,
 }: HvQueryBuilderRendererProps<SelectValue>) => {
   const { dispatchAction } = useQueryBuilderContext();
-
-  if (operator === "Empty") return null;
 
   return (
     <HvSlider
