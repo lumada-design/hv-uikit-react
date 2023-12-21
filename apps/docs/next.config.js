83bd1153ff1fd147617680b1ebe850abd4d2d117
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2023-12-21 17:27:21 +0000

    chore(docs): add basic nextra setup

---
+++ apps/docs/next.config.js
@@ -0,0 +1,38 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
+});
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
+module.exports = withNextra({
+  transpilePackages: [
+    "@hitachivantara/uikit-react-core",
+    "@hitachivantara/uikit-react-code-editor",
+    "@hitachivantara/uikit-react-icons",
+    "@hitachivantara/uikit-react-lab",
+    "@hitachivantara/uikit-react-shared",
+    "@hitachivantara/uikit-styles",
+    "@hitachivantara/uikit-react-viz",
+  ],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(tsx|ts)$/,
+      use: [
+        {
+          // Replace vite's import.meta.env.DEV for something compatible with NextJS
+          // https://nextjs.org/docs/app/building-your-application/upgrading/from-vite#step-7-migrate-the-environment-variables
+          loader: "string-replace-loader",
+          options: {
+            search: "import.meta.env.DEV",
+            replace: "process.env.NODE_ENV !== 'production'",
+            flags: "g",
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+});
---
+++ apps/docs/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:400,600"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
