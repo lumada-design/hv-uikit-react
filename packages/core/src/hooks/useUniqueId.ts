d1710712e9f898c62c1553f93644888016944eeb
Henrique Raposo <henrique.raposo@hitachivantara.com>
2023-12-21 11:26:48 +0000

    feat(useid): add react useid to useuniqueid hook

--- packages/core/src/hooks/useUniqueId.ts
+++ packages/core/src/hooks/useUniqueId.ts
@@ -1,6 +1,25 @@
-import { useMemo } from "react";
+import * as React from "react"; // this can be optimized when react 17 support is dropped
 
 import uniqueId from "lodash/uniqueId";
 
-export const useUniqueId = (id?: string, idPrefix?: string) =>
-  useMemo(() => id || uniqueId(idPrefix), [id, idPrefix]);
+/** Credit: https://github.com/radix-ui/primitives/blob/main/packages/react/id/src/id.tsx
+ *  Modified slightly to suit our purposes.
+ */
+// We `toString()` to prevent bundlers from trying to `import { useId } from 'react';`
+const useReactId = (React as any)["useId".toString()] || (() => undefined);
+
+export const useUniqueId = (
+  deterministicId?: string,
+  idPrefix?: string
+): string => {
+  const [id, setId] = React.useState<string | undefined>(useReactId());
+
+  React.useMemo(
+    () => {
+      if (!id) setId(uniqueId(idPrefix));
+    }, // loadash unique id can be removed when react 17 support is dropped
+    [id, idPrefix]
+  );
+
+  return deterministicId || (idPrefix ? idPrefix + id : id ?? "");
+};
