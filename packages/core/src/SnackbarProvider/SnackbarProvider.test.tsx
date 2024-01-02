13dc00ddf64484f7f4fbd22cd05e4697edac7430
Bruno Henriques <zettca@users.noreply.github.com>
2024-01-02 16:01:51 +0000

    docs(SnackbarProvider): improve sample

--- packages/core/src/SnackbarProvider/SnackbarProvider.test.tsx
+++ packages/core/src/SnackbarProvider/SnackbarProvider.test.tsx
@@ -1,93 +1,46 @@
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
-import { SnackbarProvider } from "./SnackbarProvider.stories";
+import { SnackbarProviderButtons as SnackbarProvider } from "./stories/SnackbarProviderButtons";
+
+const assertSnackbar = async (buttonName: string, snackbarText: string) => {
+  const snackbarSpawner = screen.getByRole("button", { name: buttonName });
+
+  expect(screen.queryByText(snackbarText)).not.toBeInTheDocument();
+
+  fireEvent.click(snackbarSpawner);
+  expect(await screen.findByText(snackbarText)).toBeInTheDocument();
+
+  fireEvent.click(snackbarSpawner);
+  fireEvent.click(snackbarSpawner);
+  fireEvent.click(snackbarSpawner);
+  fireEvent.click(snackbarSpawner);
+  const snackbars = await screen.findAllByText(snackbarText);
+  expect(snackbars.length).toBe(5);
+};
 
 describe("HvSnackbarProvider", () => {
   describe("interactions", () => {
-    it("renders the default snackbar", async () => {
-      const buttonText = "default";
-      const defaultSnackbar = "This is a snackbar.";
-      const { getByRole, findByText, findAllByText, queryByText } = render(
-        <SnackbarProvider />
-      );
+    it("renders the action snackbar", async () => {
+      render(<SnackbarProvider />);
 
-      const snackbarSpawner = getByRole("button", { name: buttonText });
-      expect(snackbarSpawner).toBeInTheDocument();
-      let snackbar = queryByText(defaultSnackbar);
-      expect(snackbar).not.toBeInTheDocument();
-      fireEvent.click(snackbarSpawner);
-      snackbar = await findByText(defaultSnackbar);
-      expect(snackbar).toBeInTheDocument();
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      const snackbars = await findAllByText(defaultSnackbar);
-      expect(snackbars.length).toBe(5);
+      await assertSnackbar("Action", "This is an action snackbar");
     });
-    it("renders the success snackbar", async () => {
-      const buttonText = "success";
-      const defaultSnackbar = "This is a success message.";
-      const { getByRole, findByText, findAllByText, queryByText } = render(
-        <SnackbarProvider />
-      );
 
-      const snackbarSpawner = getByRole("button", { name: buttonText });
-      expect(snackbarSpawner).toBeInTheDocument();
-      let snackbar = queryByText(defaultSnackbar);
-      expect(snackbar).not.toBeInTheDocument();
-      fireEvent.click(snackbarSpawner);
-      snackbar = await findByText(defaultSnackbar);
-      expect(snackbar).toBeInTheDocument();
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      const snackbars = await findAllByText(defaultSnackbar);
-      expect(snackbars.length).toBe(5);
+    it("renders the success snackbar", async () => {
+      render(<SnackbarProvider />);
+
+      await assertSnackbar("Success", "This is a success snackbar");
     });
     it("renders the warning snackbar", async () => {
-      const buttonText = "warning";
-      const defaultSnackbar = "This is a warning message.";
-      const { getByRole, findByText, findAllByText, queryByText } = render(
-        <SnackbarProvider />
-      );
+      render(<SnackbarProvider />);
 
-      const snackbarSpawner = getByRole("button", { name: buttonText });
-      expect(snackbarSpawner).toBeInTheDocument();
-      let snackbar = queryByText(defaultSnackbar);
-      expect(snackbar).not.toBeInTheDocument();
-      fireEvent.click(snackbarSpawner);
-      snackbar = await findByText(defaultSnackbar);
-      expect(snackbar).toBeInTheDocument();
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      const snackbars = await findAllByText(defaultSnackbar);
-      expect(snackbars.length).toBe(5);
+      await assertSnackbar("Warning", "This is a warning snackbar");
     });
     it("renders the error snackbar", async () => {
-      const buttonText = "error";
-      const defaultSnackbar = "This is an error message.";
-      const { getByRole, findByText, findAllByText, queryByText } = render(
-        <SnackbarProvider />
-      );
+      render(<SnackbarProvider />);
 
-      const snackbarSpawner = getByRole("button", { name: buttonText });
-      expect(snackbarSpawner).toBeInTheDocument();
-      let snackbar = queryByText(defaultSnackbar);
-      expect(snackbar).not.toBeInTheDocument();
-      fireEvent.click(snackbarSpawner);
-      snackbar = await findByText(defaultSnackbar);
-      expect(snackbar).toBeInTheDocument();
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      fireEvent.click(snackbarSpawner);
-      const snackbars = await findAllByText(defaultSnackbar);
-      expect(snackbars.length).toBe(5);
+      await assertSnackbar("Error", "This is an error snackbar");
     });
   });
 });
