f3007a00ae333027c4d657f4a36c14624b50fc4b
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-01-02 18:05:34 +0000

    chore(docs): set hv provider & add intro page

---
+++ apps/docs/components/introduction/resources.tsx
@@ -0,0 +1,71 @@
+import { css } from "@emotion/css";
+import { HvSimpleGrid } from "@hitachivantara/uikit-react-core";
+import { Tetris, Ungroup, Table } from "@hitachivantara/uikit-react-icons";
+
+const styles = {
+  root: css({ padding: 20 }),
+  container: css({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+    minHeight: "80px",
+    gap: "15px",
+    "> div": {
+      height: "80px",
+      width: "70px",
+    },
+    p: {
+      fontSize: "14px",
+      textAlign: "left",
+    },
+    h3: {
+      fontSize: "16px",
+      fontWeight: "bold",
+    },
+  }),
+};
+
+const Resources = () => (
+  <HvSimpleGrid
+    breakpoints={[
+      {
+        cols: 3,
+        minWidth: 1000,
+      },
+      {
+        cols: 2,
+        minWidth: 755,
+      },
+    ]}
+    spacing="lg"
+    className={styles.root}
+  >
+    <div className={styles.container}>
+      <Tetris iconSize="L" />
+      <h3>Components</h3>
+      <p>
+        UI Building blocks that can be used in various combinations to create a
+        larger user interface.
+      </p>
+    </div>
+    <div className={styles.container}>
+      <Ungroup iconSize="L" />
+      <h3>Widgets</h3>
+      <p>
+        Specialized components that address specific use cases and can be used
+        as base to support other scenarios.
+      </p>
+    </div>
+    <div className={styles.container}>
+      <Table iconSize="L" />
+      <h3>Templates</h3>
+      <p>
+        Pre-designed layouts that address common UI patterns and can be used to
+        quickly create a consistent design.
+      </p>
+    </div>
+  </HvSimpleGrid>
+);
+
+export default Resources;
--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,3 +1,19 @@
+import { HvProvider } from "@hitachivantara/uikit-react-core";
+import { useTheme } from "nextra-theme-docs";
+
+const MainContainer = ({ children }: { children: React.ReactNode }) => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <HvProvider
+      classNameKey="hv-docs"
+      colorMode={resolvedTheme === "dark" ? "wicked" : "dawn"}
+    >
+      {children}
+    </HvProvider>
+  );
+};
+
 // More configs here: https://nextra.site/docs/docs-theme/theme-configuration
 export default {
   logo: <span>NEXT UI Kit</span>, // TODO - Use svg icon to change the "fill" property with "currentColor"
@@ -17,4 +33,7 @@ export default {
     ),
   },
   sidebar: { toggleButton: true },
+  main: ({ children }) => <MainContainer>{children}</MainContainer>,
+  primaryHue: 212,
+  primarySaturation: 70,
 };
