68d049259d953de037f2c7a007fff5df63e39331
Henrique Raposo <henrique.raposo@hitachivantara.com>
2024-01-03 11:55:45 +0000

    chore: add sugestions

--- packages/core/src/Controls/LeftControl/LeftControl.tsx
+++ packages/core/src/Controls/LeftControl/LeftControl.tsx
@@ -5,8 +5,6 @@ import { setId } from "../../utils/setId";
 import { HvInput, HvInputProps } from "../../Input";
 import { ExtractNames } from "../../utils/classes";
 
-import { useUniqueId } from "../../hooks/useUniqueId";
-
 import { useClasses, staticClasses } from "./LeftControl.styles";
 import { HvControlsContext } from "../context/ControlsContext";
 
@@ -31,7 +29,7 @@ export interface HvLeftControlProps extends HvBaseProps {
 }
 
 export const HvLeftControl = ({
-  id: idProp,
+  id,
   classes: classesProp,
   className,
   children,
@@ -42,8 +40,6 @@ export const HvLeftControl = ({
   ...others
 }: HvLeftControlProps) => {
   const { classes, cx } = useClasses(classesProp);
-
-  const id = useUniqueId(idProp, "hvleftcontrol");
 
   const { onSearch: onSearchHandler } = useContext(HvControlsContext);
 
