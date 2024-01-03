9c3facea7370b51c78a8be6df4a99e43ed7f0971
Henrique Raposo <henrique.raposo@hitachivantara.com>
2024-01-03 11:52:25 +0000

    chore: remove loadash

--- packages/core/src/hooks/useUniqueId.ts
+++ packages/core/src/hooks/useUniqueId.ts
@@ -1,12 +1,11 @@
 import * as React from "react"; // this can be optimized when react 17 support is dropped
 
-import uniqueId from "lodash/uniqueId";
-
 /** Credit: https://github.com/radix-ui/primitives/blob/main/packages/react/id/src/id.tsx
  *  Modified slightly to suit our purposes.
  */
 // We `toString()` to prevent bundlers from trying to `import { useId } from 'react';`
 const useReactId = (React as any)["useId".toString()] || (() => undefined);
+let count = 0;
 
 export const useUniqueId = (
   deterministicId?: string,
@@ -14,12 +13,10 @@ export const useUniqueId = (
 ): string => {
   const [id, setId] = React.useState<string | undefined>(useReactId());
 
-  React.useMemo(
-    () => {
-      if (!id) setId(uniqueId(idPrefix));
-    }, // loadash unique id can be removed when react 17 support is dropped
-    [id, idPrefix]
-  );
+  React.useLayoutEffect(() => {
+    // eslint-disable-next-line no-plusplus
+    if (!deterministicId) setId((reactId) => reactId ?? String(count++));
+  }, [deterministicId, idPrefix]);
 
   return deterministicId || (idPrefix ? idPrefix + id : id ?? "");
 };
