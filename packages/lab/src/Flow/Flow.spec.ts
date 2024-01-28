2efbcc519858443c4e7ad0005425c00f1b52d5a2
VGervasio <vasco.gervasio@hitachivantara.com>
2024-01-28 18:33:40 +0000

    test: playwright flow test scenarios

---
+++ packages/lab/src/Flow/Flow.spec.ts
@@ -0,0 +1,367 @@
+import { test, expect, type Locator, type Page } from "@playwright/test";
+
+let flowCanvasLocator: Locator;
+let referencePage: Page;
+
+async function selectNode(
+  nodeGroup: string,
+  nodeName: string,
+  withExpand: boolean
+) {
+  const baseAssetNodeLocator = referencePage
+    .getByRole("listitem")
+    .filter({ hasText: nodeGroup });
+  if (withExpand) {
+    await baseAssetNodeLocator
+      .getByRole("button", { name: "Expand Group" })
+      .click();
+  }
+  return baseAssetNodeLocator.getByRole("button", { name: nodeName });
+}
+
+async function dragToPosition1(node: Locator) {
+  await node.dragTo(flowCanvasLocator, { targetPosition: { x: 120, y: 50 } });
+}
+
+async function dragToPosition2(node: Locator) {
+  await node.dragTo(flowCanvasLocator, { targetPosition: { x: 130, y: 400 } });
+}
+
+async function dragToPosition3(node: Locator) {
+  await node.dragTo(flowCanvasLocator, { targetPosition: { x: 620, y: 80 } });
+}
+
+async function connectNodes(
+  sourceNode: string,
+  sourceConnection: string,
+  destinyNode: string,
+  destinyConnection
+) {
+  const source = flowCanvasLocator
+    .getByRole("button", { name: sourceNode })
+    .locator(".react-flow__handle-right")
+    .filter({
+      has: referencePage
+        .locator("../div")
+        .getByText(sourceConnection, { exact: true }),
+    });
+  const destiny = flowCanvasLocator
+    .getByRole("button", { name: destinyNode })
+    .locator(".react-flow__handle-left")
+    .filter({
+      has: referencePage
+        .locator("../div")
+        .getByText(destinyConnection, { exact: true }),
+    });
+  await source.dragTo(destiny, { force: true });
+}
+
+async function deleteConnection(connectionName: string) {
+  await referencePage.getByRole("button", { name: connectionName }).click();
+  await referencePage.keyboard.press("Backspace");
+}
+
+async function moveUp(renderedNode: Locator, verticalMovement: number) {
+  const renderedNodeBox = await renderedNode.boundingBox();
+  const initialPointX = renderedNodeBox!.x + 30;
+  const initialPointY = renderedNodeBox!.y + 30;
+  await referencePage.mouse.move(initialPointX, initialPointY);
+  await referencePage.mouse.down();
+  await referencePage.mouse.move(
+    initialPointX,
+    initialPointY - verticalMovement
+  );
+  await referencePage.mouse.up();
+}
+
+test.beforeEach(async ({ page }) => {
+  referencePage = page;
+  flowCanvasLocator = page.locator("id=hvFlow:r1:");
+});
+
+test.describe("Node", () => {
+  test("should allow to drag Assets node to the expected position", async ({
+    page,
+  }) => {
+    await page.goto("./iframe.html?args=&id=lab-flow--main&viewMode=story");
+    await page.getByRole("button", { name: "Add Node" }).click();
+
+    const baseAssetNodeLocator = page
+      .getByRole("listitem")
+      .filter({ hasText: "Asset" });
+    await baseAssetNodeLocator
+      .getByRole("button", { name: "Expand Group" })
+      .click();
+
+    const elementToDrag = baseAssetNodeLocator.getByRole("button", {
+      name: "My Asset",
+    });
+    const elementToDragBox = await elementToDrag.boundingBox();
+
+    await expect(elementToDrag).toBeVisible();
+    const destinationX = Math.floor(elementToDragBox!.width / 2) + 10;
+    const destinationY = Math.floor(elementToDragBox!.height / 2) + 30;
+    await elementToDrag.dragTo(flowCanvasLocator, {
+      targetPosition: { x: destinationX, y: destinationY },
+    });
+
+    const dragDestinationBox = await flowCanvasLocator.boundingBox();
+    const nodeInCanvasBox = await flowCanvasLocator
+      .getByRole("button", { name: "Asset" })
+      .boundingBox();
+
+    /* expect(nodeInCanvasBox?.x).toEqual(dragDestinationBox!.x + 10);
+    expect(nodeInCanvasBox?.y).toEqual(dragDestinationBox!.y + 10); */
+
+    expect(nodeInCanvasBox?.x).toBeGreaterThanOrEqual(dragDestinationBox!.x);
+    expect(nodeInCanvasBox?.x).toBeLessThanOrEqual(dragDestinationBox!.x + 20);
+    expect(nodeInCanvasBox?.y).toBeGreaterThanOrEqual(
+      dragDestinationBox!.y + 20
+    );
+    expect(nodeInCanvasBox?.y).toBeLessThanOrEqual(dragDestinationBox!.y + 40);
+  });
+
+  test("should be able to copy a flow", async ({ page }) => {
+    await page.goto("./iframe.html?args=&id=lab-flow--main&viewMode=story");
+    await page.getByRole("button", { name: "Add Node" }).click();
+
+    const node = await selectNode("ML Model", "ML Model Prediction", true);
+    await dragToPosition1(node);
+
+    const createdNode = flowCanvasLocator.getByRole("button", {
+      name: "ML Model Prediction",
+    });
+    expect(await createdNode.all()).toHaveLength(1);
+    await flowCanvasLocator
+      .getByRole("button", { name: "ML Model Prediction" })
+      .hover();
+    await page.getByRole("button", { name: "Duplicate" }).hover();
+    await expect(
+      page.getByRole("tooltip", { name: "Duplicate" })
+    ).toBeVisible();
+    await page.getByRole("button", { name: "Duplicate" }).click();
+    expect(await createdNode.all()).toHaveLength(2);
+  });
+
+  test("should be able to delete a flow", async ({ page }) => {
+    await page.goto("./iframe.html?args=&id=lab-flow--main&viewMode=story");
+    await page.getByRole("button", { name: "Add Node" }).click();
+
+    const node = await selectNode("ML Model", "ML Model Detection", true);
+    await dragToPosition3(node);
+
+    const createdNode = flowCanvasLocator.getByRole("button", {
+      name: "ML Model Detection",
+    });
+    expect(await createdNode.all()).toHaveLength(1);
+    await flowCanvasLocator
+      .getByRole("button", { name: "ML Model Detection" })
+      .hover();
+    await flowCanvasLocator.getByRole("button", { name: "Delete" }).hover();
+    await expect(page.getByRole("tooltip", { name: "Delete" })).toBeVisible();
+    await flowCanvasLocator.getByRole("button", { name: "Delete" }).click();
+    await expect(createdNode).not.toBeVisible();
+    await expect(flowCanvasLocator.getByText("Empty Flow")).toBeVisible();
+  });
+});
+
+test.describe("Connections", () => {
+  test("should be able to connect two flows", async ({ page }) => {
+    await page.goto("./iframe.html?args=&id=lab-flow--main&viewMode=story");
+    await page.getByRole("button", { name: "Add Node" }).click();
+
+    const mlNode = await selectNode("ML Model", "ML Model Detection", true);
+    await dragToPosition1(mlNode);
+    const kpiNode = await selectNode("Insight", "KPI", true);
+    await dragToPosition3(kpiNode);
+
+    /* await flowCanvasLocator.getByRole("button", {name: "ML Model Detection"}).locator(".react-flow__handle-right").hover();
+    await page.waitForTimeout(20000);
+    await page.mouse.down();
+    const destination = flowCanvasLocator.getByRole("button", {name: "KPI"}).locator(".react-flow__handle-left");
+    const destinationBox = await destination.boundingBox();
+    await page.mouse.move(destinationBox!.x - 10, destinationBox!.y, {steps: 20});
+    await page.waitForTimeout(20000);
+    await destination.hover();
+    await page.mouse.move(destinationBox!.x - 30, destinationBox!.y);
+    await destination.hover();
+    // await page.waitForTimeout(2000);
+    expect (await destination.evaluate((el) => {
+      return window.getComputedStyle(el).getPropertyValue('background-color') })).toEqual("rgb(215, 230, 207)");
+    await expect(destination).toHaveCSS("background-color", "rgb(215, 230, 207)");
+    await page.mouse.up(); 
+    
+    
+    // await expect(destination).toHaveCSS("background-color", "rgb(153, 153, 153)"); */
+
+    await expect(
+      flowCanvasLocator.getByRole("button", { name: "Edge" })
+    ).not.toBeVisible();
+    await connectNodes("ML Model Detection", "Detection", "KPI", "Data");
+    await expect(
+      flowCanvasLocator.getByRole("button", { name: "Edge" })
+    ).toBeVisible();
+  });
+
+  test("should be able to delete a connection", async ({ page }) => {
+    await page.goto("./iframe.html?args=&id=lab-flow--main&viewMode=story");
+    await page.getByRole("button", { name: "Add Node" }).click();
+
+    const assetNode = await selectNode("Dashboard", "Dashboard", true);
+    await dragToPosition1(assetNode);
+    const lineChartNode = await selectNode("Insight", "Table", true);
+    await dragToPosition3(lineChartNode);
+
+    await connectNodes("Table", "Insight", "Dashboard", "Insights");
+    await expect(
+      flowCanvasLocator.getByRole("button", { name: "Edge" })
+    ).toBeVisible();
+    await deleteConnection("Edge");
+    await expect(
+      flowCanvasLocator.getByRole("button", { name: "Edge" })
+    ).not.toBeVisible();
+  });
+
+  test("should not be able to connect incompatible flows", async ({ page }) => {
+    await page.goto("./iframe.html?args=&id=lab-flow--main&viewMode=story");
+    await page.getByRole("button", { name: "Add Node" }).click();
+
+    const assetNode = await selectNode("Asset", "My Asset", true);
+    await dragToPosition1(assetNode);
+    const lineChartNode = await selectNode("Insight", "LineChart", true);
+    await dragToPosition3(lineChartNode);
+
+    await connectNodes("My Asset", "Sensor Group 2", "LineChart", "Data");
+    await expect(
+      flowCanvasLocator.getByRole("button", { name: "Edge" })
+    ).not.toBeVisible();
+  });
+
+  test("flow allows multiple connections", async ({ page }) => {
+    await page.goto("./iframe.html?args=&id=lab-flow--main&viewMode=story");
+    await page.getByRole("button", { name: "Add Node" }).click();
+
+    const mlNode1 = await selectNode("ML Model", "ML Model Detection", true);
+    await dragToPosition1(mlNode1);
+    const mlNode2 = await selectNode("ML Model", "ML Model Prediction", false);
+    await dragToPosition2(mlNode2);
+    const tableNode = await selectNode("Insight", "Table", true);
+    await dragToPosition3(tableNode);
+
+    await connectNodes("ML Model Prediction", "Prediction", "Table", "Data");
+    await expect(
+      flowCanvasLocator.getByRole("button", { name: "Edge" })
+    ).toBeVisible();
+    await connectNodes("ML Model Detection", "Detection", "Table", "Data");
+    expect(
+      await flowCanvasLocator.getByRole("button", { name: "Edge" }).all()
+    ).toHaveLength(2);
+  });
+
+  test("connector should respect the maximum number of connections allowed", async ({
+    page,
+  }) => {
+    await page.goto(
+      "./iframe.html?args=&id=lab-flow--visualizations&viewMode=story",
+      { waitUntil: "networkidle" }
+    );
+
+    await expect(
+      flowCanvasLocator.getByRole("button", {
+        name: "Edge from jsonInput to lineChart",
+      })
+    ).toBeVisible(); // This more meaningfull name occurs in this sample but not on the other tried on this file. Should we adapt the other sample?
+    const numberConnections = (
+      await flowCanvasLocator.getByRole("button", { name: "Edge" }).all()
+    ).length;
+
+    await connectNodes("Filter", "Filtered Data", "Line Chart", "Data");
+    expect(
+      await flowCanvasLocator.getByRole("button", { name: "Edge" }).all()
+    ).toHaveLength(numberConnections);
+    await deleteConnection("Edge from jsonInput to lineChart");
+    expect(
+      await flowCanvasLocator.getByRole("button", { name: "Edge" }).all()
+    ).toHaveLength(numberConnections - 1);
+    await connectNodes("Filter", "Filtered Data", "Line Chart", "Data");
+    expect(
+      await flowCanvasLocator.getByRole("button", { name: "Edge" }).all()
+    ).toHaveLength(numberConnections);
+  });
+
+  /* test('copying a flow does not copy its connections?', async () => {
+  });
+
+  test('deleting a flow should delete its connections?', async () => {
+  }); */
+});
+
+test.describe("Interactive button", () => {
+  test("should not be able to drag nodes after interactive button is locked", async ({
+    page,
+  }) => {
+    await page.goto(
+      "./iframe.html?args=&id=lab-flow--visualizations&viewMode=story"
+    );
+
+    const inputNode = flowCanvasLocator.getByRole("button", {
+      name: "Json Input",
+    });
+    let inputBox = await inputNode.boundingBox();
+    let lineChartBox = await flowCanvasLocator
+      .getByRole("button", { name: "Line Chart" })
+      .boundingBox();
+
+    let inputLineChartDiff = inputBox!.y - lineChartBox!.y;
+    await moveUp(inputNode, 50);
+    inputBox = await inputNode.boundingBox();
+    lineChartBox = await flowCanvasLocator
+      .getByRole("button", { name: "Line Chart" })
+      .boundingBox();
+    expect(Math.floor(inputBox!.y - lineChartBox!.y)).toEqual(
+      Math.floor(inputLineChartDiff - 50)
+    );
+
+    await page.getByRole("button", { name: "Interactive" }).click();
+
+    inputLineChartDiff = inputBox!.y - lineChartBox!.y;
+    await moveUp(inputNode, 50);
+    inputBox = await inputNode.boundingBox();
+    lineChartBox = await flowCanvasLocator
+      .getByRole("button", { name: "Line Chart" })
+      .boundingBox();
+    expect(Math.floor(inputBox!.y - lineChartBox!.y)).toEqual(
+      Math.floor(inputLineChartDiff)
+    );
+  });
+
+  test("should not be able to duplicate/delete flows after interactive button is locked", async ({
+    page,
+  }) => {
+    await page.goto("./iframe.html?args=&id=lab-flow--main&viewMode=story");
+    await page.getByRole("button", { name: "Add Node" }).click();
+
+    const node = await selectNode("ML Model", "ML Model Prediction", true);
+    await dragToPosition1(node);
+
+    const nodeBox = await flowCanvasLocator
+      .getByRole("button", { name: "ML Model Prediction" })
+      .boundingBox();
+    await page.mouse.move(nodeBox!.x + 30, nodeBox!.y + 30);
+    await expect(page.getByRole("button", { name: "Duplicate" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Delete" })).toBeVisible();
+
+    await page.getByRole("button", { name: "Interactive" }).click();
+
+    await page.mouse.move(nodeBox!.x + 30, nodeBox!.y + 30);
+    await expect(
+      page.getByRole("button", { name: "Duplicate" })
+    ).not.toBeVisible();
+    await expect(
+      page.getByRole("button", { name: "Delete" })
+    ).not.toBeVisible();
+  });
+
+  /* test('fit to view button?', async () => {
+  }); */
+});
---
+++ playwright.config.ts
@@ -0,0 +1,96 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testMatch: "**/?(*.)@(spec).*",
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : 4,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "html",
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    headless: true,
+    baseURL: process.env.CI ? process.env.URL : "http://localhost:6006",
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: {
+          width: 1920,
+          height: 1080,
+        },
+      },
+    },
+
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        viewport: {
+          width: 1920,
+          height: 1080,
+        },
+      },
+    },
+
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+        viewport: {
+          width: 1920,
+          height: 1080,
+        },
+      },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  /* webServer: {
+     command: 'npm run doc',
+     port: 6006,
+     timeout: 500000,
+    }, */
+});
