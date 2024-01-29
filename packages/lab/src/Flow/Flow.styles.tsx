e5a1586d7f659d38b0c8c07b83010a4f52e27a0b
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-01-29 12:55:25 +0000

    fix(FlowNode): handle content when already connected

--- packages/lab/src/Flow/Flow.styles.tsx
+++ packages/lab/src/Flow/Flow.styles.tsx
@@ -5,12 +5,6 @@ import { flowBaseNodeClasses } from "./Node";
 export const { staticClasses, useClasses } = createClasses("HvFlow", {
   root: {
     height: "100%",
-    "& .react-flow__handle-connecting": {
-      backgroundColor: theme.colors.negative_20,
-    },
-    "& .react-flow__handle-valid": {
-      backgroundColor: theme.colors.positive_20,
-    },
     [`& .selected > .${flowBaseNodeClasses.root}`]: {
       border: `1px solid ${theme.colors.secondary_60}`,
       borderRadius: theme.radii.round,
