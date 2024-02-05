0b5c0d76ab0df47ae45513b2081f49a18c198467
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-02-05 09:48:07 +0000

    fix(TableSection): styles

--- packages/core/src/TableSection/TableSection.styles.tsx
+++ packages/core/src/TableSection/TableSection.styles.tsx
@@ -2,26 +2,17 @@ import { theme } from "@hitachivantara/uikit-styles";
 
 import { bulkActionsClasses } from "../BulkActions";
 import { paginationClasses } from "../Pagination";
-import { sectionClasses } from "../Section";
-
 import { tableCellClasses } from "../Table/TableCell";
 import { tableContainerClasses } from "../Table/TableContainer";
 import { tableHeaderClasses } from "../Table/TableHeader";
-
 import { createClasses } from "../utils/classes";
+import { tableRowClasses } from "../Table/TableRow";
 
 export const { staticClasses, useClasses } = createClasses("HvTableSection", {
-  root: {
-    padding: 0,
-  },
+  root: {},
   header: {
-    padding: theme.spacing("xs", "xs", "xs", "sm"),
-    borderBottom: `1px solid ${theme.colors.atmo3}`,
-    [`&.${sectionClasses.raisedHeader}`]: {
-      "+ div": {
-        paddingTop: `0`,
-      },
-    },
+    // Only apply the border to divide the header and content when both are displayed
+    "+ div": { borderTop: `1px solid ${theme.colors.atmo3}` },
   },
   actions: {
     right: theme.space.sm,
@@ -29,44 +20,77 @@ export const { staticClasses, useClasses } = createClasses("HvTableSection", {
   content: {
     marginTop: 0,
     padding: 0,
-    [`&&`]: {
-      paddingTop: 0,
+
+    // Apply border radius to the first child if there's not an header
+    "&:first-of-type": {
+      "& > :first-of-type": {
+        borderTopLeftRadius: theme.radii.round,
+        borderTopRightRadius: theme.radii.round,
+      },
     },
+
+    "& > :last-child": {
+      borderBottomLeftRadius: theme.radii.round,
+      borderBottomRightRadius: theme.radii.round,
+    },
+
     [`& .${tableContainerClasses.root}`]: {
       paddingBottom: 0,
-      borderRadius: `${theme.radii.round} ${theme.radii.round} 0 0`,
     },
+
     [`& .${tableHeaderClasses.head}`]: {
       backgroundColor: theme.colors.atmo2,
       borderBottomColor: theme.colors.atmo3,
       [`&.${tableHeaderClasses.variantCheckbox}`]: {
         borderRight: "none",
       },
+      [`&.${tableHeaderClasses.variantActions}`]: {
+        borderLeft: "none",
+      },
     },
+
+    // Remove border for the last table row
+    [`& .${tableRowClasses.root}`]: {
+      "&:last-child": {
+        [`& .${tableCellClasses.root}`]: { border: "none" },
+      },
+    },
+
     [`& .${tableCellClasses.root}`]: {
       borderBottomColor: theme.colors.atmo3,
     },
+
     [`& .${tableCellClasses.variantCheckbox}`]: {
       borderRight: "none",
     },
+
+    [`& .${tableCellClasses.variantActions}`]: {
+      borderLeft: "none",
+    },
+
     [`& .${bulkActionsClasses.root}`]: {
       marginBottom: 0,
       border: "none",
       borderBottom: `1px solid ${theme.colors.atmo3}`,
       padding: theme.spacing("xs", "sm"),
-      borderRadius: `${theme.radii.round} ${theme.radii.round} 0 0`,
     },
+
     [`& .${paginationClasses.root}`]: {
       margin: 0,
       backgroundColor: theme.colors.atmo2,
       padding: theme.space.xs,
-      borderRadius: `0 0 ${theme.radii.round} ${theme.radii.round}`,
+      borderTop: `1px solid ${theme.colors.atmo3}`,
     },
+
     [`& .${paginationClasses.pageSizeOptions}`]: {
       left: theme.space.sm,
     },
   },
   hidden: {},
-  raisedHeader: {},
-  spaceTop: {},
+  raisedHeader: {
+    "+ div": {
+      paddingTop: 0,
+    },
+  },
+  spaceTop: { paddingTop: 0 },
 });
