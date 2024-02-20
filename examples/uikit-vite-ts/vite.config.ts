7b5429cc00c77acde110b833a8e40c67f6e4f1f2
Bruno Henriques <zettca@users.noreply.github.com>
2024-02-20 18:54:36 +0000

    docs(examples): add unocss to vite templates

--- examples/uikit-vite-ts/vite.config.ts
+++ examples/uikit-vite-ts/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import unoCSS from "unocss/vite";
+import { presetHv } from "@hitachivantara/uikit-uno-preset";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    unoCSS({
+      presets: [presetHv()],
+    }),
+  ],
 });
