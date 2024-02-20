000ec6244b92d86bfe08bd9e6f35557d0ad96ee6
Bruno Henriques <zettca@users.noreply.github.com>
2024-02-20 18:54:36 +0000

    docs(examples): add unocss to vite templates

--- examples/uikit-app-shell/vite.config.ts
+++ examples/uikit-app-shell/vite.config.ts
@@ -5,7 +5,9 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import unoCSS from "unocss/vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import { presetHv } from "@hitachivantara/uikit-uno-preset";
 import { HvAppShellVitePlugin } from "@hitachivantara/app-shell-vite-plugin";
 
 export default defineConfig(({ mode }) => ({
@@ -17,6 +19,13 @@ export default defineConfig(({ mode }) => ({
       },
     }),
     tsconfigPaths(),
+    unoCSS({
+      mode: "per-module",
+      presets: [presetHv()],
+    }),
+    cssInjectedByJsPlugin({
+      relativeCSSInjection: true,
+    }),
     HvAppShellVitePlugin({
       mode,
       autoMenu: true,
--- examples/uikit-vite-ts/src/main.tsx
+++ examples/uikit-vite-ts/src/main.tsx
@@ -5,6 +5,8 @@ import { HvProvider } from "@hitachivantara/uikit-react-core";
 import { Container } from "./Container";
 import { App } from "./App";
 
+import "virtual:uno.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HvProvider>
