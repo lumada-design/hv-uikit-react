d77c0ef5383c2c562dfb1ede9dc01ef107887ccc
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-03-08 17:53:59 +0000

    feat(select): button size and variant

---
+++ packages/core/src/DropdownButton/DropdownButton.styles.tsx
@@ -0,0 +1,60 @@
+import { theme } from "@hitachivantara/uikit-styles";
+
+import { createClasses } from "../utils/classes";
+import { buttonClasses } from "../Button";
+
+const disabledStyle = {
+  backgroundColor: theme.colors.atmo3,
+  borderColor: theme.colors.atmo3,
+  [`&.${buttonClasses.subtle}`]: {
+    backgroundColor: theme.colors.atmo3,
+    borderColor: theme.colors.atmo3,
+  },
+  [`&.${buttonClasses.ghost}`]: {
+    backgroundColor: theme.colors.atmo3,
+    borderColor: theme.colors.atmo3,
+  },
+};
+
+export const { staticClasses, useClasses } = createClasses("HvDropdownButton", {
+  root: {
+    userSelect: "none",
+    position: "relative",
+
+    [`&:not(.${buttonClasses.icon})`]: {
+      width: "100%",
+      minWidth: "unset",
+      justifyContent: "flex-start",
+      paddingLeft: theme.space.xs,
+    },
+  },
+  disabled: {
+    ...disabledStyle,
+  },
+  readOnly: {
+    userSelect: "text",
+    ...disabledStyle,
+  },
+  open: {},
+  openUp: {
+    borderRadius: `0px 0px ${theme.radii.base} ${theme.radii.base}`,
+  },
+  openDown: {
+    borderRadius: `${theme.radii.base} ${theme.radii.base} 0px 0px`,
+  },
+  selection: {
+    color: "inherit",
+    flex: 1,
+    textAlign: "start",
+    overflow: "clip visible",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+  },
+  placeholder: {},
+  arrowContainer: {
+    marginRight: theme.spacing(-2),
+  },
+  arrow: {
+    transition: "rotate 0.2s ease",
+  },
+});
