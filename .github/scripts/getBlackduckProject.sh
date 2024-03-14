bc907fc12ddeacbc8ac8dd15173e53e140763df8
VGervasio <vasco.gervasio@hitachivantara.com>
2024-03-14 17:43:45 +0000

    ci: update BlackDuck scans

---
+++ .github/scripts/getBlackduckProject.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+case $1 in
+"code-editor") echo "react-code-editor";;
+"core") echo "react-core";;
+"icons") echo "react-icons";;
+"lab") echo "react-lab";;
+"shared") echo "react-shared";;
+"styles") echo "styles";;
+"uno-preset") echo "react-uno-preset";;
+"viz") echo "react-viz";;
+esac
