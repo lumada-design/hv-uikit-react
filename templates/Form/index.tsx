2751e2a856a37fb237106ed3d19da04465543514
Bruno Henriques <zettca@users.noreply.github.com>
2024-03-25 09:49:24 +0000

    docs(templates): review Form template

--- templates/Form/index.tsx
+++ templates/Form/index.tsx
@@ -1,231 +1,103 @@
-import { Formik } from "formik";
-import * as yup from "yup";
+import { Phone } from "@hitachivantara/uikit-react-icons";
 import {
-  HvGrid,
-  HvGlobalActions,
-  HvButton,
   HvInput,
+  HvButton,
+  HvCheckBox,
+  HvTimePicker,
+  HvRadioGroup,
+  HvRadio,
+  HvGrid,
+  HvDatePicker,
   HvTextArea,
-  HvFormStatus,
+  HvSelect,
+  HvOption,
 } from "@hitachivantara/uikit-react-core";
-import { Map } from "@hitachivantara/uikit-react-icons";
 
-import { fields, allCountries } from "./utils";
-import classes from "./styles";
+const countries = [
+  { id: "pt", label: "Portugal" },
+  { id: "sp", label: "Spain" },
+  { id: "fr", label: "France" },
+  { id: "de", label: "Germany" },
+  { id: "us", label: "United States" },
+];
 
-/**
- * The validation schema used by Formik to validate the data inserted by the user.
- */
-const validationSchema = yup.object({
-  asset: yup.string().required(fields.asset.required),
-  location: yup.string().required(fields.location.required),
-  project: yup.string().required(fields.project.required),
-  version: yup
-    .number()
-    .typeError("Version must be a number")
-    .required(fields.version.required),
-  name: yup.string().required(fields.name.required),
-  description: yup.string().required(fields.description.required).max(256),
-  date: yup.date(),
-  time: yup.object().shape({
-    hours: yup.number(),
-    minutes: yup.number(),
-    seconds: yup.number(),
-    period: yup.string(),
-  }),
-  public: yup.bool(),
-});
-
-/**
- * Handler for the submission of the form.
- *
- * @param {Object} data - the data inserted on the form.
- */
-const onSubmit = (data, { setSubmitting }) => {
-  setSubmitting(false);
-  alert(`Formik: ${JSON.stringify(data)}`);
-};
-
-/**
- * Handler for the suggestions on the location input.
- *
- * @param {String} val - the value entered on the input
- */
-const suggestionHandler = (val) => {
-  if (typeof val !== "string" || !val) return null;
-  const foundCountries = allCountries.filter((country) =>
-    country.toUpperCase().startsWith(val.toUpperCase())
-  );
-
-  if (foundCountries.length === 0) return null;
-
-  return foundCountries.map((country, idx) => ({
-    id: `c_${idx}`,
-    label: country,
-  }));
-};
-
-const Form = () => {
-  return (
-    <>
-      <HvGlobalActions title="Deploy" className={classes.section} />
-
-      <Formik
-        initialValues={{
-          asset: "",
-          location: "",
-          project: "",
-          version: undefined,
-          name: "",
-          description: "",
-          date: undefined,
-          time: undefined,
-          public: false,
-        }}
-        onSubmit={onSubmit}
-        validationSchema={validationSchema}
-      >
-        {(props) => {
-          const {
-            errors,
-            touched,
-            handleSubmit,
-            setFieldValue,
-            setFieldTouched,
-            isValid,
-            isSubmitting,
-            values,
-            initialValues,
-          } = props;
-          // Parse the status for the field
-          const parseStatus = (name) => {
-            return errors[name] && touched[name] ? "invalid" : "valid";
-          };
-
-          // Get the status message for the field
-          const parseStatusMessage = (name) => {
-            return errors[name] && touched[name] ? errors[name] : "";
-          };
-
-          // Get generic status properties for each field
-          const fieldStatusProps = (id) => {
-            return {
-              status: parseStatus(id) as HvFormStatus,
-              statusMessage: parseStatusMessage(id),
-              onChange: (evt, value) => {
-                setFieldTouched(id);
-                setFieldValue(id, value);
-              },
-            };
-          };
-
-          return (
-            <form onSubmit={handleSubmit}>
-              <HvGrid container className={classes.section}>
-                <HvGrid item xs={12}>
-                  <HvGlobalActions title="Project Details" variant="section" />
-                </HvGrid>
-                <HvGrid item md={6} xs={12}>
-                  <HvInput
-                    id={fields.asset.id}
-                    label={fields.asset.label}
-                    required
-                    placeholder={fields.asset.placeholder}
-                    {...fieldStatusProps(fields.asset.id)}
-                  />
-                </HvGrid>
-
-                <HvGrid item md={6} xs={12}>
-                  <HvInput
-                    id={fields.location.id}
-                    label={fields.location.label}
-                    required
-                    placeholder={fields.location.placeholder}
-                    suggestionListCallback={suggestionHandler}
-                    endAdornment={<Map />}
-                    {...fieldStatusProps(fields.location.id)}
-                  />
-                </HvGrid>
-
-                <HvGrid item md={6} xs={12}>
-                  <HvInput
-                    id={fields.project.id}
-                    label={fields.project.label}
-                    required
-                    placeholder={fields.project.placeholder}
-                    {...fieldStatusProps(fields.project.id)}
-                  />
-                </HvGrid>
-
-                <HvGrid item md={6} xs={12}>
-                  <HvInput
-                    id={fields.version.id}
-                    label={fields.version.label}
-                    required
-                    placeholder={fields.version.placeholder}
-                    type="number"
-                    {...fieldStatusProps(fields.version.id)}
-                  />
-                </HvGrid>
-
-                <HvGrid item xs={12}>
-                  <HvInput
-                    id={fields.name.id}
-                    label={fields.name.label}
-                    required
-                    placeholder={fields.name.placeholder}
-                    {...fieldStatusProps(fields.name.id)}
-                  />
-                </HvGrid>
-
-                <HvGrid item xs={12}>
-                  <HvTextArea
-                    id={fields.description.id}
-                    label={fields.description.label}
-                    required
-                    rows={5}
-                    maxCharQuantity={256}
-                    placeholder={fields.description.placeholder}
-                    {...fieldStatusProps(fields.description.id)}
-                  />
-                </HvGrid>
-              </HvGrid>
-
-              <div className={classes.footer}>
-                <HvButton
-                  id="submit"
-                  type="submit"
-                  disabled={
-                    isSubmitting ||
-                    !isValid ||
-                    values.asset.trim() === initialValues.asset.trim() ||
-                    values.location.trim() === initialValues.location.trim() ||
-                    values.project.trim() === initialValues.project.trim() ||
-                    values.version === initialValues.version ||
-                    values.name.trim() === initialValues.name.trim() ||
-                    values.description.trim() ===
-                      initialValues.description.trim()
-                  }
-                  variant="primaryGhost"
-                >
-                  Submit
-                </HvButton>
-                <HvButton
-                  id="cancel"
-                  disabled={values === initialValues}
-                  variant="primaryGhost"
-                  type="reset"
-                  onClick={() => alert("Cancelling from submission.")}
-                >
-                  Cancel
-                </HvButton>
-              </div>
-            </form>
-          );
-        }}
-      </Formik>
-    </>
-  );
-};
-
-export default Form;
+export default () => (
+  <form
+    autoComplete="on"
+    onSubmit={(event) => {
+      event.preventDefault();
+      const formData = new FormData(event.currentTarget);
+      alert(JSON.stringify(Object.fromEntries(formData), null, 2));
+    }}
+  >
+    <HvGrid container maxWidth="md" rowSpacing="xs">
+      <HvGrid item xs={12} sm={6}>
+        <HvInput required name="name" label="Full Name" />
+      </HvGrid>
+      <HvGrid item xs={12} sm={6}>
+        <HvInput required type="email" name="email" label="Email" />
+      </HvGrid>
+      <HvGrid item xs={12} sm={6}>
+        <HvInput name="street-address" label="Address" />
+      </HvGrid>
+      <HvGrid item xs={12} sm={6}>
+        <HvSelect
+          required
+          name="country"
+          label="Country"
+          statusMessage="Country is required"
+        >
+          {countries.map(({ id, label }) => (
+            <HvOption key={id} value={id}>
+              {label}
+            </HvOption>
+          ))}
+        </HvSelect>
+      </HvGrid>
+      <HvGrid item xs={12} sm={6}>
+        <HvInput name="tel" label="Phone number" endAdornment={<Phone />} />
+      </HvGrid>
+      <HvGrid item xs={12} sm={6}>
+        <HvInput type="password" name="password" label="Password" />
+      </HvGrid>
+      <HvGrid item xs={12} component="hr" />
+      <HvGrid item xs={12} sm={6}>
+        <HvDatePicker name="bday" label="Birthday" placeholder="Select date" />
+      </HvGrid>
+      <HvGrid item xs={12} sm={6}>
+        <HvTimePicker name="startTime" label="Start time" />
+      </HvGrid>
+      <HvGrid item xs={12} component="hr" />
+      <HvGrid item xs={12}>
+        <HvTextArea
+          required
+          name="description"
+          description="Write a short description"
+          label="Description"
+          rows={3}
+          inputProps={{ minLength: 16, maxLength: 128 }}
+          minCharQuantity={16}
+          maxCharQuantity={128}
+        />
+      </HvGrid>
+      <HvGrid item xs={12}>
+        <HvRadioGroup required name="sex" label="Sex" orientation="horizontal">
+          <HvRadio value="male" label="Male" />
+          <HvRadio value="female" label="Female" />
+          <HvRadio value="other" label="Other" />
+        </HvRadioGroup>
+      </HvGrid>
+      <HvGrid item xs={12}>
+        <HvCheckBox
+          defaultChecked
+          name="subscribe"
+          label="Subscribe to newsletter"
+          value="yes"
+        />
+      </HvGrid>
+      <HvGrid item xs={12}>
+        <HvButton type="submit">Submit</HvButton>
+      </HvGrid>
+    </HvGrid>
+  </form>
+);
