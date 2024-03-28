b060be3bcb1e778d7248e8895334a70bb9dc5c22
Vasco Manuel Rolao Gervasio <vasco.gervasio@hitachivantara.com>
2024-03-28 17:32:41 +0000

    test: adapt a11y checks (#4100)
    * test: adapt a11y checks
    * test: remove storybook-a11y-report and related dependencies
    * test: fail step in job but pass job itself

---
+++ .storybook/test-runner.ts
@@ -0,0 +1,79 @@
+import fs from "fs";
+import { getStoryContext, type TestRunnerConfig } from "@storybook/test-runner";
+import { NodeResult, Result } from "axe-core";
+import { getAxeResults, injectAxe } from "axe-playwright";
+
+const excludeStories = [
+  "Overview",
+  "Foundation",
+  "Guides",
+  "Templates",
+  "Visualizations/Bar Chart",
+  "Visualizations/Line Chart",
+  "Visualizations/Donut Chart",
+  "Visualizations/Confusion Matrix",
+  "Widgets/Code Editor",
+];
+
+function getNodesIds(nodes: NodeResult[]) {
+  return nodes.map((n) => `\n\t\t${n.target}`).join();
+}
+
+function getResultSummary(type: string, results: Result[]) {
+  return results
+    .map((r) => `\n\t${type}: ${r.help} - ${r.impact} ${getNodesIds(r.nodes)}`)
+    .join("");
+}
+
+function writeAxeResults(
+  storyTitle: string,
+  storyName: string,
+  type: string,
+  results: Result[],
+) {
+  if (results.length > 0) {
+    const fileName = `a11y_${type}_Results.txt`;
+    fs.appendFileSync(fileName, `\n${storyTitle} - ${storyName}`, "utf-8");
+    fs.appendFileSync(fileName, getResultSummary(type, results), "utf8");
+  }
+}
+
+/*
+ * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
+ * to learn more about the test-runner hooks API.
+ */
+const config: TestRunnerConfig = {
+  async preVisit(page) {
+    await injectAxe(page);
+  },
+
+  async postVisit(page, context) {
+    const storyContext = await getStoryContext(page, context);
+    const validStories = excludeStories.filter((s) =>
+      storyContext.title.includes(s),
+    );
+    if (validStories.length != 0) return;
+
+    const axeResults = await getAxeResults(page, "#storybook-root", {
+      rules: {
+        // Disable color contrast rule
+        "color-contrast": { enabled: false },
+      },
+    });
+
+    writeAxeResults(
+      storyContext.title,
+      storyContext.name,
+      "Violation",
+      axeResults.violations,
+    );
+    writeAxeResults(
+      storyContext.title,
+      storyContext.name,
+      "Incomplete",
+      axeResults.incomplete,
+    );
+  },
+};
+
+export default config;
