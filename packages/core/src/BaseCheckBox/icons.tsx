41da8676c27a7bbccd73e632e42b96d4a883cfc6
Bruno Henriques <zettca@users.noreply.github.com>
2024-04-01 18:37:58 +0100

    fix(a11y): fix issues

--- packages/core/src/BaseCheckBox/icons.tsx
+++ packages/core/src/BaseCheckBox/icons.tsx
@@ -1,26 +1,23 @@
 export const Box = () => {
   return (
-    <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
-      <path fill="currentcolor" d="m0,0l16,0l0,16l-16,0l0,-16z" />
+    <svg viewBox="0 0 14 14" fill="currentColor">
+      <path d="m0,0l16,0l0,16l-16,0l0,-16z" />
     </svg>
   );
 };
 
 export const Check = () => {
   return (
-    <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="m5.03,12.06l-3.76,-3.75l1.42,-1.42l2.24,2.25l6.3,-7.2l1.5,1.31l-7.7,8.81z"
-        fill="currentcolor"
-      />
+    <svg viewBox="0 0 14 14" fill="currentColor">
+      <path d="m5.03,12.06l-3.76,-3.75l1.42,-1.42l2.24,2.25l6.3,-7.2l1.5,1.31l-7.7,8.81z" />
     </svg>
   );
 };
 
 export const Partial = () => {
   return (
-    <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
-      <path id="svg_2" d="m3,8l8,0l0,-2l-8,0l0,2z" fill="currentcolor" />
+    <svg viewBox="0 0 14 14" fill="currentColor">
+      <path d="m3,8l8,0l0,-2l-8,0l0,2z" />
     </svg>
   );
 };
