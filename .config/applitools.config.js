587e8365274e587b50b4015bb13d7b13f1a8a2f1
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-04-02 12:22:33 +0100

    chore: chromatic and applitools workflow combined

--- .config/applitools.config.js
+++ .config/applitools.config.js
@@ -1,19 +1,3 @@
-// Exclude everything and include at the story level
-const isExcludedPath = (kind) => {
-  const excludePaths = [
-    "Overview/",
-    "Foundation/",
-    "Guides/",
-    "Components/",
-    "Widgets/",
-    "Templates/",
-    "Visualizations/",
-    "Tests/",
-    "Lab/",
-  ];
-  return excludePaths.some((p) => kind.includes(p));
-};
-
 module.exports = {
   matchLevel: "Strict",
 
@@ -33,8 +17,6 @@ module.exports = {
     { width: 1024, height: 768, name: "safari" },
     { width: 1024, height: 768, name: "edgechromium" },
   ],
-
-  include: ({ kind }) => !isExcludedPath(kind),
 
   testConcurrency: 20,
 };
