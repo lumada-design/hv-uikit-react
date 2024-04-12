e783755d9a08c27d994e96715f8f66305a9122ff
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2024-04-12 16:24:49 +0100

    feat: add Treemap viz

--- packages/viz/src/index.ts
+++ packages/viz/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./LineChart";
 export * from "./BarChart";
 export * from "./DonutChart";
+export * from "./Treemap";
 export * from "./ConfusionMatrix";
 export * from "./ScatterPlot";
 
