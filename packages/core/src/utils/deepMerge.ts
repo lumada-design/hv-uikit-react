af83d3de3e5614b2dcf1e59a2819a4547863c8e1
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-04-16 17:28:40 +0100

    fix(useLabels): deep merge labels

---
+++ packages/core/src/utils/deepMerge.ts
@@ -0,0 +1,21 @@
+import { DeepPartial } from "../types/generic";
+
+const isObject = (val: any): val is Object =>
+  val && typeof val === "object" && !Array.isArray(val);
+
+function merge<T>(target: T, source?: DeepPartial<T>) {
+  Object.keys(source || {}).forEach((key) => {
+    if (isObject(target[key]) && isObject(source?.[key])) {
+      merge(target[key], source?.[key]);
+    } else {
+      target[key] = source?.[key];
+    }
+  });
+}
+
+/** Merges recursively all keys of source into target returning the resulting object. */
+export function deepMerge<T>(target: T, source?: DeepPartial<T>): T {
+  const result = structuredClone(target);
+  merge(result, source);
+  return result;
+}
