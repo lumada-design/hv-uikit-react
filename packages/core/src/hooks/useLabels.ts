7469280623f9726e3e60857196fad562fdf99c8c
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-04-16 17:28:40 +0100

    fix(useLabels): deep merge labels

--- packages/core/src/hooks/useLabels.ts
+++ packages/core/src/hooks/useLabels.ts
@@ -1,7 +1,10 @@
 import { useMemo } from "react";
 
-export function useLabels<T>(defaultLabels: T, labels?: Partial<T>): T {
+import { DeepPartial } from "../types/generic";
+import { deepMerge } from "../utils/deepMerge";
+
+export function useLabels<T>(defaultLabels: T, labels?: DeepPartial<T>): T {
   return useMemo(() => {
-    return { ...defaultLabels, ...labels };
+    return deepMerge(defaultLabels, labels);
   }, [defaultLabels, labels]);
 }
