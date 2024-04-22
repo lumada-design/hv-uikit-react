7ef5acda1cb8e5d03ce932748f1415ec7ab30bdc
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-04-22 15:34:11 +0100

    chore: improve vertical navigation code sample and remove unnecessary useEffect

---
+++ packages/core/src/VerticalNavigation/stories/CollapsibleIconsWithoutSubItems.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import {
+  HvVerticalNavigation,
+  HvVerticalNavigationAction,
+  HvVerticalNavigationActions,
+  HvVerticalNavigationHeader,
+  HvVerticalNavigationTree,
+} from "@hitachivantara/uikit-react-core";
+import {
+  BarChart,
+  Cloud,
+  Deploy,
+  LogOut,
+  Open,
+  User,
+} from "@hitachivantara/uikit-react-icons";
+
+const navigationData = [
+  { id: "00", label: "Installation Overview", icon: <Open /> },
+  {
+    id: "01",
+    label: "Hardware",
+    icon: <BarChart />,
+  },
+  {
+    id: "02",
+    label: "System",
+    icon: <Deploy />,
+  },
+  {
+    id: "03",
+    label: "System 2",
+    icon: <Cloud />,
+  },
+];
+
+export const CollapsibleIconsWithoutSubItems = () => {
+  const [value, setValue] = useState("01-01");
+  const [show, setShow] = useState(false);
+
+  const handleIsExpanded = () => {
+    setShow(!show);
+  };
+
+  return (
+    <div style={{ display: "flex", width: 220, height: 530 }}>
+      <HvVerticalNavigation open={show} useIcons>
+        <HvVerticalNavigationHeader
+          title="Menu"
+          onCollapseButtonClick={handleIsExpanded}
+          collapseButtonProps={{
+            "aria-label": "collapseButton",
+            "aria-expanded": show,
+          }}
+        />
+        <HvVerticalNavigationTree
+          collapsible
+          defaultExpanded
+          aria-label="Example 3 navigation"
+          selected={value}
+          onChange={(event, data) => setValue(data.id)}
+          data={navigationData}
+        />
+        <HvVerticalNavigationActions>
+          <HvVerticalNavigationAction label="Profile" icon={<User />} />
+          <HvVerticalNavigationAction label="Logout" icon={<LogOut />} />
+        </HvVerticalNavigationActions>
+      </HvVerticalNavigation>
+    </div>
+  );
+};
---
+++ packages/core/src/VerticalNavigation/stories/TreeViewMode.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import {
+  HvVerticalNavigation,
+  HvVerticalNavigationAction,
+  HvVerticalNavigationActions,
+  HvVerticalNavigationTree,
+} from "@hitachivantara/uikit-react-core";
+import { LogOut, User } from "@hitachivantara/uikit-react-icons";
+
+const navigationData = [
+  { id: "00", label: "Installation Overview" },
+  {
+    id: "01",
+    label: "Hardware",
+    data: [
+      {
+        id: "01-01",
+        label: "Ambient Monitoring",
+      },
+      {
+        id: "01-02",
+        label: "Server Status Summary",
+      },
+    ],
+  },
+  {
+    id: "02",
+    label: "System",
+    data: [
+      {
+        id: "02-01",
+        label: "Buckets",
+      },
+      {
+        id: "02-02",
+        label: "Admin Users",
+      },
+      {
+        id: "02-03",
+        label: "Log Bundle",
+        data: [
+          {
+            id: "02-03-01",
+            label: "Rest API",
+          },
+          {
+            id: "02-03-02",
+            label: "License",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const TreeViewMode = () => {
+  const [value, setValue] = useState("01-01");
+
+  return (
+    <div style={{ display: "flex", width: 220, height: 530 }}>
+      <HvVerticalNavigation>
+        <HvVerticalNavigationTree
+          mode="treeview"
+          collapsible
+          defaultExpanded
+          aria-label="Example 3 navigation"
+          selected={value}
+          onChange={(event, data) => setValue(data.id)}
+          data={navigationData}
+        />
+        <HvVerticalNavigationActions>
+          <HvVerticalNavigationAction label="Profile" icon={<User />} />
+          <HvVerticalNavigationAction label="Logout" icon={<LogOut />} />
+        </HvVerticalNavigationActions>
+      </HvVerticalNavigation>
+    </div>
+  );
+};
