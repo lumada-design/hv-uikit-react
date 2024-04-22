a680029e48462881e7ba4486574944f2740f372c
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-04-22 15:12:04 +0100

    chore: fix SliderMode story

---
+++ packages/core/src/VerticalNavigation/stories/SliderMode.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import {
+  HvVerticalNavigation,
+  HvVerticalNavigationHeader,
+  HvVerticalNavigationTree,
+} from "@hitachivantara/uikit-react-core";
+import { BarChart, Deploy, Open } from "@hitachivantara/uikit-react-icons";
+
+const navigationData = [
+  {
+    id: "menu1",
+    label: "Menu 1",
+    path: "",
+    icon: <Open />,
+    data: [
+      {
+        id: "menu1-1",
+        label: "Menu 1-1",
+        path: "",
+        icon: <Open />,
+        parent: null,
+      },
+      {
+        id: "menu1-2",
+        label: "Menu 1-2",
+        path: "",
+        icon: <BarChart />,
+        data: [
+          {
+            id: "menu1-2-1",
+            label: "Menu 1-2-1",
+            path: "",
+            icon: <Open />,
+            parent: null,
+          },
+          {
+            id: "menu1-2-2",
+            label: "Menu 1-2-2",
+            path: "",
+            icon: <BarChart />,
+            parent: null,
+          },
+          {
+            id: "menu1-2-3",
+            label: "Menu 1-2-3",
+            path: "",
+            icon: <Deploy />,
+            parent: null,
+          },
+        ],
+        parent: null,
+      },
+      {
+        id: "menu1-3",
+        label: "Menu 1-3",
+        path: "",
+        icon: <Deploy />,
+        parent: null,
+      },
+    ],
+    parent: null,
+  },
+  {
+    id: "menu2",
+    label: "Menu 2 with a very big name that should be truncated",
+    path: "",
+    icon: <BarChart />,
+    parent: null,
+  },
+  {
+    id: "menu3",
+    label: "Menu 3",
+    path: "",
+    icon: <Deploy />,
+    parent: null,
+  },
+];
+
+export const SliderMode = () => {
+  const [value, setValue] = useState("menu1-3");
+
+  return (
+    <div style={{ display: "flex", width: 220, height: 530 }}>
+      <HvVerticalNavigation open slider>
+        <HvVerticalNavigationHeader title="Menu" />
+        <HvVerticalNavigationTree
+          collapsible
+          defaultExpanded
+          aria-label="Example 4 Slider Mode"
+          selected={value}
+          onChange={(event, data) => setValue(data.id)}
+          data={navigationData}
+        />
+      </HvVerticalNavigation>
+    </div>
+  );
+};
