061ffe3e236eb2e94572aeb5425d2801b2a156c4
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-05-09 16:37:18 +0100

    feat: new filters added to visualizations

--- packages/viz/src/types/filter.ts
+++ packages/viz/src/types/filter.ts
@@ -2,11 +2,16 @@ export type HvChartFilterOperation =
   | "is"
   | "isNot"
   | "contains"
+  | "notContains"
   | "greaterThan"
   | "greaterThanOrEqual"
   | "lessThan"
   | "lessThanOrEqual"
-  | "between";
+  | "between"
+  | "ends"
+  | "notEnds"
+  | "starts"
+  | "notStarts";
 
 export type HvChartFilter = {
   field: string;
