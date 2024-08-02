302cd23c3f16875f8651fb9132d1e81844b54471
hqfox <henrique.raposo95@gmail.com>
2024-08-02 12:15:34 +0100

    chore: update to leverage polymorphic component ref

--- packages/core/src/InlineEditor/InlineEditor.tsx
+++ packages/core/src/InlineEditor/InlineEditor.tsx
@@ -6,7 +6,7 @@ import { useControlled } from "../hooks/useControlled";
 import { useDefaultProps } from "../hooks/useDefaultProps";
 import { useTheme } from "../hooks/useTheme";
 import { HvInput, HvInputProps } from "../Input";
-import { HvBaseProps } from "../types/generic";
+import { fixedForwardRef, HvBaseProps, PolymorphicRef } from "../types/generic";
 import {
   HvTypography,
   HvTypographyProps,
@@ -20,8 +20,9 @@ export { staticClasses as inlineEditorClasses };
 
 export type HvInlineEditorClasses = ExtractNames<typeof useClasses>;
 
-export interface HvInlineEditorProps<T extends React.ElementType>
-  extends HvBaseProps<HTMLDivElement, "onBlur" | "onChange"> {
+export interface HvInlineEditorProps<
+  C extends React.ElementType = typeof HvInput,
+> extends HvBaseProps<HTMLDivElement, "onBlur" | "onChange"> {
   /** The value of the form element. */
   value?: string;
   /** The default value of the form element. */
@@ -29,7 +30,7 @@ export interface HvInlineEditorProps<T extends React.ElementType>
   /** Whether the Edit icon should always be visible */
   showIcon?: boolean;
   /** Component to use as the input. The component "inherit" from `HvBaseInput` (such as `HvInput` or `HvTextArea`) */
-  component?: T;
+  component?: C;
   /** Variant of the HvTypography to display */
   variant?: HvTypographyVariants;
   /** Called when the input is blurred. */
@@ -55,10 +56,13 @@ export interface HvInlineEditorProps<T extends React.ElementType>
  * An Inline Editor allows the user to edit a record without making a major switch
  * between viewing and editing, making it an efficient method of updating a record.
  */
-export const HvInlineEditor = <T extends React.ElementType>(
-  props: HvInlineEditorProps<T> &
-    Omit<React.ComponentProps<T>, keyof HvInlineEditorProps<T>>,
-) => {
+export const HvInlineEditor = fixedForwardRef(function HvInlineEditor<
+  C extends React.ElementType = typeof HvInput,
+>(
+  props: HvInlineEditorProps<C> &
+    Omit<React.ComponentProps<C>, keyof HvInlineEditorProps<C>>,
+  ref: PolymorphicRef<C>,
+) {
   const {
     className,
     classes: classesProp,
@@ -93,7 +97,7 @@ export const HvInlineEditor = <T extends React.ElementType>(
       input.focus();
       input.select();
     }
-  }, [editMode]);
+  }, [editMode, inputRef]);
 
   const handleClick = () => {
     setEditMode(true);
@@ -125,6 +129,7 @@ export const HvInlineEditor = <T extends React.ElementType>(
     <div className={cx(classes.root, className)}>
       {editMode && !disabled ? (
         <InputComponent
+          ref={ref}
           inputRef={inputRef}
           classes={{
             root: classes.inputRoot,
@@ -174,4 +179,4 @@ export const HvInlineEditor = <T extends React.ElementType>(
       )}
     </div>
   );
-};
+});
