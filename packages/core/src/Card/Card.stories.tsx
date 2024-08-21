a2dd60d4547646aa8927eb13f54103058fef5e27
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2024-08-21 15:18:08 +0100

    feat(Card): add pentaho plus styles

--- packages/core/src/Card/Card.stories.tsx
+++ packages/core/src/Card/Card.stories.tsx
@@ -7,18 +7,16 @@ import {
   HvCardProps,
 } from "@hitachivantara/uikit-react-core";
 
-import { AllComponents as AllComponentsStory } from "./stories/AllComponents";
-import AllComponentsRaw from "./stories/AllComponents?raw";
+import { Expandable as ExpandableStory } from "./stories/Expandable";
+import ExpandableRaw from "./stories/Expandable?raw";
 import { KPICards as KPICardsStory } from "./stories/KPICards";
 import KPICardsRaw from "./stories/KPICards?raw";
 import { Main as MainStory } from "./stories/Main";
 import MainRaw from "./stories/Main?raw";
-import { NoActions as NoActionsStory } from "./stories/NoActions";
-import NoActionsRaw from "./stories/NoActions?raw";
-import { OnlyTitle as OnlyTitleStory } from "./stories/OnlyTitle";
-import OnlyTitleRaw from "./stories/OnlyTitle?raw";
 import { Selectable as SelectableStory } from "./stories/Selectable";
 import SelectableRaw from "./stories/Selectable?raw";
+import { Variants as VariantsStory } from "./stories/Variants";
+import VariantsRaw from "./stories/Variants?raw";
 
 const meta: Meta<typeof HvCard> = {
   title: "Components/Card",
@@ -48,37 +46,19 @@ export const Main: StoryObj<HvCardProps> = {
   render: (args) => <MainStory {...args} />,
 };
 
-export const AllComponents: StoryObj<HvCardProps> = {
+export const Variants: StoryObj<HvCardProps> = {
   parameters: {
     docs: {
-      source: { code: AllComponentsRaw },
-    },
-    // Enables Chromatic snapshot
-    chromatic: { disableSnapshot: false, delay: 5000 },
-  },
-  render: () => <AllComponentsStory />,
-};
-
-export const NoActions: StoryObj<HvCardProps> = {
-  parameters: {
-    docs: {
-      source: { code: NoActionsRaw },
+      description: {
+        story: `The \`Card\` component can be constructed by composing several sub-components, like \`HvCardHeader\`, \`HvCardContent\`, \`HvCardMedia\`, and \`HvActionsBar\`. 
+        This sample showcases how these modular pieces can be combined to create a wide variety of card layouts and styles.`,
+      },
+      source: { code: VariantsRaw },
     },
     // Enables Chromatic snapshot
     chromatic: { disableSnapshot: false },
   },
-  render: () => <NoActionsStory />,
-};
-
-export const OnlyTitle: StoryObj<HvCardProps> = {
-  parameters: {
-    docs: {
-      source: { code: OnlyTitleRaw },
-    },
-    // Enables Chromatic snapshot
-    chromatic: { disableSnapshot: false },
-  },
-  render: () => <OnlyTitleStory />,
+  render: () => <VariantsStory />,
 };
 
 export const KPICards: StoryObj<HvCardProps> = {
@@ -101,4 +81,17 @@ export const Selectable: StoryObj<HvCardProps> = {
     },
   },
   render: () => <SelectableStory />,
+};
+
+export const Expandable: StoryObj<HvCardProps> = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          "A card sample showcasing the ability to expand a card and show hidden content.",
+      },
+      source: { code: ExpandableRaw },
+    },
+  },
+  render: () => <ExpandableStory />,
 };
--- packages/core/src/Card/stories/OnlyTitle.tsx
+++
@@ -1,7 +0,0 @@
-import { HvCard, HvCardHeader } from "@hitachivantara/uikit-react-core";
-
-export const OnlyTitle = () => (
-  <HvCard bgcolor="atmo1" style={{ width: "500px" }}>
-    <HvCardHeader title="Advanced Server DS120" />
-  </HvCard>
-);
