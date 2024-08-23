8e1685f660fb5ac3879f75116504e1b848381665
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2024-08-23 13:06:54 +0100

    chore!: remove `Card` component from the `pentaho` package

--- packages/pentaho/src/Card/CardMedia/CardMedia.styles.tsx
+++
@@ -1,9 +0,0 @@
-import { createClasses } from "@hitachivantara/uikit-react-core";
-
-export const { staticClasses, useClasses } = createClasses("HvCardMedia", {
-  root: {},
-  image: {
-    objectFit: "cover",
-    width: "100%",
-  },
-});
