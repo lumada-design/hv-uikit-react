7dc067b9421ed0e98aeaec0b2d7cd121966da16b
Márcia Esteves <43220251+MEsteves22@users.noreply.github.com>
2024-09-10 18:02:12 +0100

    chore: theme decorator for stories updated
    @storybook/addon-themes used instead of a custom one

--- .storybook/addons/theme-selector/types.d.ts
+++
@@ -1,5 +0,0 @@
-interface Theme {
-  name: string;
-  label: string;
-  mode: string;
-}
---
+++ .storybook/modes.ts
@@ -0,0 +1,21 @@
+/** These are are the themes returned by getThemesList */
+export const allModes = {
+  "Pentaho+ dawn": {
+    theme: "pentahoPlus dawn",
+  },
+  "Pentaho+ wicked": {
+    theme: "pentahoPlus wicked",
+  },
+  "DS5 dawn": {
+    theme: "ds5 dawn",
+  },
+  "DS5 wicked": {
+    theme: "ds5 wicked",
+  },
+  "DS3 dawn": {
+    theme: "ds3 dawn",
+  },
+  "DS3 wicked": {
+    theme: "ds3 wicked",
+  },
+};
