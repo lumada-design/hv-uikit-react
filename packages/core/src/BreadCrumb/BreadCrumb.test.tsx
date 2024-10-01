057dd88a4ccfbe31e7859d363ae570b45cff8814
Bruno Henriques <bruno.henriques@hitachivantara.com>
2024-10-01 15:17:37 +0100

    fix(Breadcrumb): support path or empty origin (#4362)
    Co-authored-by: Bruno Henriques <zettca@users.noreply.github.com>

--- packages/core/src/BreadCrumb/BreadCrumb.test.tsx
+++ packages/core/src/BreadCrumb/BreadCrumb.test.tsx
@@ -14,14 +14,14 @@ const data = [
 ];
 
 describe("BreadCrumb", () => {
-  it("should render the correct number of items", () => {
+  it("renders the correct number of items", () => {
     render(<HvBreadCrumb listRoute={data} />);
     expect(screen.queryAllByRole("listitem").length).toBe(7);
     expect(screen.getByText("Label 1")).toBeInTheDocument();
     expect(screen.getByText("Label 7")).toBeInTheDocument();
   });
 
-  it("should render the correct number of items if maxVisible is specified", () => {
+  it("renders the correct number of items if maxVisible is specified", () => {
     render(<HvBreadCrumb listRoute={data} maxVisible={3} />);
     expect(screen.queryAllByRole("listitem").length).toBe(4);
     expect(screen.getByText("Label 1")).toBeInTheDocument();
@@ -29,5 +29,23 @@ describe("BreadCrumb", () => {
     expect(screen.queryByText("Label 5")).toBeNull();
     expect(screen.getByText("Label 6")).toBeInTheDocument();
     expect(screen.getByText("Label 7")).toBeInTheDocument();
+  });
+
+  it("renders an URL correctly", () => {
+    render(<HvBreadCrumb url="https://example.com/route1/route2/route3" />);
+    const firstLink = screen.getByRole("link", { name: /route1/i });
+    expect(firstLink).toBeInTheDocument();
+    expect(firstLink).toHaveAttribute("href", "https://example.com/route1");
+    expect(screen.getByRole("link", { name: /route2/i })).toBeInTheDocument();
+    expect(screen.getByText(/route3/i)).toBeInTheDocument();
+  });
+
+  it("renders a path correctly", () => {
+    render(<HvBreadCrumb url="/path1/path2/path3" />);
+    const firstLink = screen.getByRole("link", { name: /path1/i });
+    expect(firstLink).toBeInTheDocument();
+    expect(firstLink).toHaveAttribute("href", "/path1");
+    expect(screen.getByRole("link", { name: /path2/i })).toBeInTheDocument();
+    expect(screen.getByText(/path3/i)).toBeInTheDocument();
   });
 });
