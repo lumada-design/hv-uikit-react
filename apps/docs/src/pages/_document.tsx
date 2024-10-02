cc17f5e088e637722ca684c196468e6317f56bf2
francisco-guilherme <francisco.guilherme@hitachivantara.com>
2024-10-02 11:17:50 +0100

    Update apps/docs/src/pages/_document.tsx
    Co-authored-by: Bruno Henriques <bruno.henriques@hitachivantara.com>

--- apps/docs/src/pages/_document.tsx
+++ apps/docs/src/pages/_document.tsx
@@ -5,7 +5,7 @@ const Document = () => {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Open+Sans:wght@400;600&display=swap"
           rel="stylesheet"
         />
       </Head>
