dc81c8b4b2e960d75afc3a3d0aa479de253d30e5
francisco-guilherme <francisco.guilherme@hitachivantara.com>
2024-10-02 12:02:34 +0100

    chore: use utility classes for page height calc

--- apps/docs/src/components/home/Home.tsx
+++ apps/docs/src/components/home/Home.tsx
@@ -13,18 +13,12 @@ const styles: { [key: string]: CSSProperties } = {
       "linear-gradient(#f4f4f4 0.1em, transparent 0.1em), linear-gradient(90deg, #f4f4f4 0.1em, transparent 0.1em)",
     backgroundSize: "4em 4em",
   },
-  mainContainerHeight: {
-    height: "calc(100vh - 64px)",
-  },
 };
 
 export const Home = () => (
   <main className="absolute inset-0 md:overflow-hidden">
     <div style={styles.backgroundPattern} />
-    <div
-      className="mt-8 mx-auto bg-gradient-to-r from-white to-[#F1F5F9]"
-      style={styles.mainContainerHeight}
-    >
+    <div className="mt-8 mx-auto bg-gradient-to-r from-white to-[#F1F5F9] h-[calc(100vh_-_64px)]">
       <div className="relative max-w-[1440px] mx-auto grid md:grid-cols-[40%_60%] h-full">
         <section className="flex flex-col justify-center">
           <HeroSection />
