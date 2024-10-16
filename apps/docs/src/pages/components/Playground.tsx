545f24cfdffac100ea49440e2ea202dd45b67a58
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2024-10-16 15:27:52 +0100

    chore: add automatic controls type and values retrieval

--- apps/docs/src/pages/components/Playground.tsx
+++ apps/docs/src/pages/components/Playground.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import jsxToString from "react-element-to-jsx-string";
 import { CodeEditor } from "react-live-runner";
 import { classes } from "@docs/components/code/Live";
+import { useData } from "nextra/hooks";
 // @ts-ignore
 import { themes } from "prism-react-renderer";
 import {
   HvCheckBox,
   HvOption,
   HvSelect,
-  theme,
 } from "@hitachivantara/uikit-react-core";
 
 type Control = {
-  type: string;
-  defaultValue: string;
+  type?: string;
+  defaultValue?: string;
   values?: string[];
 };
 
@@ -71,10 +71,12 @@ export const Playground = ({
   controls,
   children,
 }: PlaygroundProps) => {
+  const data = useData();
+
   const initialState = Object.keys(controls || {}).reduce(
     (state, prop) => {
       if (controls[prop].defaultValue) {
-        state[prop] = controls[prop].defaultValue;
+        state[prop] = controls[prop].defaultValue || "";
       }
       return state;
     },
@@ -91,6 +93,51 @@ export const Playground = ({
     }));
   };
 
+  const getControlRenderer = useCallback(
+    (prop: string, control: Control) => {
+      const propMeta = data?.meta.docgen.props[prop];
+
+      if (!propMeta) return null;
+
+      if (propMeta.type.name === "enum") {
+        return (
+          <HvSelect
+            key={`${prop}`}
+            value={propsState[prop] || control.defaultValue}
+            style={{ minWidth: 100, width: "80%" }}
+            label={prop}
+            onChange={(e, value) => handleSelectChange(e, prop, value)}
+          >
+            {propMeta.type.value.map((v: any) => {
+              const value = v.value.replace('"', "").replace('"', "");
+              return (
+                <HvOption value={value} key={value} label={value}>
+                  {value}
+                </HvOption>
+              );
+            })}
+          </HvSelect>
+        );
+      }
+
+      if (propMeta.type.name === "boolean") {
+        return (
+          <HvCheckBox
+            key={`${prop}`}
+            label={prop}
+            checked={!!propsState[prop]}
+            onChange={(e: React.ChangeEvent, value: any) =>
+              handleSelectChange(e, prop, value)
+            }
+          />
+        );
+      }
+
+      return <div>nothing to show...</div>;
+    },
+    [data?.meta.docgen.props, propsState],
+  );
+
   if (!Component) return;
 
   const code = generateCode(
@@ -102,65 +149,19 @@ export const Playground = ({
 
   return (
     <>
-      <div className="flex justify-between gap-3 p-2 mt-1 border border-[var(--uikit-colors-atmo4)] rounded-t-round">
-        <div
-          style={{
-            width: "70%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+      <div className="flex justify-between p-2 mt-1 border border-[var(--uikit-colors-atmo4)] rounded-t-round">
+        <div className="w-[70%] flex justify-center items-center">
           <Component {...propsState} {...componentProps}>
             {children?.props.children}
           </Component>
         </div>
-        <div
-          style={{
-            width: "30%",
-            display: "flex",
-            flexDirection: "column",
-            gap: theme.space.xs,
-            justifyContent: "center",
-            alignItems: "center",
-            borderLeft: `1px solid ${theme.colors.atmo3}`,
-            paddingLeft: theme.space.sm,
-          }}
-        >
-          {Object.keys(controls || {}).map((prop, index) => {
+        <div className="w-[30%] flex flex-col gap-[var(--uikit-space-xs)] justify-center items-center border-l border-[var(--uikit-colors-atmo3)] pl-[var(--uikit-space-sm)]">
+          {Object.keys(controls || {}).map((prop) => {
             const control = controls[prop];
+            if (!control) return null;
 
-            if (control.type === "select" && control.values) {
-              return (
-                <HvSelect
-                  key={`${control.type}-${index}`}
-                  value={propsState[prop] || control.defaultValue}
-                  onChange={(e, value) => handleSelectChange(e, prop, value)}
-                  style={{ width: 200 }}
-                  label={prop}
-                >
-                  {control.values.map((value: string) => (
-                    <HvOption value={value} key={value} label={value}>
-                      {value}
-                    </HvOption>
-                  ))}
-                </HvSelect>
-              );
-            }
-
-            if (control.type === "check") {
-              return (
-                <HvCheckBox
-                  key={`${control.type}-${index}`}
-                  label={prop}
-                  checked={!!propsState[prop]}
-                  onChange={(e: React.ChangeEvent, value: any) =>
-                    handleSelectChange(e, prop, value)
-                  }
-                />
-              );
-            }
-            return null;
+            const renderer = getControlRenderer(prop, control);
+            return renderer;
           })}
         </div>
       </div>
