4e846081437a6036a8d38f7242add8cb1a1ae784
Bruno Henriques <zettca@users.noreply.github.com>
2024-10-21 19:54:06 +0100

    fix(Select): align P+ styles with HvButton

--- packages/styles/src/themes/pentahoPlus.ts
+++ packages/styles/src/themes/pentahoPlus.ts
@@ -545,6 +545,13 @@ const pentahoPlus = makeTheme((theme) => ({
         },
       },
     },
+    HvSelect: {
+      classes: {
+        panel: {
+          borderColor: buttonColors.secondary.subtleBorder,
+        },
+      },
+    },
     HvTag: {
       classes: {
         root: {
@@ -620,9 +627,11 @@ const pentahoPlus = makeTheme((theme) => ({
           borderColor: "color-mix(in srgb, currentcolor, transparent 60%)",
           ":where(:not(.HvButton-disabled))": {
             "&[data-color=primary]": {
+              borderColor: buttonColors.primary.subtleBorder,
               backgroundColor: buttonColors.primary.subtleBg,
             },
             "&[data-color=secondary]": {
+              borderColor: buttonColors.secondary.subtleBorder,
               backgroundColor: buttonColors.secondary.subtleBg,
             },
             ":hover": {
