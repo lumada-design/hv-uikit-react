d541c16d976b2d73530fc94f5ead2526369898a3
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2024-10-22 11:23:48 +0100

    docs: allow override of default control renderer, add slider

--- apps/docs/src/pages/components/Playground.tsx
+++ apps/docs/src/pages/components/Playground.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from "react";
 import jsxToString from "react-element-to-jsx-string";
 import { CodeEditor } from "react-live-runner";
 import { classes } from "@docs/components/code/Live";
+import { css } from "@emotion/css";
 import { useData } from "nextra/hooks";
 // @ts-ignore
 import { themes } from "prism-react-renderer";
@@ -9,6 +10,7 @@ import {
   HvCheckBox,
   HvOption,
   HvSelect,
+  HvSlider,
 } from "@hitachivantara/uikit-react-core";
 
 type Control = {
@@ -97,14 +99,60 @@ export const Playground = ({
     (prop: string, control: Control) => {
       const propMeta = data?.meta.docgen.props[prop];
 
-      if (!propMeta) return null;
+      if (control.type) {
+        if (control.type === "slider") {
+          const min = 1;
+          const max = propMeta.type.value.length;
+
+          const formattedLabel = (label: React.ReactNode) => {
+            if (!label) return "";
+            return propMeta.type.value[
+              parseInt(label as string, 10) - 1
+            ].value.replace(/"/g, "");
+          };
+
+          return (
+            <HvSlider
+              label={prop}
+              hideInput
+              minPointValue={min}
+              maxPointValue={max}
+              markStep={1}
+              divisionQuantity={max - min}
+              classes={{
+                root: css({ width: "100%" }),
+                sliderContainer: css({ padding: 10, paddingTop: 0 }),
+                labelContainer: css({ marginLeft: 0, marginBottom: 8 }),
+              }}
+              values={[
+                propMeta.type.value.findIndex(
+                  (p: any) => p.value.replace(/"/g, "") === propsState[prop],
+                ) + 1 ||
+                  propMeta.type.value.findIndex(
+                    (p: any) =>
+                      p.value.replace(/"/g, "") === control.defaultValue,
+                  ) + 1,
+              ]}
+              formatMark={(label) => formattedLabel(label)}
+              formatTooltip={(label) => formattedLabel(label)}
+              onChange={(values) => {
+                handleSelectChange(
+                  null,
+                  prop,
+                  propMeta.type.value[values[0] - 1]?.value.replace(/"/g, ""),
+                );
+              }}
+            />
+          );
+        }
+      }
 
       if (propMeta.type.name === "enum") {
         return (
           <HvSelect
             key={`${prop}`}
             value={propsState[prop] || control.defaultValue}
-            style={{ minWidth: 100, width: "80%" }}
+            style={{ minWidth: 100, width: "100%" }}
             label={prop}
             onChange={(e, value) => handleSelectChange(e, prop, value)}
           >
@@ -155,7 +203,7 @@ export const Playground = ({
             {children?.props.children}
           </Component>
         </div>
-        <div className="w-[30%] flex flex-col gap-[var(--uikit-space-xs)] justify-center items-center border-l border-[var(--uikit-colors-atmo3)] pl-[var(--uikit-space-sm)]">
+        <div className="w-[30%] flex flex-col gap-[var(--uikit-space-xs)] justify-center items-center border-l border-[var(--uikit-colors-atmo3)] pl-[var(--uikit-space-xs)]">
           {Object.keys(controls || {}).map((prop) => {
             const control = controls[prop];
             if (!control) return null;
