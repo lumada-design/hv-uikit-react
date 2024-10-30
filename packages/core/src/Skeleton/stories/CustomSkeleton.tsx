43ff1cd75b876b5afc2f037c46bff1b355f0c7fc
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2024-10-30 11:09:15 +0000

    chore: refactor `Skeleton` animation

--- packages/core/src/Skeleton/stories/CustomSkeleton.tsx
+++ packages/core/src/Skeleton/stories/CustomSkeleton.tsx
@@ -1,20 +1,16 @@
 import { css } from "@emotion/css";
-import { keyframes } from "@emotion/react";
 import { HvSkeleton } from "@hitachivantara/uikit-react-core";
 
 export const CustomSkeleton = () => {
-  const myAnimation = keyframes`
-      0%, 100% {
-        box-shadow: inset 0px 0px 60px 0px rgba(0,0,0,0.50);
-      }
-      50% {
-        box-shadow: inset 0px 0px 60px 0px rgba(0,0,0,0.25);
-      }
-    `;
-
   const classes = {
     root: css({
-      animation: `${myAnimation} 1s infinite`,
+      animation: "myAnimation 1s infinite",
+
+      "@keyframes myAnimation": {
+        "50%": {
+          boxShadow: "inset 0px 0px 60px 0px rgba(0,0,0,0.25)",
+        },
+      },
     }),
   };
 
