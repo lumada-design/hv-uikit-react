96e8d0c306a7ac7429f446fbf76cf940722e84ec
Bruno Henriques <zettca@users.noreply.github.com>
2024-11-22 10:42:33 +0000

    docs: remove meta listing & kebab-case urls

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,6 +5,9 @@ const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   defaultShowCopyCode: true,
+  search: {
+    codeblocks: false,
+  },
   mdxOptions: {
     rehypePlugins: [rehypeMdxCodeProps],
   },
