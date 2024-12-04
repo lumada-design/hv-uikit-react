26bf203aeeb7908d299563afa42edf2df5c93813
Bruno Henriques <zettca@users.noreply.github.com>
2024-12-04 15:12:21 +0000

    chore(lint): disallow parent index import
    improves performance (especially when pointing to root index.ts)
    and avoids (expensive) circular imports

--- packages/core/src/TimeAgo/TimeAgo.test.tsx
+++ packages/core/src/TimeAgo/TimeAgo.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
-import { HvButton } from "..";
+import { HvButton } from "../Button";
 import { HvTimeAgo } from "./TimeAgo";
 
 const EM_DASH = "—";
