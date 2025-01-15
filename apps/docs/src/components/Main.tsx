293b3f6b839339abb49d1f8ca5a74a9a46a75b2e
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2025-01-15 11:47:02 +0000

    docs: ensure styles order on documentation
    docs: ensure styles order on documentation

--- apps/docs/src/components/Main.tsx
+++ apps/docs/src/components/Main.tsx
@@ -1,6 +1,13 @@
+import createCache from "@emotion/cache";
 import { useTheme } from "nextra-theme-docs";
 import { ds5, HvProvider, pentahoPlus } from "@hitachivantara/uikit-react-core";
 import { HvVizProvider } from "@hitachivantara/uikit-react-viz";
+
+const emotionCache = createCache({
+  key: "hv-docs",
+  prepend: true,
+  container: typeof document !== "undefined" ? document.head : undefined,
+});
 
 export const Main = ({ children }: { children: React.ReactNode }) => {
   const { resolvedTheme } = useTheme();
@@ -10,6 +17,7 @@ export const Main = ({ children }: { children: React.ReactNode }) => {
       themes={[pentahoPlus, ds5]}
       theme="pentahoPlus"
       colorMode={resolvedTheme === "dark" ? "wicked" : "dawn"}
+      emotionCache={emotionCache}
     >
       <HvVizProvider>{children}</HvVizProvider>
     </HvProvider>
