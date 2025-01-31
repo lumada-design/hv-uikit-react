cfeeba63e3199be5623e2dee9ca2d442910086fa
Bruno Henriques <zettca@users.noreply.github.com>
2025-01-31 11:45:35 +0000

    feat: editable input & update styles

---
+++ apps/app/src/components/SplitterTable.tsx
@@ -0,0 +1,297 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */
+import { useRef, useState } from "react";
+import { css } from "@emotion/css";
+import { CaretDown } from "@phosphor-icons/react";
+import {
+  HvInlineEditor,
+  HvTable,
+  HvTableBody,
+  HvTableCell,
+  HvTableContainer,
+  HvTableHead,
+  HvTableRow,
+  theme,
+} from "@hitachivantara/uikit-react-core";
+
+export type Columns = Record<number, string>;
+
+const classes = {
+  crosshairContainer: css({
+    position: "absolute",
+    pointerEvents: "none",
+    inset: 0,
+    zIndex: 4,
+    "& > *": {
+      position: "absolute",
+      height: "100%",
+    },
+  }),
+  crosshairHover: css({
+    width: 2,
+    color: theme.colors.primary,
+  }),
+  crosshairColumn: css({
+    pointerEvents: "auto",
+    padding: "0 0.6ch",
+
+    color: theme.colors.pp.divider,
+
+    ":hover, &[data-state='drag']": {
+      cursor: "ew-resize",
+      color: theme.colors.primary,
+    },
+  }),
+  line: css({
+    height: "100%",
+    borderLeft: `2px solid currentcolor`,
+  }),
+  headerCell: css({
+    position: "relative",
+    display: "inline-flex",
+    alignItems: "center",
+    overflow: "hidden",
+    height: "100%",
+    padding: 0,
+    minHeight: 48,
+
+    backgroundColor: theme.colors.atmo2,
+  }),
+};
+
+export function SplitterTable({
+  data,
+  columns,
+  onColumnsChange,
+}: {
+  data: string;
+  columns: Columns;
+  onColumnsChange: (columns: Columns) => void;
+}) {
+  const [hoverColumn, setHoverColumn] = useState(-1);
+  const [charWidth, setCharWidth] = useState(8);
+  const tableRef = useRef<HTMLTableElement>(null);
+
+  /** adds the column on `index`, or removes when existing  */
+  function handleColumnToggle(index: number) {
+    if (index === 0) return;
+    const newCols = { ...columns, [index]: getName(columns) };
+    if (columns[index]) {
+      delete newCols[index];
+    }
+    onColumnsChange(newCols);
+  }
+
+  function handleColumnRemove(index: number) {
+    if (index === 0) return;
+    const newCols = { ...columns };
+    delete newCols[index];
+    onColumnsChange(newCols);
+  }
+
+  function handleColumnMove(index: number, newIndex: number) {
+    if (index === 0 || newIndex === 0) return;
+    const newCols = { ...columns };
+    const colName = newCols[index];
+    delete newCols[index];
+    newCols[newIndex] = colName;
+    onColumnsChange(newCols);
+  }
+
+  const shouldHideHover =
+    hoverColumn === -1 ||
+    Object.keys(columns).some((idx) => hoverColumn === Number(idx));
+
+  return (
+    <HvTableContainer className="relative font-mono w-fit max-h-420px">
+      <div
+        className="invisible h-0 absolute font-mono"
+        ref={(el) => {
+          if (!el) return;
+          const { width } = el.getBoundingClientRect();
+          setCharWidth(width); // get width of a character/column
+        }}
+      >
+        0
+      </div>
+      <HvTable
+        ref={tableRef}
+        onClick={(evt) => {
+          evt.preventDefault();
+          evt.stopPropagation();
+          const { tagName } = evt.target as HTMLElement;
+          if (tagName !== "PRE" && tagName !== "TD") return;
+          const rect = evt.currentTarget.getBoundingClientRect();
+          const offsetX = evt.clientX - rect.left;
+          const col = getColumn(offsetX, charWidth);
+          handleColumnToggle?.(col);
+        }}
+        onMouseMove={(evt) => {
+          const rect = evt.currentTarget.getBoundingClientRect();
+          const offsetX = evt.clientX - rect.left;
+          const col = getColumn(offsetX, charWidth);
+
+          setHoverColumn(col);
+        }}
+        onMouseLeave={() => {
+          setHoverColumn(-1);
+        }}
+      >
+        <HvTableHead stickyHeader>
+          <HvTableRow className="flex">
+            {Object.entries(columns).map(([index, name], i, cols) => (
+              <HvTableCell
+                key={`${i}-${name}`}
+                className={classes.headerCell}
+                style={{
+                  flex:
+                    i === cols.length - 1
+                      ? "1 0 auto"
+                      : `0 0 ${Number(cols[i + 1][0]) - Number(index)}ch`,
+                }}
+              >
+                <HvInlineEditor
+                  variant="captionLabel"
+                  disableClear
+                  classes={{
+                    root: "px-2px text-wrap break-all line-clamp-2 [&>*]:max-w-50px",
+                    button: "bg-transparent hover:bg-transparent p-0 [&>*]:m-0",
+                    input: "ml-2px mr-0",
+                    icon: "hidden",
+                  }}
+                  onEnter={(evt) => evt.currentTarget.blur()}
+                  defaultValue={name}
+                  onBlur={(evt, newValue) => {
+                    const newCols = { ...columns };
+                    newCols[Number(index)] = newValue;
+                    onColumnsChange(newCols);
+                  }}
+                />
+              </HvTableCell>
+            ))}
+          </HvTableRow>
+        </HvTableHead>
+        <HvTableBody>
+          <HvTableRow>
+            <HvTableCell className="p-0 select-none">
+              <pre>{data}</pre>
+            </HvTableCell>
+          </HvTableRow>
+        </HvTableBody>
+      </HvTable>
+      <div
+        className={classes.crosshairContainer}
+        style={{ height: tableRef.current?.clientHeight }}
+      >
+        {Object.entries(columns).map(([index, name], i) => (
+          <ColumnSeparatorLine
+            key={`${i}-${name}`}
+            charWidth={charWidth}
+            index={Number(index)}
+            onColumnMove={handleColumnMove}
+            onColumnRemove={handleColumnRemove}
+          />
+        ))}
+        <div
+          className={classes.crosshairHover}
+          style={{
+            left: `${hoverColumn}ch`,
+            display: shouldHideHover ? "none" : undefined,
+          }}
+        >
+          <div className="relative size-16px -left-7px -top-4px">
+            <CaretDown
+              weight="fill"
+              fontSize={16}
+              style={{ color: theme.colors.negative }}
+            />
+          </div>
+          <div className={classes.line} style={{ borderStyle: "dashed" }} />
+        </div>
+      </div>
+    </HvTableContainer>
+  );
+}
+
+function ColumnSeparatorLine({
+  index,
+  charWidth,
+  onColumnMove,
+  onColumnRemove,
+}: {
+  index: number;
+  charWidth: number;
+  onColumnMove: (index: number, newIndex: number) => void;
+  onColumnRemove: (index: number) => void;
+}) {
+  type State = "idle" | "delete" | "drag";
+  const stateRef = useRef<State>("idle");
+  const [state, setState] = useState<State>("idle");
+
+  if (index === 0) return null; // never render first column's line
+
+  function changeState(newState: State) {
+    setState(newState);
+    // keep a ref to use inside native eventHandlers
+    stateRef.current = newState;
+  }
+
+  const color =
+    (state === "delete" && theme.colors.negative) ||
+    (state === "drag" && theme.colors.primary) ||
+    "";
+
+  return (
+    <div
+      data-state={state}
+      className={classes.crosshairColumn}
+      style={{ color, left: `calc(${index}ch - 0.5ch - 1px)` }}
+      onMouseDown={(evt) => {
+        const el = evt.currentTarget.parentElement?.parentElement;
+        if (!el) return;
+
+        el.style.userSelect = "none";
+        changeState("delete");
+        const abortController = new AbortController();
+
+        el.addEventListener(
+          "mousemove",
+          (evt) => {
+            changeState("drag");
+            const { left } = el.getBoundingClientRect();
+            const offsetX = evt.clientX - left;
+            const col = getColumn(offsetX, charWidth);
+            onColumnMove(index, col);
+          },
+          { signal: abortController.signal },
+        );
+        el.addEventListener(
+          "mouseup",
+          () => {
+            el.style.userSelect = "";
+            if (stateRef.current === "delete") {
+              onColumnRemove(index);
+            }
+            changeState("idle");
+            abortController.abort();
+          },
+          { signal: abortController.signal },
+        );
+      }}
+    >
+      <div className={classes.line} />
+    </div>
+  );
+}
+
+function getName(columns?: Columns) {
+  const index = Object.values(columns || {})
+    .map((v) => Number(v))
+    .filter((v) => !Number.isNaN(v))
+    .toSorted((a, b) => a - b)
+    .at(-1);
+  return `${(index ?? 0) + 1}`;
+}
+
+function getColumn(offsetX: number, charWidth: number) {
+  return Math.round(offsetX / charWidth);
+}
--- apps/app/src/pages/Test.tsx
+++ apps/app/src/pages/Test.tsx
@@ -1,19 +1,34 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */
-import { useRef, useState } from "react";
-import { css } from "@emotion/css";
-import { CaretDown, Trash } from "@phosphor-icons/react";
-import {
-  HvButton,
-  HvTable,
-  HvTableBody,
-  HvTableCell,
-  HvTableCellProps,
-  HvTableContainer,
-  HvTableHead,
-  HvTableRow,
-  HvTypography,
-  theme,
-} from "@hitachivantara/uikit-react-core";
+import { useState } from "react";
+
+import { Columns, SplitterTable } from "~/components/SplitterTable";
+
+export function Component() {
+  const [data] = useState(() => baseData.trimStart().repeat(4));
+  const [columns, setColumns] = useState<Columns>({ 0: "1" }); // initial column required
+
+  return (
+    <SplitterTable
+      data={data}
+      columns={columns}
+      onColumnsChange={(cols) => setColumns(sortNumberColumns(cols))}
+    />
+  );
+}
+
+/** order numeric-only column names */
+function sortNumberColumns(columns: Columns) {
+  const numericCols = Object.entries(columns)
+    .filter(([, name]) => /^\d+$/.test(name))
+    .toSorted(([a], [b]) => Number(a) - Number(b));
+
+  const newCols = { ...columns };
+  numericCols.forEach(([index], i) => {
+    newCols[Number(index)] = String(i + 1);
+  });
+
+  return newCols;
+}
 
 // ID     Name                        Age Location    Occupation       Salary   Department    Email                     Phone
 const baseData = `
@@ -32,301 +47,3 @@ const baseData = `
 10013  Seraphim     Foxglove       44  Spain       Engineer         70000    R&D           seraphim@example.com      345-678-9012
 10014  Orion        Thunderforge   60  India       Manager          85000    HR            orion@example.com         456-789-0123
 `;
-
-type Columns = Record<number, string>;
-
-const initialColumn = -1;
-
-export function Component() {
-  const data = baseData.trimStart().repeat(4);
-  const [columns, setColumns] = useState<Columns>({ 0: getName() }); // initial column required
-
-  return (
-    <MagicTable data={data} columns={columns} onColumnsChange={setColumns} />
-  );
-}
-
-const classes = {
-  crosshairContainer: css({
-    position: "absolute",
-    pointerEvents: "none",
-    inset: 0,
-    zIndex: 4,
-    "& > *": {
-      position: "absolute",
-      height: "100%",
-    },
-  }),
-  crosshairHover: css({
-    width: 2,
-    color: theme.colors.primary,
-  }),
-  crosshairColumn: css({
-    pointerEvents: "auto",
-    padding: "0 0.5ch",
-
-    color: theme.colors.pp.divider,
-
-    ":hover, &[data-state='drag']": {
-      cursor: "ew-resize",
-      color: theme.colors.primary,
-    },
-    // show delete button on hover when not dragging
-    ":hover:not([data-state='drag']) > button": {
-      display: "inline-flex",
-    },
-  }),
-  line: css({
-    height: "100%",
-    borderLeft: `2px solid currentcolor`,
-  }),
-  headerCell: css({
-    position: "relative",
-    display: "inline-flex",
-    alignItems: "center",
-    height: "100%",
-    padding: 0,
-    minHeight: 48,
-
-    backgroundColor: theme.colors.atmo2,
-  }),
-};
-
-function MagicTable({
-  data,
-  columns,
-  onColumnsChange,
-}: {
-  data: string;
-  columns: Columns;
-  onColumnsChange: (columns: Columns) => void;
-}) {
-  const [hoverColumn, setHoverColumn] = useState(initialColumn);
-  const [charWidth, setCharWidth] = useState(8);
-  const tableRef = useRef<HTMLTableElement>(null);
-
-  /** adds the column on `index`, or removes when existing  */
-  function handleColumnToggle(index: number) {
-    if (index === 0) return;
-    const newCols = { ...columns, [index]: getName(columns) };
-    if (columns[index]) {
-      delete newCols[index];
-    }
-    onColumnsChange(newCols);
-  }
-
-  function handleColumnRemove(index: number) {
-    if (index === 0) return;
-    const newCols = { ...columns };
-    delete newCols[index];
-    onColumnsChange(newCols);
-  }
-
-  function handleColumnMove(index: number, newIndex: number) {
-    if (index === 0 || newIndex === 0) return;
-    const newCols = { ...columns };
-    const colName = newCols[index];
-    delete newCols[index];
-    newCols[newIndex] = colName;
-    onColumnsChange(newCols);
-  }
-
-  const shouldHideHover =
-    hoverColumn === -1 ||
-    Object.keys(columns).some(
-      (idx) => Math.abs(hoverColumn - Number(idx)) <= 2,
-    );
-
-  return (
-    <HvTableContainer className="relative font-mono w-fit max-h-420px">
-      <div
-        className="invisible h-0 absolute font-mono"
-        ref={(el) => {
-          if (!el) return;
-          const { width } = el.getBoundingClientRect();
-          setCharWidth(width); // get width of a character/column
-        }}
-      >
-        0
-      </div>
-      <HvTable
-        ref={tableRef}
-        // className="cursor-ew-resize"
-        onClick={(evt) => {
-          evt.preventDefault();
-          evt.stopPropagation();
-          const rect = evt.currentTarget.getBoundingClientRect();
-          const offsetX = evt.clientX - rect.left;
-          const col = getColumn(offsetX, charWidth);
-          handleColumnToggle?.(col);
-        }}
-        onMouseMove={(evt) => {
-          const rect = evt.currentTarget.getBoundingClientRect();
-          const offsetX = evt.clientX - rect.left;
-          const col = getColumn(offsetX, charWidth);
-
-          setHoverColumn(col);
-        }}
-        onMouseLeave={() => {
-          setHoverColumn(initialColumn);
-        }}
-      >
-        <HvTableHead stickyHeader>
-          <HvTableRow className="flex">
-            {Object.entries(columns).map(([index, name], i, cols) => (
-              <MagicCell
-                key={name}
-                column={{ index: Number(index), name }}
-                onColumnRemove={handleColumnRemove}
-                style={{
-                  flex:
-                    i === cols.length - 1
-                      ? "1 0 auto"
-                      : `0 0 ${Number(cols[i + 1][0]) - Number(index)}ch`,
-                }}
-              />
-            ))}
-          </HvTableRow>
-        </HvTableHead>
-        <HvTableBody>
-          <HvTableRow>
-            <HvTableCell className="p-0 select-none">
-              <pre>{data}</pre>
-            </HvTableCell>
-          </HvTableRow>
-        </HvTableBody>
-      </HvTable>
-      <div
-        className={classes.crosshairContainer}
-        style={{ height: tableRef.current?.clientHeight }}
-      >
-        {Object.entries(columns).map(([index, name]) => (
-          <ColumnSeparatorLine
-            key={name}
-            charWidth={charWidth}
-            index={Number(index)}
-            onColumnMove={handleColumnMove}
-            onColumnRemove={handleColumnRemove}
-          />
-        ))}
-        <div
-          className={classes.crosshairHover}
-          style={{
-            left: `${hoverColumn}ch`,
-            display: shouldHideHover ? "none" : undefined,
-          }}
-        >
-          <div className="relative size-16px -left-7px -top-4px">
-            <CaretDown
-              weight="fill"
-              fontSize={16}
-              style={{ color: theme.colors.negative }}
-            />
-          </div>
-          <div className={classes.line} style={{ borderStyle: "dashed" }} />
-        </div>
-      </div>
-    </HvTableContainer>
-  );
-}
-
-function MagicCell({
-  column,
-  onColumnRemove,
-  ...others
-}: {
-  column: { index: number; name: string };
-  onColumnRemove?: (index: number) => void;
-} & HvTableCellProps) {
-  return (
-    <HvTableCell className={classes.headerCell} {...others}>
-      <HvTypography
-        variant="captionLabel"
-        className="pl-2px pr-1px text-wrap break-all line-clamp-2"
-      >
-        {column.name}
-      </HvTypography>
-    </HvTableCell>
-  );
-}
-
-function ColumnSeparatorLine({
-  index,
-  charWidth,
-  onColumnMove,
-  onColumnRemove,
-}: {
-  index: number;
-  charWidth: number;
-  onColumnMove: (index: number, newIndex: number) => void;
-  onColumnRemove: (index: number) => void;
-}) {
-  const stateRef = useRef<"idle" | "delete" | "drag">("idle");
-
-  if (index === 0) return null; // never render first column's line
-
-  return (
-    <div
-      data-state={stateRef.current}
-      className={classes.crosshairColumn}
-      style={{ left: `calc(${index}ch - 0.5ch - 1px)` }}
-      onMouseDown={(evt) => {
-        const el = evt.currentTarget.parentElement?.parentElement;
-        if (!el) return;
-
-        el.style.userSelect = "none";
-        stateRef.current = "delete";
-
-        const abortController = new AbortController();
-
-        el.addEventListener(
-          "mousemove",
-          (evt) => {
-            stateRef.current = "drag";
-            const { left } = el.getBoundingClientRect();
-            const offsetX = evt.clientX - left;
-            const col = getColumn(offsetX, charWidth);
-            onColumnMove(index, col);
-          },
-          { signal: abortController.signal },
-        );
-        el.addEventListener(
-          "mouseup",
-          () => {
-            el.style.userSelect = "";
-            if (stateRef.current === "delete") {
-              onColumnRemove(index);
-            }
-            stateRef.current = "idle";
-            abortController.abort();
-          },
-          { signal: abortController.signal },
-        );
-      }}
-    >
-      <HvButton
-        icon
-        variant="subtle"
-        aria-label="Delete"
-        className="hidden relative float-right top-xs left-xxs"
-      >
-        <div className="p-4px">
-          <Trash fontSize={16} />
-        </div>
-      </HvButton>
-      <div className={classes.line} />
-    </div>
-  );
-}
-
-function getName(columns?: Columns) {
-  const index = Object.values(columns || {})
-    .map((v) => Number(v.split(" ")[1]))
-    .toSorted((a, b) => a - b)
-    .at(-1);
-  return `Field ${(index ?? 0) + 1}`;
-}
-
-function getColumn(offsetX: number, charWidth: number) {
-  return Math.round(offsetX / charWidth);
-}
