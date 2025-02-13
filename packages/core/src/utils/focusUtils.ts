2ecc6efb989ad27561bdc3a72bb09aca7cc3d766
Bruno Henriques <zettca@users.noreply.github.com>
2025-02-13 19:03:38 +0000

    style: remove unused focus outline

--- packages/core/src/utils/focusUtils.ts
+++ packages/core/src/utils/focusUtils.ts
@@ -1,7 +1,3 @@
 export const outlineStyles = {
-  outlineColor: "#52A8EC",
-  outlineStyle: "solid",
-  outlineWidth: "0px",
-  outlineOffset: "-1px",
   boxShadow: "0 0 0 1px #52A8EC, 0 0 0 4px rgba(29,155,209,.3)",
 };
