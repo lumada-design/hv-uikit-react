a8465da104114e90609da601b09e01429a9f8a8a
Bruno Henriques <zettca@users.noreply.github.com>
2025-03-18 01:43:32 +0000

    chore: remove more unused utils

--- packages/core/src/utils/ConditionalWrapper.tsx
+++
@@ -1,9 +0,0 @@
-export const ConditionalWrapper = ({
-  condition,
-  wrapper,
-  children,
-}: {
-  condition?: boolean;
-  wrapper: any;
-  children: React.ReactNode;
-}) => (condition ? wrapper(children) : children);
