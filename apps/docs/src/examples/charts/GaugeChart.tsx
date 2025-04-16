cbd0564cf21554f833a979ebf586d2dc70873f9d
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2025-04-16 16:02:36 +0100

    docs: add BaseChart documentation and more samples

---
+++ apps/docs/src/examples/charts/GaugeChart.tsx
@@ -0,0 +1,125 @@
+import { useMemo } from "react";
+import { GaugeChart } from "echarts/charts";
+import * as echarts from "echarts/core";
+import { useTheme } from "@hitachivantara/uikit-react-core";
+import { HvBaseChart } from "@hitachivantara/uikit-react-viz";
+
+echarts.use([GaugeChart]);
+
+export default function Demo() {
+  const { activeTheme, selectedMode } = useTheme();
+  const colors = activeTheme!.colors.modes[selectedMode];
+
+  const optionGauge = useMemo(
+    () => ({
+      series: [
+        {
+          type: "gauge",
+          startAngle: 225,
+          endAngle: -45,
+          pointer: {
+            show: false,
+          },
+          progress: {
+            show: true,
+            width: 20,
+            itemStyle: {
+              color: colors.negative,
+            },
+          },
+          axisLine: {
+            lineStyle: {
+              width: 20,
+            },
+          },
+          splitLine: {
+            show: false,
+          },
+          axisTick: {
+            show: false,
+          },
+          axisLabel: {
+            show: false,
+          },
+          detail: {
+            valueAnimation: true,
+            formatter: (value: number) =>
+              ["{l|High Risk}", "{v|" + value + "%}"].join("\n"),
+            rich: {
+              v: {
+                fontSize: 48,
+                color: colors.text,
+              },
+              l: {
+                fontSize: 16,
+                color: colors.text,
+              },
+            },
+          },
+          data: [
+            {
+              value: 80,
+            },
+          ],
+        },
+        {
+          type: "gauge",
+          startAngle: 225,
+          endAngle: -45,
+          pointer: {
+            show: true,
+            icon: "rect",
+            length: "25%",
+            offsetCenter: [0, "-80%"],
+            width: 1,
+            itemStyle: {
+              color: "#000",
+            },
+          },
+          axisLine: {
+            show: false,
+          },
+          progress: {
+            show: false,
+          },
+          axisTick: {
+            show: false,
+          },
+          splitLine: {
+            show: false,
+          },
+          axisLabel: {
+            show: false,
+          },
+          data: [
+            {
+              value: 65,
+            },
+          ],
+          detail: {
+            show: true,
+            offsetCenter: ["75%", "-90%"],
+            formatter: (value: number) => `${value}%`,
+            fontSize: 14,
+            color: "#000",
+          },
+          z: 10,
+        },
+      ],
+    }),
+    [colors],
+  );
+
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <HvBaseChart width={400} height={350} option={optionGauge} />
+    </div>
+  );
+}
