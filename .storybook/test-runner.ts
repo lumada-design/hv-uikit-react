3398d0f1447dfd91df8937abf8b6964bdd6271af
Bruno Henriques <zettca@users.noreply.github.com>
2025-04-30 09:55:43 +0100

    ci(a11y): change to checkA11y

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,46 +1,13 @@
-import fs from "node:fs";
 import { getStoryContext, type TestRunnerConfig } from "@storybook/test-runner";
-import { NodeResult, Result } from "axe-core";
-import { configureAxe, getAxeResults, injectAxe } from "axe-playwright";
+import { checkA11y, configureAxe, injectAxe } from "axe-playwright";
 
 const excludeStories = [
   "Overview",
   "Foundation",
   "Guides",
   "Templates",
-  "Visualizations/Bar Chart",
-  "Visualizations/Line Chart",
-  "Visualizations/Donut Chart",
-  "Visualizations/Confusion Matrix",
-  "Visualizations/Scatter Plot",
-  "Visualizations/Treemap",
-  "Visualizations/Heatmap",
-  "Widgets/Code Editor",
-  "Tests/Visualizations",
+  "Visualizations",
 ];
-
-function getNodesIds(nodes: NodeResult[]) {
-  return nodes.map((n) => `\n\t\t${n.target}`).join();
-}
-
-function getResultSummary(type: string, results: Result[]) {
-  return results
-    .map((r) => `\n\t${type}: ${r.help} - ${r.impact} ${getNodesIds(r.nodes)}`)
-    .join("");
-}
-
-function writeAxeResults(
-  storyTitle: string,
-  storyName: string,
-  type: string,
-  results: Result[],
-) {
-  if (results.length > 0) {
-    const fileName = `a11y_${type}_Results.txt`;
-    fs.appendFileSync(fileName, `\n${storyTitle} - ${storyName}`, "utf-8");
-    fs.appendFileSync(fileName, getResultSummary(type, results), "utf8");
-  }
-}
 
 /*
  * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
@@ -53,35 +20,26 @@ const config: TestRunnerConfig = {
 
   async postVisit(page, context) {
     const storyContext = await getStoryContext(page, context);
-    const validStories = excludeStories.filter((s) =>
+    if (storyContext.parameters?.a11y?.disable) return;
+
+    const isExcludedGroup = excludeStories.some((s) =>
       storyContext.title.includes(s),
     );
-    if (validStories.length !== 0) return;
-
-    const specificA11yRules = [{ id: "color-contrast", enabled: false }];
+    if (isExcludedGroup) return;
 
     // Apply story-level a11y rules
     await configureAxe(page, {
       rules: [
-        ...specificA11yRules,
+        { id: "color-contrast", enabled: false },
         ...(storyContext.parameters?.a11y?.config?.rules || []),
       ],
     });
 
-    const axeResults = await getAxeResults(page, "#storybook-root");
-
-    writeAxeResults(
-      storyContext.title,
-      storyContext.name,
-      "Violation",
-      axeResults.violations,
-    );
-    writeAxeResults(
-      storyContext.title,
-      storyContext.name,
-      "Incomplete",
-      axeResults.incomplete,
-    );
+    await checkA11y(page, "#storybook-root", {
+      verbose: false,
+      detailedReport: true,
+      detailedReportOptions: { html: true },
+    });
   },
 };
 
