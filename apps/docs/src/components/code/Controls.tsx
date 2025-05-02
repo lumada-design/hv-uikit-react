eef9e79766f78e93a1a9ce951846b00704247f68
Bruno Henriques <zettca@users.noreply.github.com>
2025-05-02 17:02:31 +0100

    docs: review controls

--- apps/docs/src/components/code/Controls.tsx
+++ apps/docs/src/components/code/Controls.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useData } from "nextra/hooks";
 import {
   HvCheckBox,
@@ -10,10 +11,6 @@ import {
   HvSlider,
   useTheme,
 } from "@hitachivantara/uikit-react-core";
-
-type OptionType = {
-  value: string;
-};
 
 type ControlType =
   | "check"
@@ -44,92 +41,97 @@ const defaultMap: Record<string, ControlType> = {
   enum: "select",
 };
 
-export const Controls = ({ prop, state, control, onChange }: ControlsProps) => {
-  const { colors } = useTheme();
-  const data = useData();
-  const propMeta = data?.meta?.docgen.props[prop];
+interface ControlProps extends ControlsProps {
+  label: React.ReactNode;
+  options: string[];
+}
 
-  const type: ControlType =
-    control?.type ||
-    defaultMap[propMeta?.type?.name] ||
-    defaultMap[typeof control.defaultValue] ||
-    "text";
+/**  Utility to clean up values (e.g., remove quotes) */
+const cleanValue = (value: string) => value.replace(/"/g, "");
 
-  // Utility to clean up values (e.g., remove quotes)
-  const cleanValue = (value: string) => value.replace(/"/g, "");
-
-  // Utility to extract options from prop metadata
-  const getOptions = (): OptionType[] => propMeta?.type?.value || [];
-
+/** HvSlider control for enum types with multiple options */
+function SliderControl({
+  prop,
+  state,
+  control,
+  onChange,
+  label,
+  options,
+}: ControlProps) {
   // Find the index of the current value in the options
   const findCurrentIndex = (value: string) =>
-    getOptions().findIndex((opt) => cleanValue(opt.value) === value) + 1;
+    options.findIndex((opt) => cleanValue(opt) === value) + 1;
 
-  // TODO: consider adding `capitalize` or `.nextra-code`
-  const propLabel = <code>{prop}</code>;
+  const max = options.length;
 
-  // Render slider control for enum types with multiple options
-  const renderSliderControl = () => {
-    const options = getOptions();
-    const max = options.length;
+  return (
+    <HvSlider
+      label={label}
+      hideInput
+      minPointValue={1}
+      maxPointValue={max}
+      markStep={1}
+      divisionQuantity={max - 1}
+      className="w-full pt-0"
+      classes={{
+        sliderContainer: "px-sm",
+        labelContainer: "mx-0",
+      }}
+      values={[
+        findCurrentIndex(String(state[prop])) ||
+          findCurrentIndex(String(control.defaultValue)),
+      ]}
+      formatMark={(value) => cleanValue(options[Number(value) - 1] || "")}
+      formatTooltip={(value) => cleanValue(options[Number(value) - 1] || "")}
+      onChange={(values) => onChange(prop, cleanValue(options[values[0] - 1]))}
+    />
+  );
+}
 
-    return (
-      <HvSlider
-        label={propLabel}
-        hideInput
-        minPointValue={1}
-        maxPointValue={max}
-        markStep={1}
-        divisionQuantity={max - 1}
-        className="w-full pt-0"
-        classes={{
-          sliderContainer: "px-sm",
-          labelContainer: "mx-0",
-        }}
-        values={[
-          findCurrentIndex(String(state[prop])) ||
-            findCurrentIndex(String(control.defaultValue)),
-        ]}
-        formatMark={(value) =>
-          cleanValue(options[Number(value) - 1]?.value || "")
-        }
-        formatTooltip={(value) =>
-          cleanValue(options[Number(value) - 1]?.value || "")
-        }
-        onChange={(values) =>
-          onChange(prop, cleanValue(options[values[0] - 1]?.value))
-        }
-      />
-    );
-  };
-
-  // Render radio button group for enum types
-  const renderRadioControl = () => (
+/** `HvRadio` button group for enum types */
+function RadioControl({
+  prop,
+  state,
+  control,
+  onChange,
+  label,
+  options,
+}: ControlProps) {
+  return (
     <HvRadioGroup
-      label={propLabel}
+      label={label}
       orientation="horizontal"
       value={state[prop] || control.defaultValue}
       onChange={(e, value) => onChange(prop, value)}
       className="w-full"
     >
-      {getOptions().map((v) => {
-        const value = cleanValue(v.value);
+      {options.map((v) => {
+        const value = cleanValue(v);
         return <HvRadio key={value} label={value} value={value} />;
       })}
     </HvRadioGroup>
   );
+}
 
-  // Render select dropdown for enum types
-  const renderSelectControl = () => (
+/** `HvSelect` dropdown for enum types */
+function SelectControl({
+  prop,
+  state,
+  control,
+  onChange,
+  label,
+  options,
+}: ControlProps) {
+  return (
     <HvSelect
       key={prop}
       value={state[prop] || control.defaultValue}
-      label={propLabel}
+      label={label}
       className="w-full min-w-[100px]"
       onChange={(e, value) => onChange(prop, value ?? "")}
     >
-      {getOptions().map((v) => {
-        const value = cleanValue(v.value);
+      {options.map((v) => {
+        const value = cleanValue(v);
         return (
           <HvOption value={value} key={value} label={value}>
             {value}
@@ -138,34 +140,46 @@ export const Controls = ({ prop, state, control, onChange }: ControlsProps) => {
       })}
     </HvSelect>
   );
+}
 
-  // Render checkbox for boolean types
-  const renderCheckboxControl = () => (
+/** `HvCheckbox` for boolean types */
+function CheckboxControl({ prop, state, onChange, label }: ControlProps) {
+  return (
     <HvCheckBox
       key={prop}
-      label={propLabel}
+      label={label}
       checked={!!state[prop]}
       onChange={(e, value) => onChange(prop, value)}
     />
   );
+}
 
-  // Render text input for default text types
-  const renderInputControl = () => (
+/** `HvInput` for text types */
+function InputControl({ prop, state, control, onChange, label }: ControlProps) {
+  return (
     <HvInput
       key={prop}
-      label={propLabel}
+      label={label}
       value={String(state[prop] ?? control.defaultValue)}
       onChange={(e, value) => onChange(prop, value)}
       className="w-full"
       disableClear
     />
   );
+}
 
-  // Render number input for default text types
-  const renderNumberInputControl = () => (
+/** `HvInput` for number types */
+function NumberInputControl({
+  prop,
+  state,
+  control,
+  onChange,
+  label,
+}: ControlProps) {
+  return (
     <HvInput
       key={prop}
-      label={propLabel}
+      label={label}
       inputProps={{ type: "number" }}
       value={state[prop] ?? control.defaultValue}
       onChange={(e, value) => onChange(prop, Number(value))}
@@ -173,12 +187,16 @@ export const Controls = ({ prop, state, control, onChange }: ControlsProps) => {
       disableClear
     />
   );
+}
 
-  // Render color picker for color types
-  const renderColorControl = () => (
+/** `HvColorPicker` for color types */
+function ColorControl({ prop, state, control, onChange, label }: ControlProps) {
+  const { colors } = useTheme();
+
+  return (
     <HvColorPicker
       key={prop}
-      label={propLabel}
+      label={label}
       value={
         typeof state[prop] === "string" && state[prop].includes("#")
           ? state[prop]
@@ -202,29 +220,54 @@ export const Controls = ({ prop, state, control, onChange }: ControlsProps) => {
       ]}
     />
   );
+}
 
-  // Map of renderers for specific control types
-  const rendererMap: Record<string, () => React.ReactNode> = {
-    slider: () =>
-      propMeta?.type?.name === "enum" ? renderSliderControl() : null,
-    radio: () =>
-      propMeta?.type?.name === "enum" ? renderRadioControl() : null,
-    check: renderCheckboxControl,
-    color: renderColorControl,
-    number: renderNumberInputControl,
-    text: renderInputControl,
-    select: renderSelectControl,
-    // Some props are objects, for now lets return null and hide them but we should be able to edit objects on the playground controls too
-    object: () => null,
+export const Controls = ({ prop, state, control, onChange }: ControlsProps) => {
+  const data = useData();
+  const propMeta = data?.meta?.docgen.props[prop];
+
+  const type: ControlType =
+    control?.type ||
+    defaultMap[propMeta?.type?.name] ||
+    defaultMap[typeof control.defaultValue] ||
+    "text";
+
+  const options = useMemo<string[]>(() => {
+    const propValues: { value: string }[] | undefined = propMeta?.type?.value;
+    return control.values || propValues?.map((v) => v.value) || [];
+  }, [propMeta?.type?.value, control.values]);
+
+  /** Map of renderers for specific control types */
+  const rendererMap: Record<
+    ControlType,
+    React.ElementType<ControlProps> | undefined
+  > = {
+    slider: SliderControl,
+    radio: RadioControl,
+    check: CheckboxControl,
+    color: ColorControl,
+    number: NumberInputControl,
+    text: InputControl,
+    select: SelectControl,
   };
 
-  // Render the appropriate control or log an error if unsupported
-  const renderControl = rendererMap[type];
-  if (renderControl) {
-    return renderControl();
+  const ControlComponent = rendererMap[type];
+
+  if (!ControlComponent) {
+    // eslint-disable-next-line no-console
+    console.error(`Control for "${prop}" not supported: ${type}`);
+    return null;
   }
 
-  // eslint-disable-next-line no-console
-  console.error(`Control for "${prop}" not supported: ${type}`);
-  return null;
+  return (
+    <ControlComponent
+      prop={prop}
+      state={state}
+      control={control}
+      onChange={onChange}
+      // TODO: consider adding `capitalize` or `.nextra-code`
+      label={<code>{prop}</code>}
+      options={options}
+    />
+  );
 };
