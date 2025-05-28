06776ee5fd9f56132be9e749abafce9ea9c3511a
Bruno Henriques <bruno.henriques@hitachivantara.com>
2025-05-28 18:32:47 +0100

    fix(app-shell-vite-plugin): cross-platform module resolution (#4760)
    Co-authored-by: Bruno Henriques <zettca@users.noreply.github.com>

--- packages/app-shell-vite-plugin/src/nodeModule.ts
+++ packages/app-shell-vite-plugin/src/nodeModule.ts
@@ -1,6 +1,5 @@
 import { createRequire } from "node:module";
-import { join } from "node:path";
-import { findUpSync } from "find-up-simple";
+import { dirname, join } from "node:path";
 
 export const require = createRequire(import.meta.url);
 
@@ -8,27 +7,10 @@ export const require = createRequire(import.meta.url);
  * Resolves the module entrypoint by name and normalizes slashes to be posix/unix-like forward slashes.
  *
  * @param moduleName The name of the module to be searched for
+ * @param suffix to be added after the module path
  * @returns The module path normalized
  */
-export function resolveModule(moduleName: string) {
-  return require.resolve(moduleName).replace(/\\+/g, "/");
-}
-
-/** Resolves the module directory (where `package.json` is) by `moduleName` */
-function resolveModuleDirectory(moduleName: string) {
-  const moduleEntrypoint = resolveModule(moduleName);
-  const modulePackage = findUpSync("package.json", { cwd: moduleEntrypoint });
-  return modulePackage?.slice(0, modulePackage.lastIndexOf("/")) || "";
-}
-
-/**
- * This function will find out the module path using node `require.resolve` function
- * and add the suffix param after the folder with module name.
- *
- * @param moduleName "@module/name"
- * @param suffix to be added after the module path
- * @returns the /path/to/@module/name/<suffix>
- */
-export function getModulePath(moduleName: string, suffix: string) {
-  return join(resolveModuleDirectory(moduleName), suffix);
+export function resolveModule(moduleName: string, suffix?: string) {
+  const entrypoint = require.resolve(moduleName);
+  return suffix ? join(dirname(entrypoint), suffix) : entrypoint;
 }
