0d419f24ca8b87aec3ee6dd16f983c8199da6760
Bruno Henriques <bruno.henriques@hitachivantara.com>
2025-07-16 17:31:53 +0100

    chore(deps): migrate to MUI@7 & react>=18 (#4816)
    migrate MUI & useId

--- packages/core/src/hooks/useUniqueId.ts
+++ packages/core/src/hooks/useUniqueId.ts
@@ -1,31 +1,7 @@
-// eslint-disable-next-line no-restricted-imports
-import * as React from "react"; // TODO: this can be optimized when react 17 support is dropped
+import { useId } from "react";
 
-import { useEnhancedEffect } from "./useEnhancedEffect";
+export const useUniqueId = (deterministicId?: string): string => {
+  const id = useId();
 
-/** Credit: https://github.com/radix-ui/primitives/blob/main/packages/react/id/src/id.tsx
- *  Modified slightly to suit our purposes.
- */
-// We `toString()` to prevent bundlers from trying to `import { useId } from 'react';`
-const useReactId = (React as any)["useId".toString()] || (() => undefined);
-let count = 0;
-
-export const useUniqueId = (
-  deterministicId?: string,
-  /**
-   * @deprecated
-   * Users should pick between a fully deterministic or fully generated id
-   * @example
-   * useUniqueId(setId(idPrefix, deterministicId))
-   * */
-  idPrefix?: string,
-): string => {
-  const [id, setId] = React.useState<string | undefined>(useReactId());
-
-  useEnhancedEffect(() => {
-    // eslint-disable-next-line no-plusplus
-    if (!deterministicId) setId((reactId) => reactId ?? String(count++));
-  }, [deterministicId, idPrefix]);
-
-  return deterministicId || (idPrefix ? idPrefix + id : (id ?? ""));
+  return deterministicId ?? id;
 };
