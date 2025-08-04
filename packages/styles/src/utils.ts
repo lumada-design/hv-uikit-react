36e3d90ddea5ff30d5be38768aadd88fcae31853
Bruno Henriques <bruno.henriques@hitachivantara.com>
2025-08-04 15:28:34 +0100

    feat(theme): support only light/dark color modes (#4857)
    * chore: remove createTheme in favor of mergeTheme
    
    * feat(theme): support only light/dark color modes
    
    ---------
    
    Co-authored-by: Bruno Henriques <zettca@users.noreply.github.com>

--- packages/styles/src/utils.ts
+++ packages/styles/src/utils.ts
@@ -1,6 +1,7 @@
 import type {
   DeepString,
   HvThemeBreakpoint,
+  HvThemeColorMode,
   HvThemeStructure,
   HvThemeVars,
   SpacingValue,
@@ -96,7 +97,7 @@ export const mergeTheme = (...objects: any[]): HvThemeStructure => {
 export const getThemeVars = (theme: HvThemeStructure) => {
   const cssVars: Record<string, any> = {};
 
-  const colorModes = Object.keys(theme.colors.modes);
+  const colorModes: HvThemeColorMode[] = ["light", "dark"];
 
   colorModes.forEach((colorMode) => {
     const styleName = `[data-theme="${theme.name}"][data-color-mode="${colorMode}"]`;
@@ -108,7 +109,7 @@ export const getThemeVars = (theme: HvThemeStructure) => {
 
     cssVars[styleName] = toCSSVars({
       colors: {
-        ...colors.modes[colorMode],
+        ...colors[colorMode],
       },
     });
 
