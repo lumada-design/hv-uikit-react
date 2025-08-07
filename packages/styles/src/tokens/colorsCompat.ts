7cbf531aa6d2f8dbae053532f5771a65b7f7e7e1
Bruno Henriques <zettca@users.noreply.github.com>
2025-08-07 01:17:40 +0100

    chore: revert base color value duplication

--- packages/styles/src/tokens/colorsCompat.ts
+++ packages/styles/src/tokens/colorsCompat.ts
@@ -1,77 +1,38 @@
-const light = {
+export const dsColors = {
   base_light: "#CCCCCC",
   base_dark: "#414141",
 
   // accent
   brand: "#CC0000",
-  primary: "#2064B4",
-  primary_80: "#1775E0",
-  primary_20: "#1775E019",
-  secondary: "#414141",
-  secondary_80: "#6C6B6B",
-  secondary_60: "#999999",
+  primary: ["#2064B4", "#639FE3"],
+  primary_80: ["#1775E0", "#82B2E8"],
+  primary_20: ["#1775E019", "#82B2E84C"],
+  secondary: ["#414141", "#CCCCCC"],
+  secondary_80: ["#6C6B6B", "#9A9999"],
+  secondary_60: ["#999999", "#656565"],
 
   // atmosphere
-  atmo1: "#FBFCFC",
-  atmo2: "#F4F5F5",
-  atmo3: "#E8E8E8",
-  atmo4: "#CCCED0",
+  atmo1: ["#FBFCFC", "#313131"],
+  atmo2: ["#F4F5F5", "#282828"],
+  atmo3: ["#E8E8E8", "#1F1F1F"],
+  atmo4: ["#CCCED0", "#4B4B4B"],
 
   // semantic
-  positive: "#478B1A",
-  positive_80: "#709C27",
-  positive_120: "#227A10",
-  neutral: "#4D8AC0",
-  warning: "#F9C846",
-  warning_120: "#F8AC39",
-  warning_140: "#F27C27",
-  negative: "#D43136",
-  negative_80: "#ED4747",
-  negative_120: "#B41B3A",
-  catastrophic: "#930A80",
+  positive: ["#478B1A", "#84D930"],
+  positive_80: ["#709C27", "#70BF21"],
+  positive_120: ["#227A10", "#63A621"],
+  neutral: ["#4D8AC0", "#7EBAD6"],
+  warning: ["#F9C846", "#E68C17"],
+  warning_120: ["#F8AC39", "#F57B36"],
+  warning_140: ["#F27C27", "#FE6B51"],
+  negative: ["#D43136", "#FF5E6C"],
+  negative_80: ["#ED4747", "#EC3D57"],
+  negative_120: ["#B41B3A", "#D92750"],
+  catastrophic: ["#930A80", "#9A76E7"],
   neutral_20: "#D8E6F1",
   positive_20: "#D7E6CF",
   negative_20: "#F4D3D4",
   warning_20: "#FBF2D8",
 };
 
-const dark = {
-  base_light: "#CCCCCC",
-  base_dark: "#414141",
-
-  // accent
-  brand: "#CC0000",
-  primary: "#639FE3",
-  primary_80: "#82B2E8",
-  primary_20: "#82B2E84C",
-  secondary: "#CCCCCC",
-  secondary_80: "#9A9999",
-  secondary_60: "#656565",
-
-  // atmosphere
-  atmo1: "#313131",
-  atmo2: "#282828",
-  atmo3: "#1F1F1F",
-  atmo4: "#4B4B4B",
-
-  // semantic
-  positive: "#84D930",
-  positive_80: "#70BF21",
-  positive_120: "#63A621",
-  neutral: "#7EBAD6",
-  warning: "#E68C17",
-  warning_120: "#F57B36",
-  warning_140: "#FE6B51",
-  negative: "#FF5E6C",
-  negative_80: "#EC3D57",
-  negative_120: "#D92750",
-  catastrophic: "#9A76E7",
-  neutral_20: "#D8E6F1",
-  positive_20: "#D7E6CF",
-  negative_20: "#F4D3D4",
-  warning_20: "#FBF2D8",
-};
-
-export const compatColors = { light, dark };
-
-export type HvThemeColorsCompat = typeof compatColors.light;
+export type HvThemeColorsCompat = typeof dsColors;
