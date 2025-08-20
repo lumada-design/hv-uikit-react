2addc7b0c86908897a3e47cd324cfdfb936396c3
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2025-08-20 11:15:45 +0100

    docs: add Tags examples (#4859)

---
+++ apps/docs/src/app/examples/tags/OverflowTags.tsx
@@ -0,0 +1,18 @@
+import { HvTag } from "@hitachivantara/uikit-react-core";
+
+export default function Demo() {
+  return (
+    <div className="flex w-320px p-xs border-1 border-border gap-sm overflow-x-scroll scroll-auto">
+      <HvTag label="Tag 1" />
+      <HvTag label="Tag 2" />
+      <HvTag label="Tag 3" />
+      <HvTag label="Tag 4" />
+      <HvTag label="Tag 5" />
+      <HvTag label="Tag 6" />
+      <HvTag label="Tag 7" />
+      <HvTag label="Tag 8" />
+      <HvTag label="Tag 9" />
+      <HvTag label="Tag 10" />
+    </div>
+  );
+}
