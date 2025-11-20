b05f4a7bf561a8dab367e65ce6692f177396a976
Bruno Henriques <bruno.henriques@hitachivantara.com>
2025-11-20 09:49:37 +0000

    fix(SearchInput): remove root padding (#4992)
    Co-authored-by: Bruno Henriques <zettca@users.noreply.github.com>

--- packages/core/src/SearchInput/SearchInput.styles.tsx
+++ packages/core/src/SearchInput/SearchInput.styles.tsx
@@ -1,7 +1,6 @@
 import { createClasses } from "@hitachivantara/uikit-react-utils";
-import { theme } from "@hitachivantara/uikit-styles";
 
 export const { staticClasses, useClasses } = createClasses("HvSearchInput", {
-  root: { paddingLeft: theme.space.xs },
+  root: {},
   input: { marginLeft: 0 },
 });
